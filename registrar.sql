c672356b2b6fbe7690947e667387a2f112a8ae09
Petr Blaha <petr.blaha@nic.cz>
2006-03-20 11:09:23 +0000

    zakladni seznam registratoru
    git-svn-id: file:///data/svn/enum/trunk/db@57 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ registrar.sql
@@ -0,0 +1,17 @@
+DELETE FROM Registrar;
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 100, 'REG-GENERAL-REGISTRY' ,    'GENERAL REGISTRY, s.r.o.' ,     'DomainMaster',   'www.domainmaster.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 300 , 'REG-ACTIVE24'  ,  'ACTIVE 24, s. r. o.' ,    'DOMENY.CZ' , 'www.domeny.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 400 ,'REG-HAVEL'  , 'ha-vel internet s.r.o.' ,       'ha-vel' , 'domeny.ha-vel.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 500, 'REG-IGNUM'   ,  'IGNUM, s.r.o.' ,                 'DOMENA.CZ' ,   ' www.domena.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 600 ,'REG-INTERNET-CZ',    'INTERNET CZ, a.s.' ,       'Velkoobchod domén' ,  'domeny.velkoobchod.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 700, 'REG-MIRAMO' ,   'MIRAMO spol. s r.o.' ,          '9net.cz' ,  'www.9net.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 800, 'REG-ZONER' ,   'ZONER software, s.r.o.' ,       'RegZone!' ,  'www.regZone.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 200, 'REG-CT'   , 'CESKÝ TELECOM, a.s.' ,          'Internet OnLine' , 'domeny.iol.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 110, 'REG-KRAXNET'  ,  'KRAXNET s.r.o.' ,               'XNET'   ,   'www.xnet.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES(120, 'REG-MEDIA4WEB',    'Media4web s.r.o.' ,          'Media4web'   , 'www.media4web.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES(130, 'REG-ONE'   ,   'ONE.CZ s.r.o.' ,                 'REGISTRATOR.CZ'  , 'www.registrator.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 140,'REG-WEB4U'  ,  'Web4U s.r.o.' ,                  'Správa domén',  'www.spravadomen.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 150 , 'REG-NEXTRA' ,   'NEXTRA Czech Republic s.r.o.' ,  'NEXTRA'   ,   'domeny.nextra.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 160 ,'REG-IPEX'  ,  'IPEX a.s.'              ,       'IPEX'        ,    'www.ipex.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 170 , 'REG-SKYNET',  'SkyNet, a.s.'            ,      'SkyNet'    ,        'www.skynet.cz');
+INSERT INTO Registrar ( id, roid , organization , name , url ) VALUES( 1,  'REG-LRR',    'CZ.NIC, z.s.p.o.'          ,    'LRR'         ,      'www.lrr.cz');
