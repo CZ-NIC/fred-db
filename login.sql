d3e793056f1bfa2227acf12b76e9ba6d4391ddba
Petr Blaha <petr.blaha@nic.cz>
2006-04-13 12:37:59 +0000

    logovaci tabulka pro prihlasovani klientu
    git-svn-id: file:///data/svn/enum/trunk/db@149 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ login.sql
@@ -0,0 +1,12 @@
+-- login tabulka pro pihlasovani clientu
+DROP TABLE Login CASCADE;
+CREATE TABLE Login (
+        ID SERIAL PRIMARY KEY, -- vraci se jako clientID z CORBA funkce Login
+	RegistrarID INTEGER NOT NULL REFERENCES Registrar, -- id registratora
+        LoginDate timestamp NOT NULL DEFAULT now(), -- datum a cas prihlaseni do systemu
+        loginTRID varchar(128) NOT NULL, -- cislo prihlasovaci transakce
+	LogoutDate timestamp, -- datum a cas odhlaseni
+        logoutTRID varchar(128) -- cislo odhlasovaci transakce
+        );
+
+
