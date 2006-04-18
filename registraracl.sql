2ce425c0a900ffc14375a86eeee20f68d6bfbd4c
Petr Blaha <petr.blaha@nic.cz>
2006-04-18 13:53:54 +0000

    naplneni tabulke hesel a certifikatu
    git-svn-id: file:///data/svn/enum/trunk/db@161 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ registraracl.sql
@@ -0,0 +1,17 @@
+
+DELETE FROM registraracl;
+INSERT INTO registraracl VALUES( 1, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 110, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 120 , 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 130, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 140, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 150, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 160, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 170, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 200, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 300, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 400, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 500, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 600, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 700, 3 , 'certifikat' ,  'heslo' );
+INSERT INTO registraracl VALUES( 800, 3 , 'certifikat' ,  'heslo' );
