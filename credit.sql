7b41afe67778de3b7beb1eb98570895817338fb6
Petr Blaha <petr.blaha@nic.cz>
2006-08-02 13:51:09 +0000

    uprava tabulky Credit typ operace a ID objektu
    git-svn-id: file:///data/svn/enum/trunk/db@828 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- credit.sql
+++ credit.sql
@@ -1,12 +1,11 @@
 -- tabulka pro pricitani creditu za operace DomainCreate a DomainReNew
--- DROP TABLE Credit CASCADE;
+DROP TABLE Credit CASCADE;
 CREATE TABLE Credit (
         ID SERIAL PRIMARY KEY, -- vraci se jako clientID z CORBA funkce Login
 	RegistrarID INTEGER NOT NULL REFERENCES Registrar, -- id registratora
-	DomainID  INTEGER NOT NULL , -- REFERENCES odkaz do Domain
-        -- tabulky nebo do domain_history pokud je domena vymazana	          
+	ObjectID  INTEGER NOT NULL , -- id objektu domain nsset contact
         Date timestamp NOT NULL DEFAULT now(), -- datum a cas pricteni creditu
-        DomainCreate boolean  DEFAULT true , -- true pokud je opera 
+        OperationID INTEGER NOT NULL REFERENCES enum_action , -- typ akce 
         period INTEGER   -- perioda prodlozeni platnosti domeny v mesicich
         );
 
