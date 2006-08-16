6201a74b0229085bd96444929a08c82cfbdad89a
Petr Blaha <petr.blaha@nic.cz>
2006-08-16 08:44:18 +0000

    pridan identifikator MPSV
    git-svn-id: file:///data/svn/enum/trunk/db@937 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- enum_ssntype.sql
+++ enum_ssntype.sql
@@ -11,3 +11,4 @@ INSERT INTO enum_ssntype  VALUES(1 , 'RC' , 'rodne cislo');
 INSERT INTO enum_ssntype  VALUES(2 , 'OP' , 'cislo obcanky');
 INSERT INTO enum_ssntype  VALUES(3 , 'PASS' , 'cislo pasu');
 INSERT INTO enum_ssntype  VALUES(4 , 'ICO' , 'ico');
+INSERT INTO enum_ssntype  VALUES(5 , 'MPSV' , 'identifikator ministerstva prace s soc. veci');
