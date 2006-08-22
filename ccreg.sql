d648b9c59a6966cb9cf9264ff59f7fa505a7c5ee
Petr Blaha <petr.blaha@nic.cz>
2006-08-22 09:39:48 +0000

    pridane dalsi parametry zony maximalni pocet tecek v domene a jestli je enum
    git-svn-id: file:///data/svn/enum/trunk/db@1008 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccreg.sql
+++ ccreg.sql
@@ -10,14 +10,16 @@ CREATE TABLE enum_country (
 CREATE TABLE zone (
         id SERIAL PRIMARY KEY,
         fqdn VARCHAR(255) UNIQUE NOT NULL,
-	ex_period_min int not null,
+        ex_period_min int not null,
         ex_period_max int not null,
-        val_period int not null
+        val_period int not null,
+        dots_max  int not null default 1,
+        enum_zone boolean default 'f'
         );
 
-INSERT INTO zone (fqdn ,ex_period_min, ex_period_max, val_period) VALUES('0.2.4.e164.arpa',12,120,6);
-INSERT INTO zone (fqdn ,ex_period_min, ex_period_max, val_period) VALUES('0.2.4.c.e164.arpa',12,120,6);
-INSERT INTO zone (fqdn ,ex_period_min, ex_period_max, val_period) VALUES('cz',12,120,0);
+INSERT INTO zone  VALUES(1,'0.2.4.e164.arpa',12,120,6,9,'t');
+INSERT INTO zone  VALUES(2,'0.2.4.c.e164.arpa',12,120,6,9,'t');
+INSERT INTO zone  VALUES(3,'cz',12,120,0,1,'f');
 
 -- DROP TABLE Registrar CASCADE;
 CREATE TABLE Registrar (
