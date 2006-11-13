aa596c143a1ee60416b07c1a191edf36b4efb247
Petr Blaha <petr.blaha@nic.cz>
2006-11-13 12:31:35 +0000

    pridani typu platebni operarce
    git-svn-id: file:///data/svn/enum/db/trunk@1829 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- bank.sql
+++ bank.sql
@@ -5,6 +5,18 @@ code char(4) PRIMARY KEY,
 name_short varchar(4) UNIQUE NOT NULL , -- zkratka
 name_full varchar(64) UNIQUE  NOT NULL -- uplny nazev
 );
+
+CREATE TABLE enum_bank_type (
+id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
+inout boolean, -- prijem true vydej false
+name varchar(32) -- nazev operace
+);
+
+INSERT INTO enum_bank_type VALUES ( 100 , 'f' , 'bankovni poplatky' );
+INSERT INTO enum_bank_type VALUES ( 20 , 't' , 'prevod na ucet' );
+INSERT INTO enum_bank_type VALUES ( 21 , 't' , 'trvaly prevod na ucet' );
+INSERT INTO enum_bank_type VALUES ( 30 ,  'f' , 'odchozi platba' ); 
+  
                                  
 -- ACCOUNT -- tabulka nasich uctu
 CREATE TABLE account 
@@ -36,10 +48,11 @@ id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 statement_id int  REFERENCES BANK_STATEMENT_HEAD, -- odkaz do tabulky hlavicek vypisu
 account_number char(16) UNIQUE NOT NULL , -- cislo uctu ze ktereho prisla nebo kam byla odeslana platba
 bank_code char(4)  REFERENCES enum_bank_code,   -- kod banky
+typ int  REFERENCES  enum_bank_type, -- typ operace prevod na ucet bankovni poplatek urok atd.
 KS char(10), -- konstantni symbol
 VS char(10), -- konstantni symbol
 SS char(10), -- konstantni symbol
-price money,  -- zuctovana castka  pokud je debet zaporna castka
+price money NOT NULL,  -- zuctovana castka  pokud je debet zaporna castka
 mem  varchar(64) -- poznamka
 );
 
