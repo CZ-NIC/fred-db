1d8c14c0fa2cd622fa4f3ab99352b003c14f3a08
Petr Blaha <petr.blaha@nic.cz>
2006-11-22 09:48:54 +0000

    updavy schematu pro bankovni vypisy pridano poradi vypisu pro parovani
    git-svn-id: file:///data/svn/enum/db/trunk@1933 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- bank.sql
+++ bank.sql
@@ -6,39 +6,30 @@ name_short varchar(4) UNIQUE NOT NULL , -- zkratka
 name_full varchar(64) UNIQUE  NOT NULL -- uplny nazev
 );
 
-CREATE TABLE enum_bank_type (
-id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
-inout boolean, -- prijem true vydej false
-name varchar(32) -- nazev operace
-);
-
-INSERT INTO enum_bank_type VALUES ( 100 , 'f' , 'bankovni poplatky' );
-INSERT INTO enum_bank_type VALUES ( 20 , 't' , 'prevod na ucet' );
-INSERT INTO enum_bank_type VALUES ( 21 , 't' , 'trvaly prevod na ucet' );
-INSERT INTO enum_bank_type VALUES ( 30 ,  'f' , 'odchozi platba' ); 
-  
-                                 
+                                   
 -- ACCOUNT -- tabulka nasich uctu
-CREATE TABLE account 
+CREATE TABLE bank_account 
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 account_number char(16) UNIQUE NOT NULL , -- cislo uctu
 bank_code char(4)  REFERENCES enum_bank_code,   -- kod banky
 balance  numeric(10,2), -- aktualni zustatek 
-last_statement date -- do jakeho data byl nacten vypis z banky
+last_date date, -- datum posledniho vypisu 
+last_num int  -- cislo posledniho vypisu
 );
 
 -- bankovni vypisy 
 CREATE TABLE BANK_STATEMENT_HEAD 
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
-account_id int  REFERENCES account, -- zpracovani pro dany ucet odkaz to tabulky account
-statement_from date NOT NULL, -- vypis za obdobi OD - DO
-statement_to date NOT NULL , -- do
-balance_in numeric(10,2) NOT NULL, -- pocatecni zustatek
-balance_out numeric(10,2) NOT NULL ,  -- konecny zustatek
-receipts   numeric(10,2) NOT NULL, -- prijmy za behem vypisu
-expenditures numeric(10,2) NOT NULL -- vydaje behem vypisu
+account_id int  REFERENCES bank_account, -- zpracovani pro dany ucet odkaz to tabulky account
+num int, -- poradove cislo vypisu
+create_date date NOT NULL, --  datum vytvoreni vypisu
+balance_old_date date NOT NULL , -- datum starehi zustatku
+balance_old numeric(10,2) NOT NULL, -- stary zustatek
+balance_new numeric(10,2) NOT NULL,  -- novy zustatek
+balance_credit  numeric(10,2) NOT NULL, -- prijmy za behem vypisu (creditni obrat )
+balence_debet numeric(10,2) NOT NULL -- vydaje behem vypisu  ( debetni obrat )
 );
 
 
@@ -47,13 +38,14 @@ CREATE TABLE BANK_STATEMENT_ITEM
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 statement_id int  REFERENCES BANK_STATEMENT_HEAD, -- odkaz do tabulky hlavicek vypisu
-account_number char(16) UNIQUE NOT NULL , -- cislo uctu ze ktereho prisla nebo kam byla odeslana platba
+account_number char(16) UNIQUE NOT NULL , -- cislo protiuctu ze ktereho prisla nebo kam byla odeslana platba
 bank_code char(4)  REFERENCES enum_bank_code,   -- kod banky
-typ int  REFERENCES  enum_bank_type, -- typ operace prevod na ucet bankovni poplatek urok atd.
-KS char(10), -- konstantni symbol
+code int, -- kod uctovani 1 polozka debet 2 polozka credit 4 storno debet 5 storno kredit 
+KS char(10), -- konstantni symbol (obsahuje i kod banky )
 VS char(10), -- konstantni symbol
 SS char(10), -- konstantni symbol
 price numeric(10,2) NOT NULL,  -- zuctovana castka  pokud je debet zaporna castka
+evid varchar(20), --cislo dokladu
 mem  varchar(64) -- poznamka
 );
 
