23c6e1770afdb551ce65ecb62c81f6e03ad7653e
Petr Blaha <petr.blaha@nic.cz>
2006-11-23 13:45:56 +0000

    upravy pridani cisla dokladu datumu do polozek uctu a zrudeno UNIQUE u cisla protiuctu
    git-svn-id: file:///data/svn/enum/db/trunk@1948 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- bank.sql
+++ bank.sql
@@ -29,7 +29,7 @@ balance_old_date date NOT NULL , -- datum starehi zustatku
 balance_old numeric(10,2) NOT NULL, -- stary zustatek
 balance_new numeric(10,2) NOT NULL,  -- novy zustatek
 balance_credit  numeric(10,2) NOT NULL, -- prijmy za behem vypisu (creditni obrat )
-balence_debet numeric(10,2) NOT NULL -- vydaje behem vypisu  ( debetni obrat )
+balance_debet numeric(10,2) NOT NULL -- vydaje behem vypisu  ( debetni obrat )
 );
 
 
@@ -38,15 +38,16 @@ CREATE TABLE BANK_STATEMENT_ITEM
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 statement_id int  REFERENCES BANK_STATEMENT_HEAD, -- odkaz do tabulky hlavicek vypisu
-account_number char(16) UNIQUE NOT NULL , -- cislo protiuctu ze ktereho prisla nebo kam byla odeslana platba
+account_number char(16)  NOT NULL , -- cislo protiuctu ze ktereho prisla nebo kam byla odeslana platba
 bank_code char(4)  REFERENCES enum_bank_code,   -- kod banky
 code int, -- kod uctovani 1 polozka debet 2 polozka credit 4 storno debet 5 storno kredit 
-KS char(10), -- konstantni symbol (obsahuje i kod banky )
-VS char(10), -- konstantni symbol
-SS char(10), -- konstantni symbol
+KonstSym char(10), -- konstantni symbol (obsahuje i kod banky )
+VarSymb char(10), -- konstantni symbol
+SpecSymb char(10), -- konstantni symbol
 price numeric(10,2) NOT NULL,  -- zuctovana castka  pokud je debet zaporna castka
-evid varchar(20), --cislo dokladu
-mem  varchar(64) -- poznamka
+account_evid varchar(20), --cislo dokladu
+account_date date NOT NULL, --  datum zuctovani pripsani na ucet ci odeslani
+account_memo  varchar(64) -- poznamka
 );
 
 
