7de84acd60417799ba9b4bed3346325be6ff83b4
Petr Blaha <petr.blaha@nic.cz>
2006-12-04 10:09:47 +0000

    tabluka object vyhozeno NAME (fqdn ci handle je v tabulkach domain , nsset ci contact) a vytvorena tabulka pro historii objektu kam uz se neuklada object.historyID odkaz na posledni zmenu v historii
    git-svn-id: file:///data/svn/enum/db/trunk@2050 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- object_db.sql
+++ object_db.sql
@@ -2,17 +2,30 @@
 
 CREATE TABLE OBJECT (
         ID SERIAL PRIMARY KEY,
-        historyid integer REFERENCES history, -- odkaz na posledni zmenu v historii
+        historyID integer REFERENCES history, -- odkaz na posledni zmenu v historii
         type smallint , -- typ onjektu 1 kontakt 2 nsset 3 domena
-        NAME varchar(255) UNIQUE NOT NULL , -- handle ci FQDN
         ROID varchar(64) UNIQUE NOT NULL,
         ClID INTEGER NOT NULL REFERENCES Registrar,
         CrID INTEGER NOT NULL REFERENCES Registrar,
         UpID INTEGER REFERENCES Registrar,
         CrDate timestamp NOT NULL DEFAULT now(),
---      Exdate timestamp DEFAULT NULL , specificky pro jen pro domenu DROP
         TrDate timestamp DEFAULT NULL,
         UpDate timestamp DEFAULT NULL,
         AuthInfoPw varchar(300) -- dle Honzy v XML schematech
+        );
+
+
+CREATE TABLE OBJECT_history (
+        historyID INTEGER PRIMARY KEY REFERENCES History, -- odkaz do historie
+        ID INTEGER  NOT NULL,
+        type smallint , -- typ onjektu 1 kontakt 2 nsset 3 domena         
+        ROID varchar(64)  NOT NULL,
+        ClID INTEGER NOT NULL REFERENCES Registrar,
+        CrID INTEGER NOT NULL REFERENCES Registrar,
+        UpID INTEGER REFERENCES Registrar,
+        CrDate timestamp NOT NULL,
+        Trdate timestamp,
+        UpDate timestamp,
+        AuthInfoPw varchar(300)
         );
 
