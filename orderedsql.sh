bb80f5cea64e83ca6a7b49f8497fc0fe23c47b26
Petr Blaha <petr.blaha@nic.cz>
2006-12-06 10:37:20 +0000

    prepsan zpusob vytvoreni tabulek jejich posloupnost
    git-svn-id: file:///data/svn/enum/db/trunk@2081 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- orderedsql.sh
+++ orderedsql.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 # vypsani sql prikazu ve spravnem poradi pro zalozeni nove databaze
+#base system
 cat error.sql
+cat enum_reason.sql
 cat enum_status.sql
 cat enum_ssntype.sql
-cat ccreg.sql
-cat login.sql
-cat action.sql
-cat history.sql
-cat credit.sql
-cat enum_reason.sql
 cat enum_country.sql
 cat enum_cs_country.sql
+cat zone.sql
+#zone generator
 cat genzone.sql
-cat admin.sql
+#registar & registraracl (pass) tables
+cat registrar.sql
+#login and action table plus history table
+cat login.sql
+cat action.sql
+# object table
+cat ccreg.sql
+cat history.sql
+#adif
+cat admin.sql  
+#mailer
 cat mail_notify.sql
+# banking
+cat enum_bank_code.sql
+cat bank.sql
+cat invoice.sql
+cat credit.sql
+
