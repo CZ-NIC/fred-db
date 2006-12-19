97cc616bfe47f4c33bcd7db2e6bf4bb283797264
Petr Blaha <petr.blaha@nic.cz>
2006-12-19 10:15:59 +0000

    rozdelena tabulka invoice na zalohove a ucetni faktury tabulka faktur
    git-svn-id: file:///data/svn/enum/db/trunk@2228 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- invoice.sql
+++ invoice.sql
@@ -21,17 +21,32 @@ insert into invoice_prefix values ( 3 , 3 ,  0  , 'F2006-CZ-' , 6 , 1 );
 insert into invoice_prefix values ( 4 , 1 ,  0  , 'F2006-ENUM-' , 6  , 1 );
 
 
--- invoice  faktury (zalohove i normalni)
+-- invoice  Zalohove faktury 
 CREATE TABLE invoice
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
-typ integer default 0, -- typ faktury 1 zalohova 2 normalni
 Zone INTEGER REFERENCES Zone (ID),
 CrDate timestamp NOT NULL DEFAULT now(),  -- datum a cas vytvoreni
 prefix varchar(24) UNIQUE NOT NULL , -- cislo  faktury z invoice_prefix.prefix a counter
 registrarID INTEGER NOT NULL REFERENCES Registrar, -- odkaz na registratora
 Credit numeric(10,2) NOT NULL DEFAULT 0.0, -- kredit ze ktereho se cerpa az do nuly 
 Price numeric(10,2) NOT NULL DEFAULT 0.0, -- vyse faktury i s dani
+numVAT integer  NOT NULL  DEFAULT 19, -- vyse dane 19%
+total numeric(10,2) NOT NULL DEFAULT 0.0,  -- castka bez dane
+vat numeric(10,2) NOT NULL DEFAULT 0.0  -- odvedena dan
+);
+
+-- faktur faktury za  uctovane polozky
+CREATE TABLE faktur
+(
+id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
+Zone INTEGER REFERENCES Zone (ID),
+CrDate timestamp NOT NULL DEFAULT now(),  -- datum a cas vytvoreni
+FromDate date NOT NULL , -- datum zuctovaciho odobi od-do
+ToDate date NOT NULL , -- do datumu
+prefix varchar(24) UNIQUE NOT NULL , -- cislo  faktury z invoice_prefix.prefix a counter
+registrarID INTEGER NOT NULL REFERENCES Registrar, -- odkaz na registratora
+Price numeric(10,2) NOT NULL DEFAULT 0.0, -- celkova castka
 numVAT integer  NOT NULL  DEFAULT 19, -- vyse dane 19%
 total numeric(10,2) NOT NULL DEFAULT 0.0,  -- castka bez dane
 vat numeric(10,2) NOT NULL DEFAULT 0.0  -- odvedena dan
@@ -62,6 +77,7 @@ CREATE TABLE invoice_object_registry
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 InvoiceID INTEGER REFERENCES Invoice (ID), -- id zalohove faktiry ze ktere jde castka price
+FakturID INTEGER REFERENCES Invoice (ID) , -- id ostre faktury na ktere je polozka vedena
 CrDate timestamp NOT NULL DEFAULT now(),  -- datum a cas zuctovani
 objectID integer  REFERENCES object_registry (id),
 registrarID INTEGER NOT NULL REFERENCES Registrar, -- odkaz na registratora 
