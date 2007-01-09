88ed92415abedec5fd0c218070ee59898eccf1b5
Petr Blaha <petr.blaha@nic.cz>
2007-01-09 14:45:01 +0000

    vyhozeno domain.update a promazany remove komentare
    git-svn-id: file:///data/svn/enum/db/trunk@2325 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccreg.sql
+++ ccreg.sql
@@ -32,15 +32,6 @@ CREATE INDEX object_id_idx ON Object (ID);
 -- DROP TABLE Contact CASCADE;
 CREATE TABLE Contact (
         ID INTEGER PRIMARY KEY REFERENCES object (id),
--- REMOVE        Handle varchar(255) UNIQUE NOT NULL,
--- REMOVE        ROID varchar(255) UNIQUE NOT NULL,
--- REMOVE        Status smallint[], -- TODO: create trigger to check values agains enum_status
--- REMOVE        ClID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrDate timestamp NOT NULL DEFAULT now(),
--- REMOVE        UpID INTEGER REFERENCES Registrar,
--- REMOVE        UpDate timestamp,
--- REMOVE        TrDate timestamp,
         Name varchar(1024),
         Organization varchar(1024),
         Street1 varchar(1024),
@@ -59,7 +50,6 @@ CREATE TABLE Contact (
         DiscloseTelephone boolean DEFAULT False,
         DiscloseFax boolean DEFAULT False,
         DiscloseEmail boolean DEFAULT False,
---         AuthInfoPw varchar(32),
         NotifyEmail varchar(1024),
         VAT varchar(32),
         SSN varchar(32),
@@ -68,34 +58,10 @@ CREATE TABLE Contact (
 CREATE INDEX contact_id_idx ON Contact (ID);
 
 
--- DROP TABLE Term CASCADE;
--- CREATE TABLE Term (
---        ID SERIAL PRIMARY KEY,
---        CrDate date NOT NULL
---        );
--- 
--- DROP TABLE ContactAgreement CASCADE;
--- CREATE TABLE ContactAgreement (
---         ID SERIAL PRIMARY KEY,
---         ContactID INTEGER NOT NULL REFERENCES Contact ON UPDATE Cascade,
---         TermID INTEGER NOT NULL REFERENCES Term ON UPDATE Cascade
---         );
--- CREATE INDEX contactagreement_contactid_idx ON ContactAgreement (ContactID);
--- CREATE INDEX contactagreement_termid_id ON ContactAgreement (TermID);
 
 -- DROP TABLE NSSet CASCADE;
 CREATE TABLE NSSet (
         ID INTEGER PRIMARY KEY REFERENCES object (id),
--- REMOVE        ROID varchar(255) UNIQUE NOT NULL,
--- REMOVE        Handle varchar(255) UNIQUE NOT NULL,
--- REMOVE        Status smallint[], -- TODO: create trigger to check values agains enum_status
--- REMOVE        ClID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrDate timestamp NOT NULL DEFAULT now(),
--- REMOVE        UpID INTEGER REFERENCES Registrar,
--- REMOVE        AuthInfoPw varchar(32),
--- REMOVE        TrDate timestamp,
--- REMOVE        UpDate timestamp,
         checklevel smallint default 0
         );
 CREATE INDEX nsset_id_idx ON NSSet (ID);
@@ -114,13 +80,6 @@ CREATE TABLE Host (
         ID SERIAL PRIMARY KEY,
         NSSetID INTEGER REFERENCES NSSet ON UPDATE CASCADE,
         FQDN VARCHAR(255)   NOT NULL,  -- nemuze byt UNIQUE pro dva ruzne nssety stejny dns host
---        ClID INTEGER NOT NULL REFERENCES Registrar ON UPDATE CASCADE,
--- zruseno        CrDate TIMESTAMP NOT NULL,
--- duplicitni    UpID INTEGER NOT NULL REFERENCES Registrar ON UPDATE CASCADE ON DELETE SET NULL,
--- udaje         UpDate TIMESTAMP NOT NULL,
--- s nsset       Status INTEGER[], -- TODO: write trigger to check values
--- zruseno nahrazeno vazebni tabulkou host_ipaddr_map
---     IpAddr INET[] NOT NULL,  -- NOTE: we don't have to store IP version, since it's obvious from address
         UNIQUE (NSSetID, FQDN ) -- unikatni klic
         );
 
@@ -141,20 +100,9 @@ CREATE TABLE host_ipaddr_map (
 CREATE TABLE Domain (
         ID INTEGER PRIMARY KEY REFERENCES object (ID),
         Zone INTEGER REFERENCES Zone (ID),
-
--- REMOVE        ROID varchar(255) UNIQUE NOT NULL,
--- REMOVE        FQDN varchar(255) UNIQUE NOT NULL,
--- REMOVE        Status smallint[], -- TODO: create trigger to check values agains enum_status
         Registrant INTEGER NOT NULL REFERENCES Contact,
         NSSet INTEGER NULL REFERENCES NSSet, -- odkaz na nsset muze by i NULL lze zaregistrovat domenu bez nssetu
--- REMOVE        ClID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrID INTEGER NOT NULL REFERENCES Registrar,
--- REMOVE        CrDate timestamp NOT NULL DEFAULT now(),
--- REMOVE        UpID INTEGER REFERENCES Registrar,
         Exdate timestamp NOT NULL,
--- REMOVE        TrDate timestamp,
--- REMOVE        AuthInfoPw varchar(32),
-        UpDate timestamp
         );
 CREATE INDEX domain_zone_idx ON Domain (Zone);
 CREATE INDEX domain_id_idx ON Domain (ID);
@@ -168,13 +116,6 @@ CREATE TABLE domain_contact_map (
 CREATE INDEX domain_contact_map_domainid_idx ON domain_contact_map (DomainID);
 
 
--- DROP TABLE domain_host_map CASCADE;
--- CREATE TABLE domain_host_map (
---         DomainID INTEGER NOT NULL REFERENCES Domain ON UPDATE CASCADE ON DELETE CASCADE,
---         HostID INTEGER NOT NULL REFERENCES Host ON UPDATE CASCADE,
---         UNIQUE (DomainID, HostID)
---         );
--- CREATE INDEX domain_host_map_domainid_idx ON domain_host_map (DomainID);
 
 -- DROP TABLE DNSSEC CASCADE;
 CREATE TABLE DNSSEC (
