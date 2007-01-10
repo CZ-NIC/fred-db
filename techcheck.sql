3c0780b7fa10a53cdd3c008e1878d42433cfd6d8
Jan Kryl <jan.kryl@nic.cz>
2007-01-10 16:41:57 +0000

    Commit upraveneho databazoveho backendu pro technicke testy
    git-svn-id: file:///data/svn/enum/db/trunk@2336 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- techcheck.sql
+++ techcheck.sql
@@ -13,21 +13,24 @@ CREATE TABLE check_test (
 	severity SMALLINT NOT NULL, -- uroven testu
 	description VARCHAR(300) NOT NULL, -- textovy popis testu
 	disabled BOOLEAN NOT NULL DEFAULT False,
-	script VARCHAR(300) NOT NULL -- script ktery realizuje test
+	script VARCHAR(300) NOT NULL, -- jmeno scriptu ktery realizuje test
+	need_domain BOOLEAN NOT NULL -- zda-li test dava smysl pouze s konkretnim fqdn domeny
 );
 
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (10, 'autonomous', 2, '', False, 'autonomous.sh');
+VALUES (1,  'existance',     1, '', False, 'existance.py');
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (20, 'existance', 1, '', False, 'existance.sh');
+VALUES (10, 'autonomous',    5, '', False, 'autonomous.py');
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (30, 'authoritative', 1, '', False, 'authoritative.sh');
+VALUES (20, 'presence',      2, '', False, 'presence.py');
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (40, 'heterogenous', 3, '', False, 'heterogenous.sh');
+VALUES (30, 'authoritative', 3, '', False, 'authoritative.py');
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (50, 'recursive', 2, '', False, 'recursive.sh');
+VALUES (40, 'heterogenous',  6, '', False, 'heterogenous.py');
 INSERT INTO check_test (id, name, severity, description, disabled, script)
-VALUES (60, 'recursive4all', 2, '', False, 'recursive4all.sh');
+VALUES (50, 'recursive',     4, '', False, 'recursive.py');
+INSERT INTO check_test (id, name, severity, description, disabled, script)
+VALUES (60, 'recursive4all', 4, '', False, 'recursive4all.py');
 
 CREATE TABLE check_dependance (
 	id SERIAL PRIMARY KEY,
@@ -35,26 +38,45 @@ CREATE TABLE check_dependance (
 	testid INTEGER REFERENCES check_test (id)
 );
 
-INSERT INTO check_dependance (addictid, testid) VALUES (30, 20);
-INSERT INTO check_dependance (addictid, testid) VALUES (40, 20);
-INSERT INTO check_dependance (addictid, testid) VALUES (50, 20);
-INSERT INTO check_dependance (addictid, testid) VALUES (60, 20);
+INSERT INTO check_dependance (addictid, testid) VALUES (10, 1);
+INSERT INTO check_dependance (addictid, testid) VALUES (20, 1);
+INSERT INTO check_dependance (addictid, testid) VALUES (30, 1);
+INSERT INTO check_dependance (addictid, testid) VALUES (30,20);
+INSERT INTO check_dependance (addictid, testid) VALUES (40, 1);
+INSERT INTO check_dependance (addictid, testid) VALUES (50, 1);
+INSERT INTO check_dependance (addictid, testid) VALUES (60, 1);
 
-CREATE TABLE check_domain (
+CREATE TABLE check_nsset (
 	id SERIAL PRIMARY KEY,
-	domain_id INTEGER REFERENCES object_registry (id), -- domain
-	-- domain version, actual in time of record creation
-	domain_hid INTEGER REFERENCES domain_history (historyid),
+	-- nsset version, actual in time of record creation
+	nsset_hid INTEGER REFERENCES domain_history (historyid),
 	checkdate TIMESTAMP NOT NULL DEFAULT now(),
-	reason SMALLINT NOT NULL,
-	overallstatus BOOLEAN NOT NULL
+	-- 0 = UNKNOWN
+	-- 1 = EPP
+	-- 2 = MANUAL
+	-- 3 = REGULAR
+	reason SMALLINT NOT NULL DEFAULT 0,
+	-- 0 = all tests were passed
+	-- 1 = one or more tests failed
+	-- 2 = one or more tests have unknown status and none has failed
+	overallstatus SMALLINT NOT NULL
 );
 
 CREATE TABLE check_result (
 	id SERIAL PRIMARY KEY,
-	checkid INTEGER references check_domain(id),
+	checkid INTEGER references check_nsset(id),
 	testid INTEGER references check_test(id),
-	passed BOOLEAN NOT NULL,
-	note TEXT
+	-- tri-state logic (0=passed, 1=failed, 2=unknown)
+	--    unknown occurs if script failed for unknown reason
+	status SMALLINT NOT NULL,
+	note VARCHAR(300), -- output of test script (stderr)
+	data VARCHAR(300) -- test-specific text data (stdout)
 );
 
+-- Here are stored fqdns of domains which were tested with nsset and are not
+-- part of register
+CREATE TABLE check_fqdn (
+	id SERIAL PRIMARY KEY,
+	checkid INTEGER references check_nsset(id),
+	fqdn VARCHAR(300) NOT NULL
+);
