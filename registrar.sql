7532d81d976f1d15ef509ddb5794524fc5dc7e7e
Petr Blaha <petr.blaha@nic.cz>
2007-01-17 15:08:50 +0000

    oprava tabulky od kdy ma prava registrator pracovat z danou zonou a od kdy se mu fakturuje za danouz zonu
    git-svn-id: file:///data/svn/enum/db/trunk@2423 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- registrar.sql
+++ registrar.sql
@@ -105,9 +105,10 @@ INSERT INTO  RegistrarInvoice VALUES ( 3 , 3  , 1 , '2007-01-01' , NULL );
 INSERT INTO  RegistrarInvoice VALUES ( 4 , 4  , 1 , '2007-01-01' , NULL );
 INSERT INTO  RegistrarInvoice VALUES ( 5 , 5  , 1 , '2007-01-01' , NULL );
 INSERT INTO  RegistrarInvoice VALUES ( 6 , 6  , 1 , '2007-01-01' , NULL );
-INSERT INTO  RegistrarInvoice VALUES ( 8 , 7  , 1 , '2007-01-01' , NULL );
-INSERT INTO  RegistrarInvoice VALUES ( 9 , 8  , 1 , '2007-01-01' , NULL );
-INSERT INTO  RegistrarInvoice VALUES ( 10 , 10  , 1 , '2007-01-01' , NULL );
+INSERT INTO  RegistrarInvoice VALUES ( 7 , 7  , 1 , '2007-01-01' , NULL );
+INSERT INTO  RegistrarInvoice VALUES ( 8 , 8  , 1 , '2007-01-01' , NULL );
+INSERT INTO  RegistrarInvoice VALUES ( 9 , 9  , 1 , '2007-01-01' , NULL );
+INSERT INTO  RegistrarInvoice VALUES ( 10, 10 , 1 , '2007-01-01' , NULL );
 
 
 
