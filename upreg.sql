ee3d888efb04e775fcb8f522fab4d178190d8d54
Petr Blaha <petr.blaha@nic.cz>
2007-01-18 14:27:12 +0000

    update adres a ICO DIC puls varsymb pro platbu u ENUM registratoru
    git-svn-id: file:///data/svn/enum/db/trunk@2458 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upreg.sql
@@ -0,0 +1,9 @@
+update registrar set Street1=  'Kamenická 599/26'  , postalcode=   '170 00'  , City= 'Praha 7'  , ICO=  '26460335'   , varsymb= '26460335'  , DIC=   'CZ26460335' , Country= 'CZ'  where id=         2 ;
+update registrar set Street1=   'Thámova 18'  , postalcode=   '186 00'  , City=  'Praha 8'  , ICO= '26159708'   , varsymb=  '26159708'  , DIC=   'CZ26159708' , Country= 'CZ'  where id=          3 ;
+update registrar set Street1= 'Široká 37'  , postalcode=  '370 01'  , City=  'České Budějovice'  , ICO= '45021295'   , varsymb= '45021295'  , DIC=   'CZ45021295'  , Country= 'CZ' where id=        4 ;
+update registrar set Street1=  'Novohradská 745/21'  , postalcode=  '371 07'  , City= 'České Budějovice'  , ICO= '26027267'   , varsymb= '26027267'  , DIC=   'CZ26027267' , Country= 'CZ' where id=        5 ;
+update registrar set Street1=   'Masarykova 1117'   , postalcode=  '738 01'  , City=  'Frýdek - Místek'  , ICO= '42868602'   , varsymb= '42868602'  , DIC=   'CZ42868602'  , Country= 'CZ'  where id=        6 ;
+update registrar set Street1=  'Václavské náměstí 17'  , postalcode=  '110 00'  , City= 'Praha 1'  , ICO= '25124005'   , varsymb= '25124005'  , DIC=   'CZ25124005'  , Country= 'CZ'  where id=          7;
+update registrar set Street1=  '2, rue Léon Laval'  , postalcode=  'L-3372'  , City=  'Leudelange'  , ICO= NULL   , varsymb='19406747'  , DIC=   'LU19406747'  , Country= 'LU'  where id=           8 ;
+update registrar set Street1=   'Pláničkova 1'  , postalcode=   '162 00'  , City=  'Praha 6'  , ICO= '25115804'   , varsymb= '25115804'  , DIC=   'CZ25115804'  , Country= 'CZ'  where id=          9 ;
+update registrar set Street1=   'Bernolákova 1190/4'  , postalcode=   '142 00'  , City=  'Praha 4'  , ICO= '27218791'   , varsymb= '27218791'  , DIC=   'CZ27218791'  , Country= 'CZ'          where id=         10;
