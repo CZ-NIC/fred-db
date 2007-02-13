d69b18a113915e606095e2c764f63e9276ad13d4
Jan Kryl <jan.kryl@nic.cz>
2007-02-13 13:46:46 +0000

    Uprava filemanageru - pridani typu souboru. Zmeny v databazi, v idl a kodu samotnem.
    git-svn-id: file:///data/svn/enum/db/trunk@2577 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- filemanager.sql
+++ filemanager.sql
@@ -1,8 +1,17 @@
+CREATE TABLE enum_filetype (
+	id smallint PRIMARY KEY,
+	name varchar(300)
+);
+
+INSERT INTO enum_filetype (id, name) VALUES (1, 'faktura');
+INSERT INTO enum_filetype (id, name) VALUES (2, 'bankovni vypis');
+
 CREATE TABLE files (
 	id SERIAL PRIMARY KEY,
 	name varchar(300) NOT NULL,
 	path varchar(300) NOT NULL,
 	mimetype varchar(100) NOT NULL DEFAULT 'application/octet-stream',
+	filetype smallint REFERENCES enum_filetype(id),
 	crdate timestamp NOT NULL DEFAULT now(),
 	filesize integer NOT NULL
 );
