74de3477ce8169bd9db8152210b391eba3293bc0
Petr Blaha <petr.blaha@nic.cz>
2007-02-26 12:53:07 +0000

    definovan typ notifikace dle template e-mailu
    git-svn-id: file:///data/svn/enum/db/trunk@2670 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- notify.sql
+++ notify.sql
@@ -1,12 +1,13 @@
 -- Zasilani zprav o zmene stavu objektu
 
+
 CREATE TABLE  object_status_notifications 
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 CrDate  timestamp NOT NULL DEFAULT now(),  
-type  integer  , -- typ  (expiration 1 , validation 2 , vyrazeni ze zony 3 )
+mail_type  integer   REFERENCES  mail_type(id)  , -- typ  notifikace dle rozesilaneho template e-mailu
 objectID integer  REFERENCES object_registry (id), -- id objektu
 historyid integer  REFERENCES  history(id) ,  --  zaznamenani stavu v jakem nejaky objekt je
 mailid integer references mail_archive(id), -- id odeslaneho mailu
-messageid integer  REFERENCES  message(id)  -- zaslana EPP zprava 
+messageid integer  REFERENCES  message(id)  -- pokud je take rozeslana EPP zprava 
 );
