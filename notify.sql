388ad9f22ca35cf66510d97d61c9b67ee685c95e
Petr Blaha <petr.blaha@nic.cz>
2007-03-02 11:12:18 +0000

    vytvoreni object_status_notifications_mail_map pro jednu notifikaci lze odeslat vice e-mailu napr vlastnik tech kontakty nssetu
    git-svn-id: file:///data/svn/enum/db/trunk@2711 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- notify.sql
+++ notify.sql
@@ -5,9 +5,14 @@ CREATE TABLE  object_status_notifications
 (
 id serial NOT NULL PRIMARY KEY, -- jednoznacny primarni klic
 CrDate  timestamp NOT NULL DEFAULT now(),  
-mail_type  integer   REFERENCES  mail_type(id)  , -- typ  notifikace dle rozesilaneho template e-mailu
 objectID integer  REFERENCES object_registry (id), -- id objektu
 historyid integer  REFERENCES  history(id) ,  --  zaznamenani stavu v jakem nejaky objekt je
-mailid integer references mail_archive(id), -- id odeslaneho mailu
 messageid integer  REFERENCES  message(id)  -- pokud je take rozeslana EPP zprava 
+);
+
+CREATE TABLE  object_status_notifications_mail_map
+(
+id INTEGER REFERENCES object_status_notifications(id) , -- odkaz do tabulky
+mail_type  integer   REFERENCES  mail_type(id)  , -- typ  email notifikace 
+mailid integer references mail_archive(id) -- id odeslaneho mailu
 );
