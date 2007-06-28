35d82a970d01962c3cb0e84b88f6ce32dae2e249
Milan Vachovec <milan.vachovec@nic.cz>
2007-06-28 10:06:28 +0000

    git-svn-id: file:///data/svn/enum/db/trunk@3386 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- admin.sql
+++ admin.sql
@@ -1,19 +1,19 @@
 CREATE TABLE "user"
 (
-  id serial NOT NULL, -- primarni klic
-  firstname varchar(20) NOT NULL, -- jmeno
-  surname varchar(40) NOT NULL, --- prijmeni
+  id serial NOT NULL, -- primary key
+  firstname varchar(20) NOT NULL, -- first name
+  surname varchar(40) NOT NULL, -- surname
   CONSTRAINT user_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE domain_blacklist
 (
-  id serial NOT NULL, -- primarni klic
+  id serial NOT NULL, -- primary key
   regexp varchar(255) NOT NULL, -- regularni vyraz ktery blokujeme
-  valid_from timestamp NOT NULL, -- oddkdy blokace plati
-  valid_to timestamp, -- do kdy blokace plati, pokud je NULL, neni omezena
-  reason varchar(255) NOT NULL, -- duvod proc je domena blokovana
-  creator int4, -- kdo zaznam vytvoril, pokud je NULL jedna se o systemovy zaznam jako soucast konfigurace systemu
+  valid_from timestamp NOT NULL, -- from when bloc is valid
+  valid_to timestamp, -- till when bloc is valid, if it is NULL, it isn't restricted
+  reason varchar(255) NOT NULL, -- reason why is domain blocked
+  creator int4, -- who created a record, if it is NULL, it's systems record as a part of systems configuration
   CONSTRAINT domain_blacklist_pkey PRIMARY KEY (id),
   CONSTRAINT domain_blacklist_creator_fkey FOREIGN KEY (creator)
       REFERENCES "user" (id) MATCH SIMPLE
