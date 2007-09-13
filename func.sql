f5ba4537376262a9642864458e98a43e7ed32041
Jaromír Talíř <jaromir.talir@nic.cz>
2007-09-13 15:34:23 +0000

    low level functions
    git-svn-id: file:///data/svn/enum/db/trunk@3930 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ func.sql
@@ -0,0 +1,42 @@
+-- 
+--  create temporary table and if temporary table already
+--  exists truncate it for immediate usage
+--
+CREATE OR REPLACE FUNCTION create_tmp_table(tname VARCHAR) 
+RETURNS VOID AS $$
+BEGIN
+ EXECUTE 'CREATE TABLE ' || tname || ' (id INTEGER PRIMARY KEY)';
+ EXCEPTION
+ WHEN DUPLICATE_TABLE THEN EXECUTE 'TRUNCATE TABLE ' || tname;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE UNIQUE INDEX object_registry_name_type_uniq 
+ON object_registry (name,type) WHERE erdate IS NULL;
+--
+-- create object and return it's id. duplicate is not raised as exception
+-- but return 0 as id instead. it expect unique index on table
+-- object_registry. cannot check enum subdomains!!!
+--
+CREATE OR REPLACE FUNCTION create_object(
+ crregid INTEGER, 
+ oname VARCHAR, 
+ otype INTEGER
+) 
+RETURNS INTEGER AS $$
+DECLARE iid INTEGER;
+BEGIN
+ iid := NEXTVAL('object_registry_id_seq');
+ INSERT INTO object_registry (id,roid,name,type,crid) 
+ VALUES (
+  iid,
+  (ARRAY['C','N','D'])[otype] || LPAD(iid,10,'0') || '-CZ' ,
+  oname,
+  otype,
+  crregid
+ );
+ RETURN iid;
+ EXCEPTION
+ WHEN UNIQUE_VIOLATION THEN RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
