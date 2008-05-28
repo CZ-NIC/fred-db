ee75943d4985dc5388cecdad18c249c09d674401
Jiří Šádek <jiri.sadek@nic.cz>
2008-05-28 17:58:50 +0000

    Database changes for conversion domain.exdate type from timestamp to date
    git-svn-id: file:///data/svn/enum/db/trunk@5467 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/1_8_2-1_9_0.sql
+++ upgrades/1_8_2-1_9_0.sql
@@ -42,3 +42,73 @@ applicable form on association pages.
                                              support <?cs var:defaults.company ?>
 ');
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (20, 20);
+
+
+--
+-- changing domain.exdate type from timestamp to date
+--
+
+-- need convert exdate to CET zone
+UPDATE domain SET exdate = exdate at time zone 'CET';
+-- drop domain_states view to allow alter table command
+DROP VIEW domain_states;
+-- alter table 
+ALTER TABLE domain ALTER COLUMN exdate TYPE date;
+-- recreate domain_states view
+CREATE VIEW domain_states AS
+SELECT
+  d.id AS object_id,
+  o.historyid AS object_hid,
+  COALESCE(osr.states,'{}') ||
+  CASE WHEN date_test(d.exdate::date,ep_ex_not.val) 
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+       THEN ARRAY[8] ELSE '{}' END ||  -- expirationWarning
+  CASE WHEN date_test(d.exdate::date,'0')                
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+       THEN ARRAY[9] ELSE '{}' END ||  -- expired
+  CASE WHEN date_time_test(d.exdate::date,ep_ex_dns.val,ep_tm.val,ep_tz.val)
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+       THEN ARRAY[10] ELSE '{}' END || -- unguarded
+  CASE WHEN date_test(e.exdate::date,ep_val_not1.val)
+       THEN ARRAY[11] ELSE '{}' END || -- validationWarning1
+  CASE WHEN date_test(e.exdate::date,ep_val_not2.val)
+       THEN ARRAY[12] ELSE '{}' END || -- validationWarning2
+  CASE WHEN date_time_test(e.exdate::date,'0',ep_tm.val,ep_tz.val)
+       THEN ARRAY[13] ELSE '{}' END || -- notValidated
+  CASE WHEN d.nsset ISNULL 
+       THEN ARRAY[14] ELSE '{}' END || -- nssetMissing
+  CASE WHEN
+    d.nsset ISNULL OR
+    5 = ANY(COALESCE(osr.states,'{}')) OR                -- outzoneManual
+    (((date_time_test(d.exdate::date,ep_ex_dns.val,ep_tm.val,ep_tz.val)
+       AND NOT (2 = ANY(COALESCE(osr.states,'{}')))      -- !renewProhibited
+      ) OR date_time_test(e.exdate::date,'0',ep_tm.val,ep_tz.val)) AND 
+     NOT (6 = ANY(COALESCE(osr.states,'{}'))))           -- !inzoneManual
+       THEN ARRAY[15] ELSE '{}' END || -- outzone
+  CASE WHEN date_time_test(d.exdate::date,ep_ex_reg.val,ep_tm.val,ep_tz.val)
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+            AND NOT (1 = ANY(COALESCE(osr.states,'{}'))) -- !deleteProhibited
+       THEN ARRAY[17] ELSE '{}' END || -- deleteCandidate
+  CASE WHEN date_test(d.exdate::date,ep_ex_let.val)
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+       THEN ARRAY[19] ELSE '{}' END || -- deleteWarning
+  CASE WHEN date_time_test(d.exdate::date,ep_ex_dns.val,ep_tm.val,ep_tz.val)
+            AND NOT (2 = ANY(COALESCE(osr.states,'{}'))) -- !renewProhibited
+            AND NOT (6 = ANY(COALESCE(osr.states,'{}'))) -- !inzoneManual
+       THEN ARRAY[20] ELSE '{}' END    -- outzoneUnguarded
+  AS states
+FROM
+  object_registry o,
+  domain d
+  LEFT JOIN enumval e ON (d.id=e.domainid)
+  LEFT JOIN object_state_request_now osr ON (d.id=osr.object_id)
+  JOIN enum_parameters ep_ex_not ON (ep_ex_not.id=3)
+  JOIN enum_parameters ep_ex_dns ON (ep_ex_dns.id=4)
+  JOIN enum_parameters ep_ex_let ON (ep_ex_let.id=5)
+  JOIN enum_parameters ep_ex_reg ON (ep_ex_reg.id=6)
+  JOIN enum_parameters ep_val_not1 ON (ep_val_not1.id=7)
+  JOIN enum_parameters ep_val_not2 ON (ep_val_not2.id=8)
+  JOIN enum_parameters ep_tm ON (ep_tm.id=9)
+  JOIN enum_parameters ep_tz ON (ep_tz.id=10)
+WHERE d.id=o.id;
+
