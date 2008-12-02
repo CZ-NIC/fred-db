972c6d715a65c52ab99f3ba569bfbadbee282acc
Juraj Viceník <juraj.vicenik@nic.cz>
2008-12-02 09:16:17 +0000

    Changes to logging system, Ticket #1673
    git-svn-id: file:///data/svn/enum/db/trunk@6883 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/structure_log.sql
+++ sql/structure_log.sql
@@ -1,22 +1,38 @@
 CREATE TABLE log_entry (
 	id SERIAL PRIMARY KEY,
-	time timestamp NOT NULL,	-- TODO what type?
-	source_ip varchar(15) NOT NULL,
-	flag integer NOT NULL,		-- enum type
-	component integer NOT NULL,	-- enum type
-	content varchar(2000) NOT NULL -- TODO size
-	);
-	
-CREATE TABLE log_property (
-	id SERIAL PRIMARY KEY,
-	entry_id integer NOT NULL REFERENCES log_entry,
+	time_begin timestamp NOT NULL,	-- begin of the transaction
+	time_end timestamp,		-- end of transaction, it is set if the information is complete 
+					-- e.g. if an error message from backend is successfully logged, it's still set	
+					-- NULL in cases like crash of the server
+	source_ip INET NOT NULL,
+	service integer NOT NULL	-- where does the event come from
+);
 
-	name varchar(30) NOT NULL,	-- property name
-	value varchar(1024)		-- property value
+CREATE TABLE log_raw_content (
+	entry_id integer NOT NULL REFERENCES log_entry(id),
+	request varchar(2000),
+	response varchar(2000)
+);
+
+CREATE TABLE log_property_name (
+	id SERIAL PRIMARY KEY,
+	name varchar(30) NOT NULL
 );
 	
-CREATE INDEX log_entry_time_idx ON log_entry(time);
+CREATE TABLE log_property_value (
+	id SERIAL PRIMARY KEY,
+	entry_id integer NOT NULL REFERENCES log_entry(id),
+
+	name_id integer NOT NULL REFERENCES log_property_name(id),
+	value varchar(1024) NOT NULL,		-- property value
+	output boolean DEFAULT False		-- whether it's output (response) property; if False it's input (request)
+);
+	
+CREATE INDEX log_entry_time_begin_idx ON log_entry(time_begin);
+CREATE INDEX log_entry_time_end_idx ON log_entry(time_end);
 CREATE INDEX log_entry_source_ip_idx ON log_entry(source_ip);
-CREATE INDEX log_entry_client_id_idx ON log_entry(client_id);
-CREATE INDEX log_property_value_idx ON log_property(value); 
-CREATE INDEX log_property_name_idx ON log_property(name); 
+CREATE INDEX log_entry_service_idx ON log_entry(service);
+
+CREATE INDEX log_property_value_idx ON log_property_value(value); 
+CREATE INDEX log_property_name_idx ON log_property_name(name); 
+
