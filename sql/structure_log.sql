2aecd74742e982fd47a1d9e00f4ba46091f8dbe9
Juraj Viceník <juraj.vicenik@nic.cz>
2009-01-14 10:00:46 +0000

    UPDATE: fred-logd: child properties support, log_raw_content table changed
    git-svn-id: file:///data/svn/enum/db/trunk@7041 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/structure_log.sql
+++ sql/structure_log.sql
@@ -4,14 +4,14 @@ CREATE TABLE log_entry (
 	time_end timestamp,		-- end of transaction, it is set if the information is complete 
 					-- e.g. if an error message from backend is successfully logged, it's still set	
 					-- NULL in cases like crash of the server
-	source_ip INET NOT NULL,
+	source_ip INET,
 	service integer NOT NULL	-- where does the event come from
 );
 
 CREATE TABLE log_raw_content (
 	entry_id integer NOT NULL REFERENCES log_entry(id),
-	request varchar(2000),
-	response varchar(2000)
+	content varchar(2000) NOT NULL,
+	is_response boolean DEFAULT False -- true if the content is response, false if it's request
 );
 
 CREATE TABLE log_property_name (
@@ -25,14 +25,26 @@ CREATE TABLE log_property_value (
 
 	name_id integer NOT NULL REFERENCES log_property_name(id),
 	value varchar(1024) NOT NULL,		-- property value
-	output boolean DEFAULT False		-- whether it's output (response) property; if False it's input (request)
+	output boolean DEFAULT False,		-- whether it's output (response) property; if False it's input (request)
+
+	parent_id integer REFERENCES log_property_value(id)
+						-- in case of child property, the id of the parent, NULL otherwise
 );
-	
+
 CREATE INDEX log_entry_time_begin_idx ON log_entry(time_begin);
 CREATE INDEX log_entry_time_end_idx ON log_entry(time_end);
 CREATE INDEX log_entry_source_ip_idx ON log_entry(source_ip);
 CREATE INDEX log_entry_service_idx ON log_entry(service);
 
-CREATE INDEX log_property_value_idx ON log_property_value(value); 
-CREATE INDEX log_property_name_idx ON log_property_name(name); 
+CREATE INDEX log_raw_content_entry_id_idx ON log_raw_content(entry_id);
+CREATE INDEX log_raw_content_content_idx ON log_raw_content(content);
+CREATE INDEX log_raw_content_is_response_idx ON log_raw_content(is_response);
+
+CREATE INDEX log_property_name_name_idx ON log_property_name(name); 
+
+CREATE INDEX log_property_value_entry_id_idx ON log_property_value(entry_id); 
+CREATE INDEX log_property_value_name_id_idx ON log_property_value(name_id); 
+CREATE INDEX log_property_value_value_idx ON log_property_value(value); 
+CREATE INDEX log_property_value_output_idx ON log_property_value(output); 
+CREATE INDEX log_property_value_parent_id_idx ON log_property_value(parent_id); 
 
