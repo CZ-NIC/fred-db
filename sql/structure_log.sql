9bf4eaa405a0bd2763bf8e078b2939ce8cc92d5e
Juraj Viceník <juraj.vicenik@nic.cz>
2009-03-26 14:35:16 +0000

    UPDATE: Fist testing partitioning implementation, also fields action_type and is_monitoring added to log_entry
    git-svn-id: file:///data/svn/enum/db/trunk@7583 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/structure_log.sql
+++ sql/structure_log.sql
@@ -1,15 +1,24 @@
 CREATE TABLE log_entry (
+    CONSTRAINT log_entry_no_insert_root CHECK (false),   -- constraint for partitioned table
+    
 	id SERIAL PRIMARY KEY,
 	time_begin timestamp NOT NULL,	-- begin of the transaction
 	time_end timestamp,		-- end of transaction, it is set if the information is complete 
 					-- e.g. if an error message from backend is successfully logged, it's still set	
 					-- NULL in cases like crash of the server
 	source_ip INET,
-	service integer NOT NULL,	-- where does the event come from
-	action_type integer REFERENCES log_action_type(id)
+	service integer NOT NULL,	-- service code - enum LogServiceType
+	action_type integer REFERENCES log_action_type(id),
+		
+	is_monitoring boolean NOT NULL
 );
 
 CREATE TABLE log_raw_content (
+	CONSTRAINT log_raw_content_no_insert_root CHECK (false), -- constraint for partitioned table
+
+	entry_time_begin timestamp NOT NULL, -- TEMP: for partitioning
+
+
 	entry_id integer NOT NULL REFERENCES log_entry(id),
 	content text NOT NULL,
 	is_response boolean DEFAULT False -- true if the content is response, false if it's request
@@ -21,6 +30,10 @@ CREATE TABLE log_property_name (
 );
 	
 CREATE TABLE log_property_value (
+	CONSTRAINT log_property_value_no_insert_root CHECK (false),    -- constraint for partitioned table
+
+	entry_time_begin timestamp NOT NULL, -- TEMP: for partitioning
+	
 	id SERIAL PRIMARY KEY,
 	entry_id integer NOT NULL REFERENCES log_entry(id),
 
@@ -33,6 +46,8 @@ CREATE TABLE log_property_value (
 );
 
 CREATE TABLE log_session (
+	CONSTRAINT log_property_value_no_insert_root CHECK (false),    -- constraint for partitioned table
+
 	id serial primary key,
 	name varchar(255) not null,
 	login_date timestamp not null default now(), 
@@ -47,13 +62,17 @@ CREATE INDEX log_entry_time_begin_idx ON log_entry(time_begin);
 CREATE INDEX log_entry_time_end_idx ON log_entry(time_end);
 CREATE INDEX log_entry_source_ip_idx ON log_entry(source_ip);
 CREATE INDEX log_entry_service_idx ON log_entry(service);
+CREATE INDEX log_entry_action_type_idx ON log_entry(action_type);
+CREATE INDEX log_entry_monitoring_idx ON log_entry(is_monitoring);
 
+CREATE INDEX log_raw_content_entry_time_begin_idx ON log_raw_content(entry_time_begin);
 CREATE INDEX log_raw_content_entry_id_idx ON log_raw_content(entry_id);
 CREATE INDEX log_raw_content_content_idx ON log_raw_content(content);
 CREATE INDEX log_raw_content_is_response_idx ON log_raw_content(is_response);
 
 CREATE INDEX log_property_name_name_idx ON log_property_name(name); 
 
+CREATE INDEX log_property_value_entry_time_begin_idx ON log_property_value(entry_time_begin);
 CREATE INDEX log_property_value_entry_id_idx ON log_property_value(entry_id); 
 CREATE INDEX log_property_value_name_id_idx ON log_property_value(name_id); 
 CREATE INDEX log_property_value_value_idx ON log_property_value(value); 
@@ -66,11 +85,11 @@ CREATE TABLE log_action_type (
         );
 
 COMMENT ON TABLE log_action_type IS 
-'List of action which can be done using epp communication over central registry
+'List of requests which can be used by clients
 
 id  - status
 100 - ClientLogin
-101 - ClientLogin
+101 - ClientLogout
 120 - PollAcknowledgement
 121 - PollResponse
 200 - ContactCheck
