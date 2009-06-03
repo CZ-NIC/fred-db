c4e9aed90a2714e14487260b5074e3ea06dc725c
Jiří Šádek <jiri.sadek@nic.cz>
2009-06-03 13:59:58 +0000

    FIX: ticket #1959 authinfo mail template update
    git-svn-id: file:///data/svn/enum/db/trunk@8178 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upgrades/2_1_3-2_1_4.sql
@@ -0,0 +1,55 @@
+---
+--- dont forget to update database schema version
+---
+UPDATE enum_parameters SET val = '2.1.4' WHERE id = 1;
+
+
+---
+--- ticket #1959 - fix authinfo template
+---
+UPDATE mail_templates SET template = 
+' English version of the e-mail is entered below the Czech version
+
+Zaslání autorizační informace
+
+Vážený zákazníku,
+
+   na základě Vaší žádosti, podané prostřednictvím registrátora
+<?cs var:registrar ?>, Vám zasíláme požadované heslo
+příslušející <?cs if:type == #3 ?>k doméně<?cs elif:type == #1 ?>ke kontaktu s identifikátorem<?cs elif:type == #2 ?>k sadě nameserverů s identifikátorem<?cs elif:type == #4 ?>k sadě klíčů s identifikátorem<?cs /if ?> <?cs var:handle ?>.
+
+   Heslo je: <?cs var:authinfo ?>
+
+   Tato zpráva je zaslána pouze na e-mailovou adresu uvedenou u příslušné
+osoby v Centrálním registru doménových jmen.
+
+   V případě, že jste tuto žádost nepodali, oznamte prosím tuto
+skutečnost na adresu <?cs var:defaults.emailsupport ?>.
+
+
+                                             S pozdravem
+                                             podpora <?cs var:defaults.company ?>
+
+
+
+Sending authorization information
+
+Dear customer,
+
+   Based on your request submitted via the registrar <?cs var:registrar ?>,
+we are sending the requested password that belongs to
+the <?cs if:type == #3 ?>domain name<?cs elif:type == #1 ?>contact with identifier<?cs elif:type == #2 ?>NS set with identifier<?cs elif:type == #4 ?>Keyset with identifier<?cs /if ?> <?cs var:handle ?>.
+
+   The password is: <?cs var:authinfo ?>
+
+   This message is being sent only to the e-mail address that we have on file
+for a particular person in the Central Registry of Domain Names.
+
+   If you did not submit the aforementioned request, please, notify us about
+this fact at the following address <?cs var:defaults.emailsupport ?>.
+
+
+                                             Yours sincerely
+                                             support <?cs var:defaults.company ?>'
+WHERE id = 2;
+
