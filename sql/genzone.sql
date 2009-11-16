8d4067cd509a2bd64f3086cd91a9f8f87d13fd14
Jan Zima <jan.zima@nic.cz>
2009-11-16 09:30:47 +0000

    Comments on zone tables and columns , Ticket #2095
    git-svn-id: file:///data/svn/enum/db/trunk@9467 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/genzone.sql
+++ sql/genzone.sql
@@ -1,27 +1,46 @@
 -- table holding data from SOA record for a zone
 CREATE TABLE zone_soa (
-        Zone INTEGER PRIMARY KEY REFERENCES Zone (ID),
-        TTL INTEGER NOT NULL,
-        Hostmaster VARCHAR(255) NOT NULL,
-        Serial INTEGER,
-        Refresh INTEGER NOT NULL,
-        Update_retr INTEGER NOT NULL, 
-        Expiry INTEGER NOT NULL,
-        Minimum INTEGER NOT NULL,
-        ns_fqdn VARCHAR(255) NOT NULL
+        Zone INTEGER PRIMARY KEY REFERENCES Zone (ID),  --default period of validity of records in the zone in seconds
+        TTL INTEGER NOT NULL,  --default period of validity of records in the zone in seconds
+        Hostmaster VARCHAR(255) NOT NULL,  --responsible person email (in format: user@domain.tld )
+        Serial INTEGER,  --serial number incremented on change in the form YYYYMMDDnn (year, month, date, revision)
+        Refresh INTEGER NOT NULL,  --secondary nameservers copy of zone refresh interval in seconds
+        Update_retr INTEGER NOT NULL,  --retry interval of secondary nameservers zone update (in case of failed zone refresh) in seconds
+        Expiry INTEGER NOT NULL,  --zone expiration period for secondary nameservers in seconds
+        Minimum INTEGER NOT NULL,  --the time a NAME ERROR = NXDOMAIN result may be cached by any resolver in seconds
+        ns_fqdn VARCHAR(255) NOT NULL  --primary nameserver fully qualified name
         );
+
+comment on table zone_soa is
+'Table holding data from SOA record for a zone';
+comment on column zone_soa.zone is 'zone id';
+comment on column zone_soa.ttl is 'default period of validity of records in the zone in seconds';
+comment on column zone_soa.hostmaster is 'responsible person email (in format: user@domain.tld )';
+comment on column zone_soa.serial is 'serial number incremented on change in the form YYYYMMDDnn (year, month, date, revision)';
+comment on column zone_soa.refresh is 'secondary nameservers copy of zone refresh interval in seconds';
+comment on column zone_soa.update_retr is 'retry interval of secondary nameservers zone update (in case of failed zone refresh) in seconds';
+comment on column zone_soa.expiry is 'zone expiration period for secondary nameservers in seconds';
+comment on column zone_soa.minimum is 'the time a NAME ERROR = NXDOMAIN result may be cached by any resolver in seconds';
+comment on column zone_soa.ns_fqdn is 'primary nameserver fully qualified name';
 
 -- Nameservers for a zone
 CREATE TABLE zone_ns (
-        id SERIAL PRIMARY KEY,
-        Zone INTEGER REFERENCES Zone (ID),
-        fqdn VARCHAR(255) NOT NULL,
-        addrs INET[] NOT NULL
+        id SERIAL PRIMARY KEY,  --unique automatically generated identifier
+        Zone INTEGER REFERENCES Zone (ID),  --zone id
+        fqdn VARCHAR(255) NOT NULL,  --nameserver fully qualified name
+        addrs INET[] NOT NULL  --nameserver ip addresses array
         );
+
+comment on table zone_ns is
+'This table contains nameservers for a zone';
+comment on column zone_ns.id is 'unique automatically generated identifier';
+comment on column zone_ns.zone is 'zone id';
+comment on column zone_ns.fqdn is 'nameserver fully qualified name';
+comment on column zone_ns.fqdn is 'nameserver ip addresses array';
 
 -- List of status for domain zone generator classification
 -- supplement missing enum type in postgresql
-CREATE TABLE genzone_domain_status (
+CREATE TABLE genzone_domain_status (  --deprecated, unused, prepared for removal
 	id integer PRIMARY KEY, -- id of status
 	name char(20) NOT NULL -- decriptive name
 );
@@ -33,7 +52,8 @@ INSERT INTO genzone_domain_status VALUES (4,'expired');
 INSERT INTO genzone_domain_status VALUES (5,'is not validated');
 
 comment on table genzone_domain_status is
-'List of status for domain zone generator classification
+'deprecated, unused, prepared for removal
+List of status for domain zone generator classification
 
 id - name
  1 - domain is in zone
@@ -43,7 +63,7 @@ id - name
  5 - domain is not validated';
 
 -- History of generation of domain in zone file
-CREATE TABLE genzone_domain_history (
+CREATE TABLE genzone_domain_history ( --deprecated, unused, prepared for removal
     -- id of record
     id SERIAL PRIMARY KEY,
     -- domain  
@@ -66,3 +86,6 @@ CREATE INDEX genzone_domain_history_domain_hid_idx
   ON genzone_domain_history (domain_hid);
 CREATE INDEX genzone_domain_history_domain_id_idx 
   ON genzone_domain_history (domain_id);
+
+comment on table genzone_domain_history is
+'deprecated, unused, prepared for removal';
--- sql/zone.sql
+++ sql/zone.sql
@@ -1,11 +1,21 @@
 -- DROP TABLE zone CASCADE;
 CREATE TABLE zone (
         id SERIAL PRIMARY KEY,
-        fqdn VARCHAR(255) UNIQUE NOT NULL,
-        ex_period_min int not null,
-        ex_period_max int not null,
-        val_period int not null,
-        dots_max  int not null default 1,
-        enum_zone boolean default 'f'
+        fqdn VARCHAR(255) UNIQUE NOT NULL,  --zone fully qualified name
+        ex_period_min int not null,  --minimal prolongation of the period of domains validity in months
+        ex_period_max int not null,  --maximal prolongation of the period of domains validity in months
+        val_period int not null,  --enum domains revalidation period in months
+        dots_max  int not null default 1,  --maximal number of dots in zone name
+        enum_zone boolean default 'f'  --flag if zone is for enum
         );
+
+comment on table zone is
+'This table contains zone parameters';
+comment on column zone.id is 'unique automatically generated identifier';
+comment on column zone.fqdn is 'zone fully qualified name';
+comment on column zone.ex_period_min is 'minimal prolongation of the period of domains validity in months';
+comment on column zone.ex_period_max is 'maximal prolongation of the period of domains validity in months';
+comment on column zone.val_period is 'enum domains revalidation period in months';
+comment on column zone.dots_max is 'maximal number of dots in zone name';
+comment on column zone.enum_zone is 'flag if zone is for enum';
 
