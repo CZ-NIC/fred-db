7dbdcf2ebc90d1b0468bdbb962e94e6d10a33150
Jiří Šádek <jiri.sadek@nic.cz>
2009-12-16 12:57:48 +0000

    merged r9835 from branche 2.2
    git-svn-id: file:///data/svn/enum/db/trunk@9836 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/public_request.sql
+++ sql/public_request.sql
@@ -4,25 +4,25 @@ CREATE TABLE public_request (
   id serial NOT NULL PRIMARY KEY,
   request_type smallint NOT NULL, -- vsechny typy zadosti
   epp_action_id integer REFERENCES action(id),
-  registrar_id integer REFERENCES registrar(id),
   create_time timestamp without time zone DEFAULT now() NOT NULL,
   status smallint DEFAULT 1 NOT NULL,
   resolve_time timestamp without time zone,
   reason character varying(512),
   email_to_answer character varying(255),
-  answer_email_id integer REFERENCES mail_archive(id)
+  answer_email_id integer REFERENCES mail_archive(id),
+  registrar_id integer REFERENCES registrar(id)
 );
 
 comment on table public_request is 'table of general requests give in by public users';
 comment on column public_request.request_type is 'code of request';
 comment on column public_request.epp_action_id is 'reference on action when request is submitted by registrar via EPP protocol (otherwise NULL)';
-comment on column public_request.registrar_id is 'reference to registrar when request is submitted via EPP protocol (otherwise NULL)';
 comment on column public_request.create_time is 'request creation time';
 comment on column public_request.status is 'code of request actual status';
 comment on column public_request.resolve_time is 'time when request was processed (closed)';
 comment on column public_request.reason is 'reason';
 comment on column public_request.email_to_answer is 'manual entered email by user for sending answer (if it is automatic from object contact it is NULL)';
 comment on column public_request.answer_email_id is 'reference to mail which was send after request was processed';
+comment on column public_request.registrar_id is 'reference to registrar when request is submitted via EPP protocol (otherwise NULL)';
 
 CREATE TABLE public_request_objects_map (
   request_id integer REFERENCES public_request(id) PRIMARY KEY,
