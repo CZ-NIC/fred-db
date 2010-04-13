4ba19c85fbab28b976cd2d3f50feaf18973ceca1
Jan Zima <jan.zima@nic.cz>
2010-04-13 15:34:05 +0000

    check whether registrar_group is empty and not cancelled, Ticket #3747
    git-svn-id: file:///data/svn/enum/db/trunk@11286 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-registrar_certification.sql
+++ upgrades/NEXT-registrar_certification.sql
@@ -7,6 +7,11 @@ UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
 --- Ticket #3747
 ---
 
+--drop TABLE registrar_group_map;
+--drop TABLE registrar_group;
+--drop TABLE registrar_certification;
+--drop domain classification_type;
+
 CREATE DOMAIN classification_type AS integer NOT NULL
 	CONSTRAINT classification_type_check CHECK (VALUE IN (0, 1, 2, 3, 4, 5)); 
 
@@ -43,6 +48,32 @@ CREATE TABLE registrar_group
     short_name varchar(255) NOT NULL UNIQUE, -- short name of the group
     cancelled timestamp -- when the group was cancelled
 );
+
+
+--check whether registrar_group is empty and not cancelled
+CREATE OR REPLACE FUNCTION cancel_registrar_group_check() RETURNS "trigger" AS $$
+DECLARE
+    registrars_in_group INTEGER;
+BEGIN
+    IF (OLD.cancelled is not null) THEN
+        RAISE EXCEPTION 'Registrar group already cancelled';
+    END IF;
+    SELECT count(*) INTO registrars_in_group 
+        FROM registrar_group_map WHERE registrar_group_id = NEW.id;
+    IF (registrars_in_group > 0 AND NEW.cancelled is not null) THEN
+        RAISE EXCEPTION 'Unable to cancel non-empty registrar_group';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+COMMENT ON FUNCTION cancel_registrar_group_check()
+	IS 'check whether registrar_group is empty and not cancelled'; 
+
+CREATE TRIGGER "trigger_cancel_registrar_group"
+  BEFORE UPDATE  ON registrar_group
+  FOR EACH ROW EXECUTE PROCEDURE cancel_registrar_group_check();
+
 
 CREATE INDEX registrar_group_short_name_idx ON registrar_group(short_name);
 
