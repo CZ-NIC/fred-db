d100ef0f9bfd68a7694986c786b0e31f8bf1bc33
Jiří Šádek <jiri.sadek@nic.cz>
2010-05-05 08:03:22 +0000

    Ticket #3797 - upgrade script
    git-svn-id: file:///data/svn/enum/db/trunk@11525 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upgrades/NEXT-general_email_notify_dns_inactivation.sql
@@ -0,0 +1,8 @@
+---
+--- Ticket #3797
+---
+
+-- state: outzoneUnguarded, obj: domain, 
+-- template: expiration_dns_owner, emails: generic emails (like kontakt@... postmaster@... info@...)
+INSERT INTO notify_statechange_map VALUES (12, 20, 3, 4, 3);
+
