500030ed5224df43ab9792a3c9e58545065e389a
Jan Zima <jan.zima@nic.cz>
2010-06-17 14:51:20 +0000

    simpler query #4113
    git-svn-id: file:///data/svn/enum/db/trunk@12282 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-registrar_group_initial_membership_dml.sql
+++ upgrades/NEXT-registrar_group_initial_membership_dml.sql
@@ -8,24 +8,13 @@ UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
 ---
 
 INSERT INTO registrar_group_map (registrar_id, registrar_group_id, member_from)
-SELECT DISTINCT registrarid AS registrar_id
-    , (SELECT id 
-         FROM registrar_group 
-        WHERE short_name = 'uncertified'
-      ) AS registrar_group_id
-    , CURRENT_DATE AS member_from
-  FROM (SELECT r.id AS registrarid
-        , z.id AS zoneid
-        , z.fqdn AS zone_fqdn
-        , (SELECT count(*) 
-             FROM registrarinvoice ri 
-            WHERE fromdate <= CURRENT_DATE
-                AND (todate >= CURRENT_DATE OR todate IS null)
-                AND ri.registrarid = r.id 
-                AND ri.zone = z.id
-          ) AS isinzone
-          FROM registrar AS r , zone AS z
-       ) AS rii
- WHERE isinzone > 0 
-   AND (zone_fqdn = 'cz' OR zone_fqdn = '0.2.4.e164.arpa');
-
+SELECT DISTINCT registrarid,
+       (SELECT id FROM registrar_group WHERE short_name = 'uncertified'),
+       CURRENT_DATE 
+  FROM (SELECT r.id AS registrarid 
+          FROM registrar r 
+               JOIN registrarinvoice ri ON ri.registrarid = r.id 
+               JOIN zone z ON z.id = ri.zone 
+         WHERE ri.fromdate <= CURRENT_DATE 
+               AND (todate >= CURRENT_DATE OR todate IS NULL) 
+               AND z.fqdn = 'cz' OR z.fqdn = '0.2.4.e164.arpa') AS riz;
