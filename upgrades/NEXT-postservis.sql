9ac8374a376d71bfd96ee9250f6cc94f339d89ec
Juraj Viceník <juraj.vicenik@nic.cz>
2010-06-25 09:44:34 +0000

    Ticket #4114 - upgrade script for converting data to new tables
    git-svn-id: file:///data/svn/enum/db/trunk@12479 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-postservis.sql
+++ upgrades/NEXT-postservis.sql
@@ -25,27 +25,36 @@ CREATE TABLE letter_archive (
 );
 
 comment on table letter_archive is 'letters sent electronically as PDF documents to postal service, address is included in the document';
-comment on table letter_archive.status is 'initial (default) status is ''file generated & ready for processing'' ';
-comment on table letter_archive.file_id is 'file with pdf about notification (null for old)';
-comment on table letter_archive.crdate is 'date of insertion in table';
-comment on table letter_archive.moddate is 'date of sending (even if unsuccesfull), it is the time when the send attempt finished';
-comment on table letter_archive.attempt is 'failed attempts to send data';
+comment on column letter_archive.status is 'initial (default) status is ''file generated & ready for processing'' ';
+comment on column letter_archive.file_id is 'file with pdf about notification (null for old)';
+comment on column letter_archive.crdate is 'date of insertion in table';
+comment on column letter_archive.moddate is 'date of sending (even if unsuccesfull), it is the time when the send attempt finished';
+comment on column letter_archive.attempt is 'failed attempts to send data';
 
 -- TODO upgrade script to fill data into letter_archive and evacuate file_id in notify_letters which should be dropped
 
+INSERT INTO letter_archive (status , file_id) SELECT 5, file_id FROM notify_letters;
 
-ALTER TABLE notify_letters DROP COLUMN file_id;
-
-ALTER TABLE notify_letters ADD COLUMN status INTEGER NOT NULL DEFAULT 1;
 ALTER TABLE notify_letters ADD COLUMN contact_history_id INTEGER;
 -- there has to be some foreign key declared - but not this
 ALTER TABLE notify_letters ADD FOREIGN KEY (contact_history_id) REFERENCES contact_history(historyid);
 ALTER TABLE notify_letters ADD COLUMN letter_id INTEGER;
 ALTER TABLE notify_letters ADD FOREIGN KEY (letter_id) REFERENCES letter_archive(id);
 
+UPDATE notify_letters nl SET letter_id = la.id FROM letter_archive la WHERE la.file_id = nl.file_id;
+
+
+UPDATE notify_letters nl SET contact_history_id = h.id
+    FROM object_state os 
+        join domain_history dh on os.ohid_from = dh.historyid 
+        join contact_history ch on ch.id = dh.registrant
+        join history h on h.id=ch.historyid and os.valid_from >= h.valid_from and os.valid_from < h.valid_to
+        where os.id = nl.state_id;
+
+ALTER TABLE notify_letters DROP COLUMN file_id;
+
 comment on column notify_letters.contact_history_id is 'which contact is the file sent to';
 comment on column notify_letters.letter_id is 'which message notifies the state change';
 
+CREATE INDEX notify_letters_contact_id_idx ON notify_letters(contact_history_id);
 
-CREATE INDEX notify_letters_status_idx ON notify_letters (status);
-CREATE INDEX notify_letters_contact_id_idx ON notify_letters(contact_id);
