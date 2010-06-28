e0103f4a089f4582b5e79c7e6347b0b3668f0780
Jiří Šádek <jiri.sadek@nic.cz>
2010-06-28 13:51:29 +0000

    will not be applied
    git-svn-id: file:///data/svn/enum/db/trunk@12526 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-bank_payment.sql
+++
@@ -1,4 +0,0 @@
--- set type of account_name and account_memo to character varying(63) #3946
-ALTER TABLE bank_payment ALTER COLUMN account_name TYPE character varying (63);
-ALTER TABLE bank_payment ALTER COLUMN account_memo TYPE character varying (63);
-
