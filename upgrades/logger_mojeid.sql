ba7bb3338c02ea01a2aa59f8a2321fc7c4c376d1
Jiří Šádek <jiri.sadek@nic.cz>
2010-08-04 08:42:05 +0000

    fixed due to renaming
    git-svn-id: file:///data/svn/enum/db/trunk@13163 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/logger_mojeid.sql
+++ upgrades/logger_mojeid.sql
@@ -1,15 +1,15 @@
 INSERT INTO service (id, partition_postfix, name) VALUES (6, 'mojeid_', 'MojeID');
 
 -- Intranet functions
-INSERT INTO request_type (id, status, service) VALUES (1500, 'OpenIDRequest', 6);
-INSERT INTO request_type (id, status, service) VALUES (1501, 'Login', 6);
-INSERT INTO request_type (id, status, service) VALUES (1502, 'Logout', 6);
-INSERT INTO request_type (id, status, service) VALUES (1503, 'UserCreate', 6);
-INSERT INTO request_type (id, status, service) VALUES (1504, 'UserUpdate', 6);
-INSERT INTO request_type (id, status, service) VALUES (1505, 'PasswordChange', 6);
-INSERT INTO request_type (id, status, service) VALUES (1506, 'CertificateChange', 6);
-INSERT INTO request_type (id, status, service) VALUES (1507, 'PasswordResetRequest', 6);
-INSERT INTO request_type (id, status, service) VALUES (1508, 'PasswordReset', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1500, 'OpenIDRequest', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1501, 'Login', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1502, 'Logout', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1503, 'UserCreate', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1504, 'UserUpdate', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1505, 'PasswordChange', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1506, 'CertificateChange', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1507, 'PasswordResetRequest', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1508, 'PasswordReset', 6);
 
 
 SELECT setval('request_type_id_seq', (SELECT MAX(id) FROM request_type));
