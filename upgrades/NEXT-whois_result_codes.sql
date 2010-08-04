d6d921040928eb5e5174a7f62c39ce7955306c59
Jan Zima <jan.zima@nic.cz>
2010-08-04 15:22:16 +0000

    mod-whoisd result codes #4340
    git-svn-id: file:///data/svn/enum/db/trunk@13182 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upgrades/NEXT-whois_result_codes.sql
@@ -0,0 +1,18 @@
+---
+--- don't forget to update database schema version
+---
+
+UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
+
+
+---
+--- #4340
+--- 
+---
+
+-- mod-whoisd result codes
+INSERT INTO result_code (service_id, result_code, name) VALUES (0, 101 , 'NoEntriesFound');
+INSERT INTO result_code (service_id, result_code, name) VALUES (0, 107 , 'UsageError');
+INSERT INTO result_code (service_id, result_code, name) VALUES (0, 108 , 'InvalidRequest');
+INSERT INTO result_code (service_id, result_code, name) VALUES (0, 501 , 'InternalServerError');
+
