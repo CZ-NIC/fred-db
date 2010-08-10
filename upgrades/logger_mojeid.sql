8dbd810d7de27bfb195a889ac0708e1b2983f1b3
Tomáš Diviš <tomas.divis@nic.cz>
2010-08-10 12:01:54 +0000

    Unified change operation names to Change (was somewhere Update somewhere Change)
    git-svn-id: file:///data/svn/enum/db/trunk@13274 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/logger_mojeid.sql
+++ upgrades/logger_mojeid.sql
@@ -9,12 +9,12 @@ INSERT INTO request_type (id, name, service_id) VALUES (1500, 'OpenIDRequest', 6
 INSERT INTO request_type (id, name, service_id) VALUES (1501, 'Login', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1502, 'Logout', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1503, 'UserCreate', 6);
-INSERT INTO request_type (id, name, service_id) VALUES (1504, 'UserUpdate', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1504, 'UserChange', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1505, 'PasswordChange', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1506, 'CertificateChange', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1507, 'PasswordResetRequest', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1508, 'PasswordReset', 6);
-INSERT INTO request_type (id, name, service_id) VALUES (1509, 'TrustUpdate', 6);
+INSERT INTO request_type (id, name, service_id) VALUES (1509, 'TrustChange', 6);
 
 
 SELECT setval('request_type_id_seq', (SELECT MAX(id) FROM request_type));
