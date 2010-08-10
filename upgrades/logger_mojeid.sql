8179db005482dc8d48b98e7e8593289fe79a0b26
Zdeněk Böhm <zdenek.bohm@nic.cz>
2010-08-10 08:52:23 +0000

    #4278 add note about translation
    git-svn-id: file:///data/svn/enum/db/trunk@13270 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/logger_mojeid.sql
+++ upgrades/logger_mojeid.sql
@@ -1,6 +1,10 @@
 INSERT INTO service (id, partition_postfix, name) VALUES (6, 'mojeid_', 'MojeID');
 
 -- Intranet functions
+
+-- Important! If you add an item here, do not forget to add a translation in 
+--      enum/nicms/base/trunk/apps/nicommon/logger/history_reader.py: request_type_codes
+
 INSERT INTO request_type (id, name, service_id) VALUES (1500, 'OpenIDRequest', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1501, 'Login', 6);
 INSERT INTO request_type (id, name, service_id) VALUES (1502, 'Logout', 6);
