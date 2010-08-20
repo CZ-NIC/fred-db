a0703efbb20411d3ac71050d0cd348a4d0eeb1d0
Tomáš Diviš <tomas.divis@nic.cz>
2010-08-20 13:24:00 +0000

    Added NEXT-logger_webadmin.sql
    git-svn-id: file:///data/svn/enum/db/trunk@13422 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upgrades/NEXT-logger_webadmin.sql
@@ -0,0 +1,3 @@
+INSERT INTO result_code (service_id, result_code, name) VALUES (4, 1 , 'Success');
+INSERT INTO result_code (service_id, result_code, name) VALUES (4, 2 , 'Fail');
+INSERT INTO result_code (service_id, result_code, name) VALUES (4, 3 , 'Error');
