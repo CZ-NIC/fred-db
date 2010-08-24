71964c03ad391184d59e82db2dbf8391b18b12de
Juraj Viceník <juraj.vicenik@nic.cz>
2010-08-24 13:36:52 +0000

    Ticket #4392 - object references for logger - upgrade scripts
    git-svn-id: file:///data/svn/enum/db/trunk@13448 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ upgrades/NEXT-object_references_dml.sql
@@ -0,0 +1,18 @@
+--- don't forget to update database schema version
+---
+---
+
+
+
+UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
+
+
+-- Ticket #4392
+
+INSERT INTO request_object_type (id, name) VALUES (1, 'contact');
+INSERT INTO request_object_type (id, name) VALUES (2, 'nsset');
+INSERT INTO request_object_type (id, name) VALUES (3, 'domain');
+INSERT INTO request_object_type (id, name) VALUES (4, 'keyset');
+INSERT INTO request_object_type (id, name) VALUES (5, 'registrar');
+
+
