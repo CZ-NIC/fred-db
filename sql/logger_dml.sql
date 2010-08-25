f1d6d07e5e6c4e01bf266b8d55f90f3f63dc684e
Juraj Viceník <juraj.vicenik@nic.cz>
2010-08-25 09:11:54 +0000

    Ticket #4392 - added some object types
    git-svn-id: file:///data/svn/enum/db/trunk@13470 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/logger_dml.sql
+++ sql/logger_dml.sql
@@ -164,7 +164,13 @@ INSERT INTO request_object_type (id, name) VALUES (1, 'contact');
 INSERT INTO request_object_type (id, name) VALUES (2, 'nsset');
 INSERT INTO request_object_type (id, name) VALUES (3, 'domain');
 INSERT INTO request_object_type (id, name) VALUES (4, 'keyset');
-INSERT INTO request_object_type (id, name) VALUES (5, 'keyset');
+INSERT INTO request_object_type (id, name) VALUES (5, 'registrar');
+INSERT INTO request_object_type (id, name) VALUES (6, 'mail');
+INSERT INTO request_object_type (id, name) VALUES (7, 'file');
+INSERT INTO request_object_type (id, name) VALUES (8, 'publicrequest');
+INSERT INTO request_object_type (id, name) VALUES (9, 'invoice');
+INSERT INTO request_object_type (id, name) VALUES (10, 'bankstatement');
+INSERT INTO request_object_type (id, name) VALUES (11, 'request');
 
 --
 -- have to be done manually (logger will be in separate database)
--- upgrades/NEXT-object_references_dml.sql
+++ upgrades/NEXT-object_references_dml.sql
@@ -14,5 +14,12 @@ INSERT INTO request_object_type (id, name) VALUES (2, 'nsset');
 INSERT INTO request_object_type (id, name) VALUES (3, 'domain');
 INSERT INTO request_object_type (id, name) VALUES (4, 'keyset');
 INSERT INTO request_object_type (id, name) VALUES (5, 'registrar');
+INSERT INTO request_object_type (id, name) VALUES (6, 'mail');
+INSERT INTO request_object_type (id, name) VALUES (7, 'file');
+INSERT INTO request_object_type (id, name) VALUES (8, 'publicrequest');
+INSERT INTO request_object_type (id, name) VALUES (9, 'invoice');
+INSERT INTO request_object_type (id, name) VALUES (10, 'bankstatement');
+INSERT INTO request_object_type (id, name) VALUES (11, 'request');
+
 
 
