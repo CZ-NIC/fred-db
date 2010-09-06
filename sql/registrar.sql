aca8dfbd3b30cf096fc95d0b53c9ad4a6bd83b3e
Jiří Šádek <jiri.sadek@nic.cz>
2010-09-06 12:39:13 +0000

    Ticket #4291 - size of ico and dic altered
    git-svn-id: file:///data/svn/enum/db/trunk@13617 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/registrar.sql
+++ sql/registrar.sql
@@ -1,8 +1,8 @@
 -- DROP TABLE Registrar CASCADE;
 CREATE TABLE Registrar (
   ID SERIAL PRIMARY KEY,
-  ICO  char(10), -- ICO of registrar
-  DIC  char(15), -- DIC of registrar
+  ICO  char(50), -- ICO of registrar
+  DIC  char(50), -- DIC of registrar
   varsymb  char(10)  , -- coupling variable symbol ( ico )
   VAT boolean DEFAULT True, -- whether VAT should be count by invoicing 
   Handle varchar(255) UNIQUE NOT NULL,
