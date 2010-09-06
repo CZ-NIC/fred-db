9150b427dff794d5a14c28095ee793c3db3ab0dc
Juraj Viceník <juraj.vicenik@nic.cz>
2010-09-06 13:41:19 +0000

    Updated SP create_parts (added creation of reqest_object_ref parts), moved these procedures the logical place in logger_partitioning.sql file
    git-svn-id: file:///data/svn/enum/db/trunk@13621 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/logger_partitioning.sql
+++ sql/logger_partitioning.sql
@@ -489,5 +489,62 @@ DO INSTEAD SELECT tr_session ( NEW.id, NEW.user_name, NEW.user_id, NEW.login_dat
 CREATE OR REPLACE RULE request_object_ref_insert_function AS ON INSERT TO request_object_ref 
 DO INSTEAD SELECT tr_request_object_ref (NEW.id, NEW.request_time_begin, NEW.request_service_id, NEW.request_monitoring, NEW.request_id, NEW.object_type_id, NEW.object_id);
 
+--- this was originally contained in the file create_parts.sql
 
+-- CREATE partitions for a specific month
+CREATE OR REPLACE FUNCTION create_parts_for_month(part_time TIMESTAMP WITHOUT TIME ZONE) RETURNS VOID AS 
+$create_parts_for_month$ DECLARE
+        serv INTEGER;
+        cur REFCURSOR;
+BEGIN
+
+        -- a chance for minor optimization: create_tbl_* needs partitions_postfix 
+        --- which can be selected from table service. 
+        OPEN cur FOR SELECT id FROM service;
+        LOOP
+            FETCH cur INTO serv;
+            EXIT WHEN NOT FOUND;
+
+            PERFORM create_tbl_request(part_time, serv, false);
+            PERFORM create_tbl_request_data(part_time, serv, false);
+            PERFORM create_tbl_request_property_value(part_time, serv, false);
+            PERFORM create_tbl_request_object_ref(part_time, serv, false);
+            
+        END LOOP;
+
+        close cur;
+
+        -- monitoring (service type doesn't matter here - specifying 1)
+        PERFORM create_tbl_request(part_time, 1, true);
+        PERFORM create_tbl_request_data(part_time, 1, true);
+        PERFORM create_tbl_request_property_value(part_time, 1, true);
+        PERFORM create_tbl_request_object_ref(part_time, 1, true);
+
+        -- now service type -1 for session tables
+        PERFORM create_tbl_session(part_time);
+            
+END;
+$create_parts_for_month$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION create_parts(term_date TIMESTAMP WITHOUT TIME ZONE) RETURNS VOID AS $create_parts$
+DECLARE
+        term_month_beg TIMESTAMP WITHOUT TIME ZONE;
+        cur_month_beg  TIMESTAMP WITHOUT TIME ZONE;
+
+BEGIN
+        cur_month_beg := date_trunc('month', now());
+
+        term_month_beg := date_trunc('month', term_date);
+
+        LOOP 
+            PERFORM create_parts_for_month(cur_month_beg);
+
+            EXIT WHEN cur_month_beg = term_month_beg;
+            cur_month_beg := cur_month_beg + interval '1 month';
+        END LOOP;
+
+END;
+$create_parts$ LANGUAGE plpgsql;
+                
 
--- upgrades/NEXT-create_parts.sql
+++ upgrades/NEXT-create_parts.sql
@@ -1,3 +1,11 @@
+---
+--- don't forget to update database schema version
+---
+
+UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
+
+
+--- no ticket assigned 
 
 -- CREATE partitions for a specific month
 CREATE OR REPLACE FUNCTION create_parts_for_month(part_time TIMESTAMP WITHOUT TIME ZONE) RETURNS VOID AS 
@@ -16,6 +24,7 @@ BEGIN
             PERFORM create_tbl_request(part_time, serv, false);
             PERFORM create_tbl_request_data(part_time, serv, false);
             PERFORM create_tbl_request_property_value(part_time, serv, false);
+            PERFORM create_tbl_request_object_ref(part_time, serv, false);
             
         END LOOP;
 
@@ -25,6 +34,7 @@ BEGIN
         PERFORM create_tbl_request(part_time, 1, true);
         PERFORM create_tbl_request_data(part_time, 1, true);
         PERFORM create_tbl_request_property_value(part_time, 1, true);
+        PERFORM create_tbl_request_object_ref(part_time, 1, true);
 
         -- now service type -1 for session tables
         PERFORM create_tbl_session(part_time);
@@ -33,23 +43,3 @@ END;
 $create_parts_for_month$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION create_parts(term_date TIMESTAMP WITHOUT TIME ZONE) RETURNS VOID AS $create_parts$
-DECLARE
-        term_month_beg TIMESTAMP WITHOUT TIME ZONE;
-        cur_month_beg  TIMESTAMP WITHOUT TIME ZONE;
-
-BEGIN
-        cur_month_beg := date_trunc('month', now());
-
-        term_month_beg := date_trunc('month', term_date);
-
-        LOOP 
-            PERFORM create_parts_for_month(cur_month_beg);
-
-            EXIT WHEN cur_month_beg = term_month_beg;
-            cur_month_beg := cur_month_beg + interval '1 month';
-        END LOOP;
-
-END;
-$create_parts$ LANGUAGE plpgsql;
-                
