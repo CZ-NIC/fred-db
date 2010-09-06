46cacd96a6048ccb13296fb81f849d88bbf4f297
Jiří Šádek <jiri.sadek@nic.cz>
2010-09-06 15:39:19 +0000

    Ticket #3493 - add publish flag to domain changes notification
    git-svn-id: file:///data/svn/enum/db/trunk@13638 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/mail_templates.sql
+++ sql/mail_templates.sql
@@ -549,6 +549,7 @@ INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 <?cs /if ?><?cs if:changes.domain.admin_c ?>Administrativní  kontakty / Administrative contacts: <?cs call:print_value(which, changes.domain.admin_c) ?>
 <?cs /if ?><?cs if:changes.domain.temp_c ?>Dočasné kontakty / Temporary contacts: <?cs call:print_value(which, changes.domain.temp_c) ?>
 <?cs /if ?><?cs if:changes.domain.val_ex_date ?>Validováno do / Validation expiration date: <?cs call:print_value(which, changes.domain.val_ex_date) ?>
+<?cs /if ?><?cs if:changes.domain.publish ?>Přidat do ENUM tel.sezn. / Include into ENUM dict: <?cs call:print_value_bool(which, changes.domain.publish, "ano / yes", "ne / no") ?>
 <?cs /if ?><?cs elif:type == #4 ?><?cs if:changes.keyset.admin_c ?>Technické kontakty / Technical contacts: <?cs call:print_value(which, changes.keyset.admin_c) ?>
 <?cs /if ?><?cs if:subcount(changes.keyset.ds) > #0 ?><?cs call:print_value_list(which, changes.keyset.ds, "DS záznam / DS record") ?>
 <?cs /if ?><?cs if:subcount(changes.keyset.dnskey) > #0 ?><?cs call:print_value_list(which, changes.keyset.dnskey, "DNS klíče / DNS keys") ?>
