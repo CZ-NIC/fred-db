860fcb1581355cc109bd1fc75975970ca86e0a9e
Juraj Viceník <juraj.vicenik@nic.cz>
2010-09-15 15:56:53 +0000

    Ticket $4327 - refactored upgrade scripts for logger, added renaming by #4392
    git-svn-id: file:///data/svn/enum/db/trunk@13834 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-logger_webadmin.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO result_code (service_id, result_code, name) VALUES (4, 1 , 'Success');
-INSERT INTO result_code (service_id, result_code, name) VALUES (4, 2 , 'Fail');
-INSERT INTO result_code (service_id, result_code, name) VALUES (4, 3 , 'Error');
