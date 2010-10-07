a44e6101880c62252bfdb7fd0422a95ff809c527
Jiří Šádek <jiri.sadek@nic.cz>
2010-10-07 14:30:02 +0000

    Ticket #4597 - new identification template
    git-svn-id: file:///data/svn/enum/db/trunk@14300 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/mail_templates_mojeid.sql
+++ sql/mail_templates_mojeid.sql
@@ -1,34 +1,45 @@
 INSERT INTO mail_type (id, name, subject) VALUES (21, 'mojeid_identification', 'Informace k žádosti o identifikaci / Information about identification request ');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (21, 'plain', 1,
-'English version of the e-mail is entered below the Czech version
+'
+Ověření nového uživatele mojeID
 
-Informace k založení žádosti o identifikaci
 
-Vážený zákazníku,
+Vážený uživateli,
 
-   datum:  <?cs var:reqdate ?>
-   typ:  <?cs if:rtype == #1 ?>identifikace typu sms<?cs elif:rtype == #2 ?>identifikace typu dopis<?cs /if ?>
-   handle:  <?cs var:handle ?>
-   1.heslo: <?cs var:passwd ?><?cs if:passwd2 ?>
-   2.heslo: <?cs var:passwd2 ?> (demo mód)<?cs /if ?>
-   url: <?cs var:url ?>
+    tento e-mail potvrzuje úspěšné založení mojeID s těmito údaji:
 
-                                             S pozdravem
-                                             podpora <?cs var:defaults.company ?>
+    účet mojeID: <?cs var:handle ?>
+    jméno:       <?cs var:firstname ?>
+    příjmení:    <?cs var:lastname ?>
+    e-mail:      <?cs var:email ?>
 
-Information about submitted identification request
 
-Dear customer,
+    Pro další užívání účtu mojeID je nutné provést ověření Vaší
+    totožnosti pomocí dvou kódů PIN1 a PIN2.
 
-   date:  <?cs var:reqdate ?>
-   type:  <?cs if:rtype == #1 ?>sms identification<?cs elif:rtype == #2 ?>snail mail identification<?cs /if ?>
-   handle:  <?cs var:handle ?>
-   1.passwd: <?cs var:passwd ?><?cs if:passwd2 ?>
-   2.passwd: <?cs var:passwd2 ?> (demo mode)<?cs /if ?>
-   url: <?cs var:url ?>
+    <?cs if:rtype == #1 ?>Jelikož jste zvolili ověření prostřednictvím e-mailu a SMS,
+    zasíláme Vám touto cestou kód PIN1. Současně Vám zasíláme kód
+    PIN2 pomocí SMS do Vašeho mobilního telefonu.<?cs elif:rtype == #2 ?>Jelikož jste zvolili ověření prostřednictvím e-mailu a dopisu,
+    zasíláme Vám touto cestou kód PIN1. Současně Vám zasíláme kód
+    PIN2 formou doporučeného dopisu na Vaši poštovní adresu.<?cs /if ?>
 
-                                             Yours sincerely
-                                             support <?cs var:defaults.company ?>
+    Váš PIN1 je: <?cs var:passwd ?><?cs if:passwd2 ?>
+    Váš PIN2 je: <?cs var:passwd2 ?> (demo mód)<?cs /if ?>
+
+    <?cs if:rtype == #1 ?>Prosím klikněte na níže uvedený odkaz, který Vás přesměruje
+    na stránku s ověřením:<?cs elif:rtype == #2 ?>V okamžiku, kdy získáte PIN2, klikněte prosím na níže uvedený
+    odkaz, který Vás přesměruje na stránku s ověřením:<?cs /if ?>
+
+    <?cs var:url ?>
+
+
+    Přejeme Vám mnoho radosti při používání služby mojeID.
+
+
+    Váš tým CZ.NIC.
+
+
+                                   podpora <?cs var:defaults.company ?>
 ');
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (21, 21);
