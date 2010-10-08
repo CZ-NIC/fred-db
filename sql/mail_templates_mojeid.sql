f3b78d4a9c62f1c8faf9d5dd7b26fddf866f197d
Jiří Šádek <jiri.sadek@nic.cz>
2010-10-08 12:23:49 +0000

    Ticket #4597 - demo mode text changed, ?password1= added to url
    git-svn-id: file:///data/svn/enum/db/trunk@14314 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/mail_templates_mojeid.sql
+++ sql/mail_templates_mojeid.sql
@@ -18,11 +18,12 @@ Vážený uživateli,
     Pro další užívání účtu mojeID je nutné provést ověření Vaší
     totožnosti pomocí dvou kódů PIN1 a PIN2.
 
-    <?cs if:rtype == #1 ?>Jelikož jste zvolili ověření prostřednictvím e-mailu a SMS,
+    <?cs if:passwd2 ?>Z důvodu zapnutého demo módu na serveru Vám zasíláme oba
+    kódy PIN1 a PIN2 na email.<?cs else ?><?cs if:rtype == #1 ?>Jelikož jste zvolili ověření prostřednictvím e-mailu a SMS,
     zasíláme Vám touto cestou kód PIN1. Současně Vám zasíláme kód
     PIN2 pomocí SMS do Vašeho mobilního telefonu.<?cs elif:rtype == #2 ?>Jelikož jste zvolili ověření prostřednictvím e-mailu a dopisu,
     zasíláme Vám touto cestou kód PIN1. Současně Vám zasíláme kód
-    PIN2 formou doporučeného dopisu na Vaši poštovní adresu.<?cs /if ?>
+    PIN2 formou doporučeného dopisu na Vaši poštovní adresu.<?cs /if ?><?cs /if ?>
 
     Váš PIN1 je: <?cs var:passwd ?><?cs if:passwd2 ?>
     Váš PIN2 je: <?cs var:passwd2 ?> (demo mód)<?cs /if ?>
@@ -31,7 +32,7 @@ Vážený uživateli,
     na stránku s ověřením:<?cs elif:rtype == #2 ?>V okamžiku, kdy získáte PIN2, klikněte prosím na níže uvedený
     odkaz, který Vás přesměruje na stránku s ověřením:<?cs /if ?>
 
-    <?cs var:url ?>
+    <?cs var:url ?>?password1=<?cs var:passwd ?>
 
 
     Přejeme Vám mnoho radosti při používání služby mojeID.
