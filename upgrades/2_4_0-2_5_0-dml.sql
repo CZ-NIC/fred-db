45c2336fe9413fd4aebeebcea575ab73d332746a
Jaromír Talíř <jaromir.talir@nic.cz>
2010-10-18 10:00:20 +0000

    fixing MojeID DML commands
    git-svn-id: file:///data/svn/enum/db/trunk@14487 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/2_4_0-2_5_0-dml.sql
+++ upgrades/2_4_0-2_5_0-dml.sql
@@ -150,53 +150,84 @@ DNSSEC signature:
 WHERE id = 16;
 
 ---
---- MojeID messages
+--- MojeID 
 ---
 
-INSERT INTO mail_type (id, name, subject) VALUES (21, 'mojeid_identification', 'Informace k žádosti o identifikaci / Information about identification request ');
+---
+--- messages templates
+---
+INSERT INTO mail_type (id, name, subject) VALUES (21, 'mojeid_identification', 'Založení účtu mojeID');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (21, 'plain', 1,
-'English version of the e-mail is entered below the Czech version
+'
+Vážený uživateli,
 
-Informace k založení žádosti o identifikaci
+tento e-mail potvrzuje úspěšné založení účtu mojeID s těmito údaji:
 
-Vážený zákazníku,
+účet mojeID: <?cs var:handle ?>
+jméno:       <?cs var:firstname ?>
+příjmení:    <?cs var:lastname ?>
+e-mail:      <?cs var:email ?>
 
-   datum:  <?cs var:reqdate ?>
-   typ:  <?cs if:rtype == #1 ?>identifikace typu sms<?cs elif:rtype == #2 ?>identifikace typu dopis<?cs /if ?>
-   handle:  <?cs var:handle ?> 
-   1.heslo: <?cs var:passwd ?> 
-   url: <?cs var:url ?>
+Pro aktivaci Vašeho účtu je nutné vložit kódy PIN1 a PIN2.
 
-                                             S pozdravem
-                                             podpora <?cs var:defaults.company ?>
+PIN1: <?cs var:passwd ?>
+PIN2: Vám byl zaslán <?cs if:rtype == #1 ?>pomocí SMS.<?cs elif:rtype == #2 ?>poštou.<?cs /if ?><?cs if:passwd2?>
+V demo režimu není odesílání SMS a pošty aktivní. PIN2: <?cs var:passwd2 ?> <?cs /if ?><?cs if:passwd3?>
+V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 ?> <?cs /if ?>
 
-Information about submitted identification request
+Aktivaci účtu proveďte kliknutím na následující odkaz:
 
-Dear customer,
+<?cs var:url ?>?password1=<?cs var:passwd ?>
 
-   date:  <?cs var:reqdate ?>
-   type:  <?cs if:rtype == #1 ?>sms identification<?cs elif:rtype == #2 ?>snail mail identification<?cs /if ?>
-   handle:  <?cs var:handle ?> 
-   1.passwd: <?cs var:passwd ?> 
-   url: <?cs var:url ?>
-
-                                             Yours sincerely
-                                             support <?cs var:defaults.company ?>
+Váš tým <?cs var:defaults.company ?>
 ');
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (21, 21);
 
+INSERT INTO mail_type (id, name, subject) VALUES (22, 'mojeid_validation', 'Validace účtu mojeID <?cs if:status == #1 ?>provedena<?cs else ?>neprovedena<?cs /if?>');
+INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
+(22, 'plain', 1,
+'
+<?cs if:status == #1 ?>
+Na základě žádosti číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> byla provedena validace účtu mojeID.
+<?cs else ?>
+Váš účet mojeID: 
+<cs /if?>
+
+Jméno : <?cs var:name ?>
+<?cs if:org ?>Organizace : <?cs var:org><?cs /if ?>
+<?cs if:ic ?>IČ : <?cs var:ic><?cs /if ?>
+<?cs if:birthdate?>Datum narození : <?cs var:birtdate><?cs /if ?>
+Adresa : <?cs var:address>
+
+<?cs if:status != #1 ?>
+u kterého bylo požádáno o validaci žádostí číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> nebyl validován.
+<cs /if?>
+
+Váš tým <?cs var:defaults.company ?>
+
+');
+INSERT INTO mail_type_template_map (typeid, templateid) VALUES (22, 22);
 
 ---
---- Ticket #4508 - mojeid new contact states
+--- file types
 ---
+INSERT INTO enum_filetype (id, name) VALUES (7, 'mojeid contact identification request');
 
-INSERT INTO enum_object_states
-  VALUES (21,'conditionallyIdentifiedContact','{1}','t','t');
-INSERT INTO enum_object_states
-  VALUES (22,'identifiedContact','{1}','t','t');
-INSERT INTO enum_object_states
-  VALUES (23,'validatedContact','{1}','t','t');
+
+---
+--- contact states
+---
+INSERT INTO enum_object_states VALUES (21,'conditionallyIdentifiedContact','{1}','t','t');
+INSERT INTO enum_object_states VALUES (22,'identifiedContact','{1}','t','t');
+INSERT INTO enum_object_states VALUES (23,'validatedContact','{1}','t','t');
+
+INSERT INTO enum_object_states_desc VALUES (21, 'CS', 'Kontakt je podmínečně identifikován');
+INSERT INTO enum_object_states_desc VALUES (21, 'EN', 'Contact is conditionally identified');
+INSERT INTO enum_object_states_desc VALUES (22, 'CS', 'Kontakt je identifikován');
+INSERT INTO enum_object_states_desc VALUES (22, 'EN', 'Contact is identified');
+INSERT INTO enum_object_states_desc VALUES (23, 'CS', 'Kontakt je validován');
+INSERT INTO enum_object_states_desc VALUES (23, 'EN', 'Contact is validated');
 
 ---
 --- Messaging
