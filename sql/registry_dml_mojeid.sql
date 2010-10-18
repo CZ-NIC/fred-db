25cd2430b5ede48bd106355e4b5afa902895a9c5
Jaromír Talíř <jaromir.talir@nic.cz>
2010-10-18 21:07:06 +0000

    upgraded text of validation answer
    git-svn-id: file:///data/svn/enum/db/trunk@14542 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/registry_dml_mojeid.sql
+++ sql/registry_dml_mojeid.sql
@@ -34,22 +34,19 @@ INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (22, 'plain', 1,
 '
 <?cs if:status == #1 ?>
-Na základě žádosti číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> byla provedena validace účtu mojeID.
-<?cs else ?>
-Váš účet mojeID:
-<?cs /if ?>
+Na základě žádosti číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> byla provedena validace účtu mojeID.<?cs else ?>
+Váš účet mojeID:<?cs /if ?>
 
 Jméno : <?cs var:name ?><?cs if:org ?>
 Organizace : <?cs var:org ?><?cs /if ?><?cs if:ic ?>
 IČ : <?cs var:ic ?><?cs /if ?><?cs if:birthdate ?>
-Datum narození : <?cs var:birtdate ?><?cs /if ?>
+Datum narození : <?cs var:birthdate ?><?cs /if ?>
 Adresa : <?cs var:address ?>
-
-<?cs if:status != #1 ?>u kterého bylo požádáno o validaci žádostí číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> nebyl validován.
+<?cs if:status != #1 ?>
+u kterého bylo požádáno o validaci žádostí číslo <?cs var:reqid ?> ze dne <?cs var:reqdate ?> nebyl validován.
 <?cs /if ?>
-
 Váš tým <?cs var:defaults.company ?>
-');
+' id);
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (22, 22);
 
 
