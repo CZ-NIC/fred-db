8a8fbba1eac629140bbda5e05a9e5678f79b7869
Jan Zima <jan.zima@nic.cz>
2010-11-10 10:11:29 +0000

    old partial db upgrades removed #4511
    git-svn-id: file:///data/svn/enum/db/trunk@15054 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-registrars-ico-dic.sql
+++
@@ -1,7 +0,0 @@
----
---- Ticket #4291
----
-
-ALTER TABLE registrar ALTER COLUMN ico TYPE VARCHAR(50);
-ALTER TABLE registrar ALTER COLUMN dic TYPE VARCHAR(50);
-
--- upgrades/NEXT-request_id_history.sql
+++
@@ -1,5 +0,0 @@
----
---- Ticket #4508 - logger request_id for history records
----
-
-ALTER TABLE history ADD COLUMN request_id integer;
