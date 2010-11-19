d5c14859b2eac0d868a7c403fd78856b62f339d9
Jiří Šádek <jiri.sadek@nic.cz>
2010-11-19 15:29:22 +0000

    Ticket #4790 - bugfix
    git-svn-id: file:///data/svn/enum/db/trunk@15176 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-contact_disclose.sql
+++ upgrades/NEXT-contact_disclose.sql
@@ -11,35 +11,31 @@ UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
 ---
 
 --dml
-UPDATE contact SET disclosename = TRUE WHERE disclosename IS NULL; 
+UPDATE contact SET disclosename = TRUE WHERE disclosename IS NULL;
 UPDATE contact SET discloseorganization = TRUE WHERE discloseorganization IS NULL;
 UPDATE contact SET discloseaddress = TRUE WHERE discloseaddress IS NULL;
-
 UPDATE contact SET disclosetelephone = FALSE WHERE disclosetelephone IS NULL;
-UPDATE contact SET disclosetelephone = FALSE WHERE disclosefax IS NULL; 
-UPDATE contact SET disclosetelephone = FALSE WHERE discloseemail IS NULL; 
-UPDATE contact SET disclosetelephone = FALSE WHERE disclosevat IS NULL; 
-UPDATE contact SET disclosetelephone = FALSE WHERE discloseident IS NULL; 
-UPDATE contact SET disclosetelephone = FALSE WHERE disclosenotifyemail IS NULL;  
+UPDATE contact SET disclosefax = FALSE WHERE disclosefax IS NULL;
+UPDATE contact SET discloseemail = FALSE WHERE discloseemail IS NULL;
+UPDATE contact SET disclosevat = FALSE WHERE disclosevat IS NULL;
+UPDATE contact SET discloseident = FALSE WHERE discloseident IS NULL;
+UPDATE contact SET disclosenotifyemail = FALSE WHERE disclosenotifyemail IS NULL;
 
-UPDATE contact_history SET disclosename = TRUE WHERE disclosename IS NULL; 
+UPDATE contact_history SET disclosename = TRUE WHERE disclosename IS NULL;
 UPDATE contact_history SET discloseorganization = TRUE WHERE discloseorganization IS NULL;
 UPDATE contact_history SET discloseaddress = TRUE WHERE discloseaddress IS NULL;
-
 UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosetelephone IS NULL;
-UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosefax IS NULL; 
-UPDATE contact_history SET disclosetelephone = FALSE WHERE discloseemail IS NULL; 
-UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosevat IS NULL; 
-UPDATE contact_history SET disclosetelephone = FALSE WHERE discloseident IS NULL; 
-UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosenotifyemail IS NULL;  
-
+UPDATE contact_history SET disclosefax = FALSE WHERE disclosefax IS NULL;
+UPDATE contact_history SET discloseemail = FALSE WHERE discloseemail IS NULL;
+UPDATE contact_history SET disclosevat = FALSE WHERE disclosevat IS NULL;
+UPDATE contact_history SET discloseident = FALSE WHERE discloseident IS NULL;
+UPDATE contact_history SET disclosenotifyemail = FALSE WHERE disclosenotifyemail IS NULL;
 
 
 --ddl
 ALTER TABLE contact ALTER COLUMN disclosename SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseorganization SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseaddress SET NOT NULL;
-
 ALTER TABLE contact ALTER COLUMN disclosetelephone SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN disclosefax SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseemail SET NOT NULL;
@@ -50,7 +46,6 @@ ALTER TABLE contact ALTER COLUMN disclosenotifyemail SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN disclosename SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseorganization SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseaddress SET NOT NULL;
-
 ALTER TABLE contact_history ALTER COLUMN disclosetelephone SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN disclosefax SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseemail SET NOT NULL;
