b7d6cb025521e79bdb201ccb70468d5f74bb92fd
Jan Zima <jan.zima@nic.cz>
2010-11-19 10:50:01 +0000

    contact disclose  #4790
    git-svn-id: file:///data/svn/enum/db/trunk@15162 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-contact_disclose.sql
+++ upgrades/NEXT-contact_disclose.sql
@@ -14,6 +14,7 @@ UPDATE enum_parameters SET val = '<insert version here>' WHERE id = 1;
 ALTER TABLE contact ALTER COLUMN disclosename SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseorganization SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseaddress SET NOT NULL;
+
 ALTER TABLE contact ALTER COLUMN disclosetelephone SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN disclosefax SET NOT NULL;
 ALTER TABLE contact ALTER COLUMN discloseemail SET NOT NULL;
@@ -24,6 +25,7 @@ ALTER TABLE contact ALTER COLUMN disclosenotifyemail SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN disclosename SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseorganization SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseaddress SET NOT NULL;
+
 ALTER TABLE contact_history ALTER COLUMN disclosetelephone SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN disclosefax SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseemail SET NOT NULL;
@@ -31,7 +33,26 @@ ALTER TABLE contact_history ALTER COLUMN disclosevat SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN discloseident SET NOT NULL;
 ALTER TABLE contact_history ALTER COLUMN disclosenotifyemail SET NOT NULL;
 
+--dml
+UPDATE contact SET disclosename = TRUE; 
+UPDATE contact SET discloseorganization = TRUE;
+UPDATE contact SET discloseaddress = TRUE;
 
+UPDATE contact SET disclosetelephone = FALSE WHERE disclosetelephone IS NULL;
+UPDATE contact SET disclosetelephone = FALSE WHERE disclosefax IS NULL; 
+UPDATE contact SET disclosetelephone = FALSE WHERE discloseemail IS NULL; 
+UPDATE contact SET disclosetelephone = FALSE WHERE disclosevat IS NULL; 
+UPDATE contact SET disclosetelephone = FALSE WHERE discloseident IS NULL; 
+UPDATE contact SET disclosetelephone = FALSE WHERE disclosenotifyemail IS NULL;  
 
+UPDATE contact_history SET disclosename = TRUE; 
+UPDATE contact_history SET discloseorganization = TRUE;
+UPDATE contact_history SET discloseaddress = TRUE;
 
+UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosetelephone IS NULL;
+UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosefax IS NULL; 
+UPDATE contact_history SET disclosetelephone = FALSE WHERE discloseemail IS NULL; 
+UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosevat IS NULL; 
+UPDATE contact_history SET disclosetelephone = FALSE WHERE discloseident IS NULL; 
+UPDATE contact_history SET disclosetelephone = FALSE WHERE disclosenotifyemail IS NULL;  
 
