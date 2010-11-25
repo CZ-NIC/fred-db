b4f0efec97bbb7189d1449720f274e9047fe5f01
Jiří Šádek <jiri.sadek@nic.cz>
2010-11-25 15:41:40 +0000

    Ticket #4842 - separate db upgrade script
    git-svn-id: file:///data/svn/enum/db/trunk@15274 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/2.5.0-2.5.1-dml.sql
+++ upgrades/2.5.0-2.5.1-dml.sql
@@ -6,11 +6,9 @@ UPDATE enum_parameters SET val = '2.5.1' WHERE id = 1;
 
 
 ---
---- #4790
---- set contact disclose not null
+--- #4790 - set contact disclose not null (run before schema change)
 ---
 
---dml
 UPDATE contact SET disclosename = TRUE WHERE disclosename IS NULL;
 UPDATE contact SET discloseorganization = TRUE WHERE discloseorganization IS NULL;
 UPDATE contact SET discloseaddress = TRUE WHERE discloseaddress IS NULL;
@@ -30,27 +28,4 @@ UPDATE contact_history SET discloseemail = FALSE WHERE discloseemail IS NULL;
 UPDATE contact_history SET disclosevat = FALSE WHERE disclosevat IS NULL;
 UPDATE contact_history SET discloseident = FALSE WHERE discloseident IS NULL;
 UPDATE contact_history SET disclosenotifyemail = FALSE WHERE disclosenotifyemail IS NULL;
-
-
---ddl
-ALTER TABLE contact ALTER COLUMN disclosename SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN discloseorganization SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN discloseaddress SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN disclosetelephone SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN disclosefax SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN discloseemail SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN disclosevat SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN discloseident SET NOT NULL;
-ALTER TABLE contact ALTER COLUMN disclosenotifyemail SET NOT NULL;
-
-ALTER TABLE contact_history ALTER COLUMN disclosename SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN discloseorganization SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN discloseaddress SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN disclosetelephone SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN disclosefax SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN discloseemail SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN disclosevat SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN discloseident SET NOT NULL;
-ALTER TABLE contact_history ALTER COLUMN disclosenotifyemail SET NOT NULL;
-
 
