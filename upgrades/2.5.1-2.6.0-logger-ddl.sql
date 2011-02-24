f517deab83646df285cc029410c934353c33668c
Juraj Viceník <juraj.vicenik@nic.cz>
2011-02-24 16:51:33 +0000

    Ticket #5113 - extend property name space and upgrade existing (specific to our server) trimmed names - upgrade scripts
    git-svn-id: file:///data/svn/enum/db/trunk@16208 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/2.5.1-2.6.0-logger-ddl.sql
+++ upgrades/2.5.1-2.6.0-logger-ddl.sql
@@ -12,3 +12,11 @@ CREATE INDEX request_user_id_idx ON request(user_id);
 
 DROP INDEX request_data_content_idx;
 
+---
+--- Ticket #5113 - long property names from Daphne
+--- 
+
+ALTER TABLE request_property_name ALTER COLUMN name TYPE VARCHAR(256);
+
+
+
