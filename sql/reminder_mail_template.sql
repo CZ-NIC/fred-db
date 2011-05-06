88b4f7a138d9e099ee59b823c99f991ff291b015
Jiří Šádek <jiri.sadek@nic.cz>
2011-05-06 12:44:07 +0000

    Ticket #5102 - reminder_contact_message_map relation; check to not send mail for already send reminder (left join with this table)
    git-svn-id: file:///data/svn/enum/db/trunk@16927 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/reminder_mail_template.sql
+++ sql/reminder_mail_template.sql
@@ -138,3 +138,15 @@ CREATE TABLE reminder_registrar_parameter (
     template_memo text,
     reply_to varchar(200)
 );
+
+
+
+CREATE TABLE reminder_contact_message_map (
+    reminder_date date NOT NULL,
+    contact_id integer NOT NULL REFERENCES object_registry(id),
+    message_id integer REFERENCES mail_archive(id),
+    PRIMARY KEY(reminder_date, contact_id)
+);
+
+
+
