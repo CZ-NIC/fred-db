550adfa6024030295183b8c50739c8b49673dda3
Juraj Viceník <juraj.vicenik@nic.cz>
2011-05-11 15:54:53 +0000

    Ticket #5259 - actions for public request
    git-svn-id: file:///data/svn/enum/db/trunk@16971 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/logger_dml_pubrequest.sql
+++ sql/logger_dml_pubrequest.sql
@@ -2,5 +2,14 @@ INSERT INTO service (id, partition_postfix, name) VALUES
 (2, 'pubreq_', 'Public Request');
 
 -- public request result code
-INSERT INTO result_code (service_id, result_code, name) VALUES (2, 0 , 'Ok');
-INSERT INTO result_code (service_id, result_code, name) VALUES (2, 1 , 'Error');
+INSERT INTO result_code (service_id, result_code, name) VALUES 
+(2, 0 , 'Ok'),
+(2, 1 , 'Error');
+
+INSERT INTO request_type (service_id, id, name) VALUES 
+(2, 1600, 'AuthInfo'),
+(2, 1601, 'BlockTransfer'),
+(2, 1602, 'BlockChanges'),
+(2, 1603, 'UnblockTransfer'),
+(2, 1604, 'UnblockChanges');
+
