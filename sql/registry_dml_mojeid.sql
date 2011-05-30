a02d971951e9d3e086f754229dcb5eee832f58c1
Tomáš Diviš <tomas.divis@nic.cz>
2011-05-30 14:26:01 +0000

    #4910 - Change of text of email for changing email.
    git-svn-id: file:///data/svn/enum/db/trunk@17257 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/registry_dml_mojeid.sql
+++ sql/registry_dml_mojeid.sql
@@ -49,11 +49,14 @@ Váš tým <?cs var:defaults.company ?>
 ');
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (22, 22);
 
-INSERT INTO mail_type (id, name, subject) VALUES (24, 'mojeid_email_change', 'MojeID - PIN pro změnu emailu / MojeID email change PIN');
+INSERT INTO mail_type (id, name, subject) VALUES (24, 'mojeid_email_change', 'MojeID - změna emailu');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (24, 'plain', 1,
-'PIN pro změnu emailu k mojeID účtu <?cs var:username ?> je: <?cs var:pin ?>
-PIN for changing your email of mojeID account <?cs var:username ?> is: <?cs var:pin ?>');
+'Vážený uživateli,
+
+k dokončení procedury změny emailu zadejte prosím kód PIN1: <?cs var:pin ?>
+
+Váš tým CZ.NIC');
 INSERT INTO mail_type_template_map (typeid, templateid) VALUES (24, 24);
 
 ---
