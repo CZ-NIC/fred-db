cb18a537f6159bf30de81282f237a41c34905524
jvicenik <jvicenik@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-08-17 16:36:40 +0000

    Ticket #5666 - database table for disconnected registrars info
    git-svn-id: file:///data/svn/enum/db/trunk@18188 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ sql/registrar_disconnect.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE registrar_disconnect (
+    id SERIAL PRIMARY KEY,
+    registrarid INTEGER NOT NULL REFERENCES registrar(id),
+    blocked_from TIMESTAMP DEFAULT now(),
+    blocked_to TIMESTAMP,
+    unblock_request_id BIGINT
+)
+
---
+++ upgrades/NEXT-registrar_disconnect.sql
@@ -0,0 +1,12 @@
+---
+--- Ticket #5666
+---
+
+CREATE TABLE registrar_disconnect (
+    id SERIAL PRIMARY KEY,
+    registrarid INTEGER NOT NULL REFERENCES registrar(id),
+    blocked_from TIMESTAMP DEFAULT now(),
+    blocked_to TIMESTAMP,
+    unblock_request_id BIGINT
+)
+
