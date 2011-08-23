06a59b0345c2e2777ca7417922d5e54e9077ee66
jvicenik <jvicenik@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-08-23 14:29:08 +0000

    Ticket #5666 - added NOT NULL
    git-svn-id: file:///data/svn/enum/db/trunk@18267 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/registrar_disconnect.sql
+++ sql/registrar_disconnect.sql
@@ -2,7 +2,7 @@
 CREATE TABLE registrar_disconnect (
     id SERIAL PRIMARY KEY,
     registrarid INTEGER NOT NULL REFERENCES registrar(id),
-    blocked_from TIMESTAMP DEFAULT now(),
+    blocked_from TIMESTAMP NOT NULL DEFAULT now(),
     blocked_to TIMESTAMP,
     unblock_request_id BIGINT
 );
