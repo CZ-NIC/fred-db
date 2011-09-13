1195f64a8cb17b5a73eecf4f58367925f9cdf4be
jzima <jzima@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-09-13 16:16:19 +0000

    null operation_price on deposit invoice   #5796
    git-svn-id: file:///data/svn/enum/db/trunk@18514 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- sql/invoice.sql
+++ sql/invoice.sql
@@ -27,7 +27,7 @@ prefix bigint UNIQUE NOT NULL , -- 9 placed number of invoice from invoice_prefi
 registrar_id INTEGER NOT NULL REFERENCES registrar, -- link to registrar
 -- TODO registrarhistoryID for links to right ICO and DIC addresses
 balance numeric(10,2) DEFAULT 0.0, -- credit from which is taken till zero if it is NULL it is normal invoice 
-operations_price numeric(10,2) NOT NULL DEFAULT 0.0, -- invoice high also with tax 
+operations_price numeric(10,2) DEFAULT 0.0, -- account invoice sum price of operations  
 VAT numeric NOT NULL, -- VAT percent used for this invoice)
 total numeric(10,2) NOT NULL  DEFAULT 0.0 ,  -- amount without tax ( for accounting is same as price = total amount without tax);
 totalVAT numeric(10,2)  NOT NULL DEFAULT 0.0 , -- tax paid (0 for accounted tax it is paid at advance invoice)
