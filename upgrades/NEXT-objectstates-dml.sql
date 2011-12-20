e6be8cc096740dc733e77722dfb3683807d3c4c9
jzima <jzima@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-12-20 16:14:21 +0000

    set mojeidcontact state  #6304
    git-svn-id: file:///data/svn/enum/db/trunk@19594 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-objectstates-dml.sql
+++ upgrades/NEXT-objectstates-dml.sql
@@ -5,8 +5,12 @@ INSERT INTO enum_object_states VALUES (24,'mojeidContact','{1}','t','f');
 INSERT INTO enum_object_states_desc VALUES (24, 'CS', 'MojeID kontakt');
 INSERT INTO enum_object_states_desc VALUES (24, 'EN', 'MojeID contact');
 
-INSERT INTO object_state_request (object_id,state_id,crdate, valid_from)
-SELECT DISTINCT os.object_id, (SELECT id FROM enum_object_states WHERE name='mojeidContact') AS state_id, now() AS crdate, now() AS valid_from 
+INSERT INTO object_state_request (object_id,state_id, valid_from, valid_to)
+SELECT object_id, (SELECT id FROM enum_object_states WHERE name='mojeidContact') AS state_id, valid_from 
+, (CASE WHEN one_of_valid_to_is_null THEN null ELSE max_valid_to END) as valid_to 
+FROM (SELECT object_id, MIN(valid_from) AS valid_from
+, MAX(valid_to) AS max_valid_to, bool_or(valid_to is null) AS one_of_valid_to_is_null
+FROM (SELECT os.object_id, os.valid_from, os.valid_to
 FROM contact c JOIN object o ON o.id=c.id 
 JOIN object_registry obr ON obr.id = o.id
 JOIN registrar r ON r.id=o.clid
@@ -15,4 +19,7 @@ JOIN enum_object_states eos ON eos.id=os.state_id
 WHERE r.handle='REG-MOJEID'
 AND (eos.name='conditionallyIdentifiedContact'
 OR eos.name='identifiedContact'
-OR eos.name='validatedContact');
+OR eos.name='validatedContact')  
+) AS tmp1
+GROUP BY object_id
+) AS tmp2
