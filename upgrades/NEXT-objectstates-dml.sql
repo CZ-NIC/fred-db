09bce5ad600a2164e01624c5b1bc2e7b7085d06a
jzima <jzima@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-12-21 15:12:07 +0000

    set mojeidcontact state  #6304
    git-svn-id: file:///data/svn/enum/db/trunk@19600 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-objectstates-dml.sql
+++ upgrades/NEXT-objectstates-dml.sql
@@ -5,10 +5,9 @@ INSERT INTO enum_object_states VALUES (24,'mojeidContact','{1}','t','f');
 INSERT INTO enum_object_states_desc VALUES (24, 'CS', 'MojeID kontakt');
 INSERT INTO enum_object_states_desc VALUES (24, 'EN', 'MojeID contact');
 
-INSERT INTO object_state_request (object_id,state_id, valid_from, valid_to, canceled)
+INSERT INTO object_state_request (object_id,state_id, valid_from, valid_to)
 SELECT object_id, (SELECT id FROM enum_object_states WHERE name='mojeidContact') AS state_id, valid_from 
-, (CASE WHEN one_of_valid_to_is_null THEN null ELSE max_valid_to END) as valid_to 
-, (CASE WHEN one_of_valid_to_is_null THEN null ELSE max_valid_to END) as canceled
+, (CASE WHEN one_of_valid_to_is_null THEN null ELSE max_valid_to END) as valid_to
 FROM (SELECT object_id, MIN(valid_from) AS valid_from
 , MAX(valid_to) AS max_valid_to, bool_or(valid_to is null) AS one_of_valid_to_is_null
 FROM (SELECT os.object_id, os.valid_from, os.valid_to
