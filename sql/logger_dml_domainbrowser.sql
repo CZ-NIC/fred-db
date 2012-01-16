52ebf64ac3afe6271da9dbfa2186b0fafaf0e52a
jhosek <jhosek@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2012-01-16 13:01:04 +0000

    Added domainbrowser's logger initialisation
    git-svn-id: file:///data/svn/enum/db/trunk@19721 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ sql/logger_dml_domainbrowser.sql
@@ -0,0 +1,16 @@
+INSERT INTO service (id, partition_postfix, name) VALUES 
+(7, 'd_browser_', 'Domainbrowser');
+
+INSERT INTO request_type (service_id, id, name) VALUES 
+(7, 1700, 'Login'),
+(7, 1701, 'Logout'),
+(7, 1702, 'BlockingAction'),
+(7, 1703, 'DiscloseAction'),
+(7, 1704, 'Browse'),
+(7, 1705, 'Detail');
+
+INSERT INTO result_code (service_id, result_code, name) VALUES 
+(7, 1 , 'Success'),
+(7, 2 , 'Fail'),
+(7, 3 , 'Error'),
+(7, 4 , 'NotValidated');
