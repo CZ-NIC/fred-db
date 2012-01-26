7fc65b52e5146ea284ac9f61264e7e2e2c1639f2
Jiri Sadek <jiri.sadek@nic.cz>
2012-01-26 15:27:19 +0100

    Ticket #6440 - fix name for contact conditional identification public request

--- sql/public_request_dml_verification.sql
+++ sql/public_request_dml_verification.sql
@@ -2,7 +2,7 @@
 --- Public request types for contact verification
 ---
 
-INSERT INTO enum_public_request_type (id, name, description) VALUES (12, 'conditional_contact_identification', 'Conditional identification');
+INSERT INTO enum_public_request_type (id, name, description) VALUES (12, 'contact_conditional_identification', 'Conditional identification');
 INSERT INTO enum_public_request_type (id, name, description) VALUES (13, 'contact_identification', 'Full identification');
 INSERT INTO enum_public_request_type (id, name, description) VALUES (14, 'contact_validation', 'Validation');
 
