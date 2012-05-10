22c6ac1ab5eefa3ab545c2ca232108c1e06fb95b
jsadek <jsadek@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2012-05-10 15:52:53 +0000

    Ticket #6796 - initial version of 2.10 and 2.11 upgrade scripts
    git-svn-id: file:///data/svn/enum/db/trunk@20423 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- upgrades/NEXT-account_number-ddl.sql
+++
@@ -1,8 +0,0 @@
----
---- Ticket #6462
----
-
-ALTER TABLE bank_payment
-ALTER COLUMN account_number 
-TYPE text;
-
--- upgrades/NEXT-object_registry_fkey-ddl.sql
+++
@@ -1,7 +0,0 @@
----
---- Ticket #6772
----
-
-ALTER TABLE epp_info_buffer_content DROP CONSTRAINT
- epp_info_buffer_content_object_id_fkey;
-
