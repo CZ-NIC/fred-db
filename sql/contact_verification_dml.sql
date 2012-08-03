cf8aaad34499042a3670df47334881b96c778605
Jan Zima <jan.zima@nic.cz>
2012-08-03 14:32:54 +0200

    Ticket #6164 - sms message types, renamed public request types upgrade script

---
+++ sql/contact_verification_dml.sql
@@ -0,0 +1,7 @@
+---
+--- sms types
+---
+
+INSERT INTO message_type (id, type) VALUES (6, 'contact_verification_pin2');
+INSERT INTO message_type (id, type) VALUES (7, 'contact_verification_pin3');
+
---
+++ upgrades/NEXT-contact-verification-dml.sql
@@ -0,0 +1,9 @@
+---
+--- Ticket #6164
+---
+
+---
+--- sms types
+---
+INSERT INTO message_type (id, type) VALUES (6, 'contact_verification_pin2');
+INSERT INTO message_type (id, type) VALUES (7, 'contact_verification_pin3');
