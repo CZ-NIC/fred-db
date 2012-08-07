5c25301b8f7d681e441808de74f1a5c5dd960b07
Jan Zima <jan.zima@nic.cz>
2012-08-07 14:28:10 +0200

    Ticket #6164 - dummy email templates for contact verification

--- sql/contact_verification_dml.sql
+++ sql/contact_verification_dml.sql
@@ -9,7 +9,7 @@ INSERT INTO message_type (id, type) VALUES (7, 'contact_verification_pin3');
 --- email types
 ---
 
-INSERT INTO mail_type (id, name, subject) VALUES (25, 'conditional_contact_identification_url', 'Podmíněná identifikace kontaktu s url');
+INSERT INTO mail_type (id, name, subject) VALUES (25, 'conditional_contact_identification', 'Podmíněná identifikace kontaktu');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (25, 'plain', 1,
 '
@@ -31,7 +31,7 @@ V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 
 
 Aktivaci kontaktu proveďte kliknutím na následující odkaz:
 
-<?cs var:url ?>?handle=<?cs var:handle ?>
+<?cs var:url ?>?password1=<?cs var:passwd ?>
 
 Váš tým <?cs var:defaults.company ?>
 ');
@@ -39,39 +39,9 @@ INSERT INTO mail_type_template_map (typeid, templateid) VALUES (25, 25);
 
 
 
-INSERT INTO mail_type (id, name, subject) VALUES (26, 'conditional_contact_identification', 'Podmíněná identifikace kontaktu');
+INSERT INTO mail_type (id, name, subject) VALUES (26, 'contact_identification', 'Identifikace kontaktu');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (26, 'plain', 1,
-'
-Vážený uživateli,
-
-podmíněná identifikace kontaktu kontaktu s těmito údaji:
-
-kontakt: <?cs var:handle ?>
-jméno:       <?cs var:firstname ?>
-příjmení:    <?cs var:lastname ?>
-e-mail:      <?cs var:email ?>
-
-Pro aktivaci Vašeho kontaktu je nutné vložit kódy PIN1 a PIN2.
-
-PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.<?cs if:passwd2?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN2: <?cs var:passwd2 ?> <?cs /if ?><?cs if:passwd3?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 ?> <?cs /if ?>
-
-Aktivaci kontaktu proveďte kliknutím na následující odkaz:
-
-<?cs var:url ?>?password1=<?cs var:passwd ?>
-
-Váš tým <?cs var:defaults.company ?>
-');
-INSERT INTO mail_type_template_map (typeid, templateid) VALUES (26, 26);
-
-
-
-INSERT INTO mail_type (id, name, subject) VALUES (27, 'contact_identification', 'Identifikace kontaktu');
-INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
-(27, 'plain', 1,
 '
 Vážený uživateli,
 
@@ -91,10 +61,11 @@ V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 
 
 Aktivaci kontaktu proveďte kliknutím na následující odkaz:
 
-<?cs var:url ?>?password1=<?cs var:passwd ?>
+<?cs var:url ?>?handle=<?cs var:handle ?>
 
 Váš tým <?cs var:defaults.company ?>
 ');
-INSERT INTO mail_type_template_map (typeid, templateid) VALUES (27, 27);
+INSERT INTO mail_type_template_map (typeid, templateid) VALUES (26, 26);
+
 
 
--- upgrades/NEXT-contact-verification-messages-templates-dml.sql
+++ upgrades/NEXT-contact-verification-messages-templates-dml.sql
@@ -2,7 +2,7 @@
 --- Ticket #6164
 ---
 
-INSERT INTO mail_type (id, name, subject) VALUES (25, 'conditional_contact_identification_url', 'Podmíněná identifikace kontaktu s url');
+INSERT INTO mail_type (id, name, subject) VALUES (25, 'conditional_contact_identification', 'Podmíněná identifikace kontaktu');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (25, 'plain', 1,
 '
@@ -32,39 +32,9 @@ INSERT INTO mail_type_template_map (typeid, templateid) VALUES (25, 25);
 
 
 
-INSERT INTO mail_type (id, name, subject) VALUES (26, 'conditional_contact_identification', 'Podmíněná identifikace kontaktu');
+INSERT INTO mail_type (id, name, subject) VALUES (26, 'contact_identification', 'Identifikace kontaktu');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (26, 'plain', 1,
-'
-Vážený uživateli,
-
-podmíněná identifikace kontaktu kontaktu s těmito údaji:
-
-kontakt: <?cs var:handle ?>
-jméno:       <?cs var:firstname ?>
-příjmení:    <?cs var:lastname ?>
-e-mail:      <?cs var:email ?>
-
-Pro aktivaci Vašeho kontaktu je nutné vložit kódy PIN1 a PIN2.
-
-PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.<?cs if:passwd2?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN2: <?cs var:passwd2 ?> <?cs /if ?><?cs if:passwd3?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 ?> <?cs /if ?>
-
-Aktivaci kontaktu proveďte kliknutím na následující odkaz:
-
-<?cs var:url ?>?password1=<?cs var:passwd ?>
-
-Váš tým <?cs var:defaults.company ?>
-');
-INSERT INTO mail_type_template_map (typeid, templateid) VALUES (26, 26);
-
-
-
-INSERT INTO mail_type (id, name, subject) VALUES (27, 'contact_identification', 'Identifikace kontaktu');
-INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
-(27, 'plain', 1,
 '
 Vážený uživateli,
 
@@ -88,5 +58,5 @@ Aktivaci kontaktu proveďte kliknutím na následující odkaz:
 
 Váš tým <?cs var:defaults.company ?>
 ');
-INSERT INTO mail_type_template_map (typeid, templateid) VALUES (27, 27);
+INSERT INTO mail_type_template_map (typeid, templateid) VALUES (26, 26);
 
