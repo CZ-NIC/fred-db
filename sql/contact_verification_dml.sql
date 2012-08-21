dece1c1c11ebe4841f8f5f3dce4eec31cf6b9908
Jiri Sadek <jiri.sadek@nic.cz>
2012-08-21 15:09:14 +0200

    Ticket #7182 - removed demo mode from (mojeid/verification) templates

--- sql/contact_verification_dml.sql
+++ sql/contact_verification_dml.sql
@@ -25,9 +25,7 @@ e-mail:      <?cs var:email ?>
 Pro aktivaci Vašeho kontaktu je nutné vložit kódy PIN1 a PIN2.
 
 PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.<?cs if:passwd2?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN2: <?cs var:passwd2 ?> <?cs /if ?><?cs if:passwd3?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 ?> <?cs /if ?>
+PIN2: Vám byl zaslán pomocí SMS.
 
 Aktivaci kontaktu proveďte kliknutím na následující odkaz:
 
@@ -55,9 +53,7 @@ e-mail:      <?cs var:email ?>
 Pro aktivaci Vašeho kontaktu je nutné vložit kódy PIN1 a PIN2.
 
 PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.<?cs if:passwd2?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN2: <?cs var:passwd2 ?> <?cs /if ?><?cs if:passwd3?>
-V demo režimu není odesílání SMS a pošty aktivní. PIN3: <?cs var:passwd3 ?> <?cs /if ?>
+PIN2: Vám byl zaslán pomocí SMS.
 
 Aktivaci kontaktu proveďte kliknutím na následující odkaz:
 
