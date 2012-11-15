0592857a4ec15cae1c4229d0c29e82c10bcfc05e
Jaromir Talir <jaromir.talir@nic.cz>
2012-11-15 19:02:58 +0100

    Ticket #7687 - update of identification email

--- sql/registry_dml_mojeid.sql
+++ sql/registry_dml_mojeid.sql
@@ -1,27 +1,33 @@
 ---
 --- messages templates
 ---
-INSERT INTO mail_type (id, name, subject) VALUES (21, 'mojeid_identification', 'Založení účtu mojeID');
+INSERT INTO mail_type (id, name, subject) VALUES (21, 'mojeid_identification', '[mojeID] Založení účtu - PIN1 pro aktivaci mojeID');
 INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
 (21, 'plain', 1,
 '
 Vážený uživateli,
 
-tento e-mail potvrzuje úspěšné založení účtu mojeID s těmito údaji:
+před tím, než Vám aktivujeme účet mojeID, musíme ověřit správnost Vašich
+kontatních údajů, a to prostřednictvím kódů PIN1 a PIN2.
+
+PIN1: <?cs var:passwd ?>
+PIN2: Vám byl zaslán pomocí SMS.
+
+Do formuláře pro zadání těchto kódů budete přesměrováni po kliknutí na
+následující odkaz:
+
+https://<?cs var:hostname ?>/identify/email-sms/<?cs var:identification ?>/?password1=<?cs var:passwd ?>
+
+Po úspěšném odeslání formuláře budete moci začít Váš účet mojeID používat.
+Zároveň Vám pošleme poštou dopis s kódem PIN3, po jehož zadání bude Váš
+účet plně aktivní.
+
+Základní údaje o Vašem účtu:
 
 účet mojeID: <?cs var:handle ?>
 jméno:       <?cs var:firstname ?>
 příjmení:    <?cs var:lastname ?>
 e-mail:      <?cs var:email ?>
-
-Pro aktivaci Vašeho účtu je nutné vložit kódy PIN1 a PIN2.
-
-PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.
-
-Aktivaci účtu proveďte kliknutím na následující odkaz:
-
-https://<?cs var:hostname ?>/identify/email-sms/<?cs var:identification ?>/?password1=<?cs var:passwd ?>
 
 Váš tým <?cs var:defaults.company ?>
 ');
