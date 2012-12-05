b2ef7beac764146601a4e57086c1ba1c9590207c
Zdeněk Böhm <zdenek.bohm@nic.cz>
2012-12-05 13:15:14 +0100

    Ticket #7685 - Add DomainBrowser AuthInfoAction.

--- sql/logger_dml_domainbrowser.sql
+++ sql/logger_dml_domainbrowser.sql
@@ -7,11 +7,14 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (7, 1702, 'BlockingAction'),
 (7, 1703, 'DiscloseAction'),
 (7, 1704, 'Browse'),
-(7, 1705, 'Detail');
+(7, 1705, 'Detail'),
+(7, 1706, 'AuthInfoAction')
+;
 
 INSERT INTO result_code (service_id, result_code, name) VALUES 
 (7, 1 , 'Success'),
 (7, 2 , 'Fail'),
 (7, 3 , 'Error'),
 (7, 4 , 'NotValidated'),
-(7, 5 , 'Warning');
+(7, 5 , 'Warning')
+;
