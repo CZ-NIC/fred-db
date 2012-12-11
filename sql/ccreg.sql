0b71c89f70bfe920243dadb2504870efa5cb7408
Jan Zima <jan.zima@nic.cz>
2012-12-11 14:21:12 +0100

    Ticket #7652 - raise exception with custom error message if value is null

--- sql/ccreg.sql
+++ sql/ccreg.sql
@@ -232,3 +232,21 @@ CREATE TABLE ENUMVal (
         publish BOOLEAN NOT NULL DEFAULT false
         );
 
+---
+--- #7652
+---
+
+-- if value is null then raise exception, else return value
+CREATE OR REPLACE FUNCTION raise_exception_ifnull(value anyelement,errmsg text) 
+RETURNS anyelement AS 
+$BODY$  
+DECLARE
+BEGIN
+    IF value IS NULL THEN
+        RAISE EXCEPTION '%', errmsg;
+    END IF;
+    RETURN value;
+END;  
+$BODY$
+LANGUAGE 'plpgsql';
+
