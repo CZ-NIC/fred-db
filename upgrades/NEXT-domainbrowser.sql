70e3a05115bc7a395983002e258beb19b8260349
Zdeněk Böhm <zdenek.bohm@nic.cz>
2013-01-03 11:39:18 +0100

    Ticket #7685 - Add insert for Domainbrowser logger.

--- upgrades/NEXT-domainbrowser.sql
+++ upgrades/NEXT-domainbrowser.sql
@@ -1,3 +1,20 @@
-UPDATE request_type SET name = 'BlockingChange' WHERE id = 1702;
-UPDATE request_type SET name = 'DiscloseChange' WHERE id = 1703;
-INSERT INTO request_type (service_id, id, name) VALUES (7, 1706, 'AuthInfoChange');
+INSERT INTO service (id, partition_postfix, name) VALUES
+(7, 'd_browser_', 'Domainbrowser');
+
+INSERT INTO request_type (service_id, id, name) VALUES
+(7, 1700, 'Login'),
+(7, 1701, 'Logout'),
+(7, 1702, 'BlockingChange'),
+(7, 1703, 'DiscloseChange'),
+(7, 1704, 'Browse'),
+(7, 1705, 'Detail'),
+(7, 1706, 'AuthInfoChange')
+;
+
+INSERT INTO result_code (service_id, result_code, name) VALUES
+(7, 1 , 'Success'),
+(7, 2 , 'Fail'),
+(7, 3 , 'Error'),
+(7, 4 , 'NotValidated'),
+(7, 5 , 'Warning')
+;
