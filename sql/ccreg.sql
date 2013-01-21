5bde9eeda9268cd0be5df1fbc15658862a18d4e3
Jan Zima <jan.zima@nic.cz>
2013-01-21 15:52:17 +0100

    Ticket #7652 - object types in sql

--- sql/ccreg.sql
+++ sql/ccreg.sql
@@ -266,3 +266,20 @@ $BODY$
 LANGUAGE 'plpgsql';
 
 
+CREATE TABLE enum_object_type
+(
+  id integer NOT NULL,
+  name TEXT NOT NULL,
+  CONSTRAINT enum_object_type_pkey PRIMARY KEY (id),
+  CONSTRAINT enum_object_type_name_key UNIQUE (name)
+);
+
+
+INSERT INTO enum_object_type (id,name) VALUES ( 1 , 'contact' );
+INSERT INTO enum_object_type (id,name) VALUES ( 2 , 'nsset' );
+INSERT INTO enum_object_type (id,name) VALUES ( 3 , 'domain' );
+INSERT INTO enum_object_type (id,name) VALUES ( 4 , 'keyset' );
+
+ALTER TABLE object_registry ADD CONSTRAINT object_registry_type_fkey FOREIGN KEY (type)
+      REFERENCES enum_object_type (id);
+
