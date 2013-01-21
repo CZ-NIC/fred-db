6c4061a7c9d01afd9a8e2462175ff01730708d8b
Jan Zima <jan.zima@nic.cz>
2013-01-21 13:45:55 +0100

    Ticket #7652 - upgrade split

--- upgrades/NEXT-operation-ddl.sql
+++ upgrades/NEXT-operation-ddl.sql
@@ -40,12 +40,3 @@ CREATE TABLE enum_object_type
   CONSTRAINT enum_object_type_pkey PRIMARY KEY (id),
   CONSTRAINT enum_object_type_name_key UNIQUE (name)
 );
-
-INSERT INTO enum_object_type (id,name) VALUES ( 1 , 'contact' );
-INSERT INTO enum_object_type (id,name) VALUES ( 2 , 'nsset' );
-INSERT INTO enum_object_type (id,name) VALUES ( 3 , 'domain' );
-INSERT INTO enum_object_type (id,name) VALUES ( 4 , 'keyset' );
-
-ALTER TABLE object_registry ADD CONSTRAINT object_registry_type_fkey FOREIGN KEY (type)
-      REFERENCES enum_object_type (id);
-
