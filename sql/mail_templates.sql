a47f4dbf75c227e33aac3191d5c920e5c018cc30
Jiri Sadek <jiri.sadek@nic.cz>
2013-03-12 16:12:41 +0100

    Ticket #7732 - removed request_id from english version of contact merge notification email

--- sql/mail_templates.sql
+++ sql/mail_templates.sql
@@ -936,7 +936,7 @@ Information concerning the merging of duplicate entries
 Dear Customer,
 
     To simplify the administration and management of contact data in the register, the following changes have been implemented in accordance with the Domain Name Registration Rules, Section 11.10:
-Duplicate contact entries with different identifiers but identical contents were unified. All duplicate contact details were merged into a single entry carrying the identifier <?cs var:dst_contact_handle ?>. The identification number of the request is <?cs var:request_id ?>.
+Duplicate contact entries with different identifiers but identical contents were unified. All duplicate contact details were merged into a single entry carrying the identifier <?cs var:dst_contact_handle ?>.
 
 <?cs if:domain_registrant_list.0 ?>Holders were changed for the following domains:<?cs each:item = domain_registrant_list ?>
     <?cs var:item ?><?cs /each ?><?cs /if ?><?cs if:domain_admin_list.0 ?>
