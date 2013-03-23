c4b0750cf9dc3e619a6af9817b1b2812a369ab56
Jaromir Talir <jaromir.talir@nic.cz>
2013-03-23 01:07:48 -0600

    Ticket #8458 - various fixes in upgrades script to be able to use them

--- upgrades/2.10.0-2.11.0-02-dml.sql
+++ upgrades/2.10.0-2.11.0-02-dml.sql
@@ -12,31 +12,32 @@ INSERT INTO invoice_type (id, name) VALUES (1, 'account');
 
 SELECT setval('invoice_type_id_seq', 1);
 
-INSERT INTO invoice_number_prefix
-    (prefix, zone_id, invoice_type_id)
-    VALUES (24,
-            (SELECT id FROM zone WHERE fqdn = 'cz'),
-            (SELECT id FROM invoice_type WHERE name = 'advance')
-           );
+-- Initialization of prefixes should be part of configuration and not upgrade
+--INSERT INTO invoice_number_prefix
+--    (prefix, zone_id, invoice_type_id)
+--    VALUES (24,
+--            (SELECT id FROM zone WHERE fqdn = 'cz'),
+--            (SELECT id FROM invoice_type WHERE name = 'advance')
+--           );
 
-INSERT INTO invoice_number_prefix
-    (prefix, zone_id, invoice_type_id)
-    VALUES (23,
-            (SELECT id FROM zone WHERE fqdn = 'cz'),
-            (SELECT id FROM invoice_type WHERE name = 'account')
-           );
+--INSERT INTO invoice_number_prefix
+--    (prefix, zone_id, invoice_type_id)
+--    VALUES (23,
+--            (SELECT id FROM zone WHERE fqdn = 'cz'),
+--            (SELECT id FROM invoice_type WHERE name = 'account')
+--           );
 
-INSERT INTO invoice_number_prefix
-    (prefix, zone_id, invoice_type_id)
-    VALUES (11,
-            (SELECT id FROM zone WHERE fqdn = '0.2.4.e164.arpa'),
-            (SELECT id FROM invoice_type WHERE name = 'advance')
-           );
+--INSERT INTO invoice_number_prefix
+--    (prefix, zone_id, invoice_type_id)
+--    VALUES (11,
+--            (SELECT id FROM zone WHERE fqdn = '0.2.4.e164.arpa'),
+--            (SELECT id FROM invoice_type WHERE name = 'advance')
+--           );
 
-INSERT INTO invoice_number_prefix
-    (prefix, zone_id, invoice_type_id)
-    VALUES (12,
-            (SELECT id FROM zone WHERE fqdn = '0.2.4.e164.arpa'),
-            (SELECT id FROM invoice_type WHERE name = 'account')
-           );
+--INSERT INTO invoice_number_prefix
+--    (prefix, zone_id, invoice_type_id)
+--    VALUES (12,
+--            (SELECT id FROM zone WHERE fqdn = '0.2.4.e164.arpa'),
+--            (SELECT id FROM invoice_type WHERE name = 'account')
+--           );
 
--- upgrades/2.7.0-2.8.0-dml.sql
+++ upgrades/2.7.0-2.8.0-dml.sql
@@ -13,8 +13,10 @@ INSERT INTO messagetype (id, name) VALUES (16, 'request_fee_info');
 --- general registry operation used in price_list entry for request charging
 ---
 INSERT INTO enum_operation (id, operation) VALUES (3, 'GeneralEppOperation');
-INSERT INTO request_fee_parameter (id, valid_from, count_free_base, count_free_per_domain) VALUES (1, '2011-05-31 22:00:00', 25000, 100);
-INSERT INTO price_list (zone, operation, valid_from, price, period) SELECT id, 3, '2011-05-31 22:00:00', 0.1, 0  FROM zone WHERE fqdn='cz';
+
+-- Following inserts should be part of configuration not upgrade scripts
+-- INSERT INTO request_fee_parameter (id, valid_from, count_free_base, count_free_per_domain) VALUES (1, '2011-05-31 22:00:00', 25000, 100);
+-- INSERT INTO price_list (zone, operation, valid_from, price, period) SELECT id, 3, '2011-05-31 22:00:00', 0.1, 0  FROM zone WHERE fqdn='cz';
 
 ---
 --- #5583 - typo
