266ce20417af9c989140b4f47ef1f90d96cf9912
Jan Zima <jan.zima@nic.cz>
2013-04-10 15:42:48 +0200

    Ticket #7875 - unique keyset dnskey constraint

--- sql/keyset.sql
+++ sql/keyset.sql
@@ -52,6 +52,8 @@ CREATE TABLE dnskey (
 
 CREATE INDEX dnskey_keysetid_idx ON dnskey (keysetid);
 
+ALTER TABLE dnskey ADD CONSTRAINT dnskey_unique_key UNIQUE (keysetid, flags, protocol, alg, key);
+
 comment on table dnskey is '';
 comment on column dnskey.id is 'unique automatically generated identifier';
 comment on column dnskey.keysetid is 'reference to relevant record in keyset table';
--- upgrades/NEXT-update-keyset-ddl.sql
+++ upgrades/NEXT-update-keyset-ddl.sql
@@ -3,3 +3,5 @@
 ---
 
 CREATE INDEX dnskey_keysetid_idx ON dnskey (keysetid);
+
+ALTER TABLE dnskey ADD CONSTRAINT dnskey_unique_key UNIQUE (keysetid, flags, protocol, alg, key);
