7c95fa24193164b06620f01a5d53d1f25d663042
Michal Strnad <michal.strnad@nic.cz>
2013-07-25 12:17:58 +0200

    Ticket #8130 - Enable contact state serverBlocked

--- upgrades/NEXT-administrative_object_blocking.sql
+++ upgrades/NEXT-administrative_object_blocking.sql
@@ -8,3 +8,5 @@ CREATE TABLE object_state_request_reason
     PRIMARY KEY (object_state_request_id,state_on)
 );
 
+-- Enable contact state serverBlocked
+UPDATE enum_object_states SET types='{1,3}' WHERE name='serverBlocked';
