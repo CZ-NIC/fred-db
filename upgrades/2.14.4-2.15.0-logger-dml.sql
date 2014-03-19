899301f83759d241b2ee717a4ff177725cb9eab4
Jiri Sadek <jiri.sadek@nic.cz>
2014-03-19 10:12:20 +0100

    Resolve upgrade script issue because of late merging

--- upgrades/2.14.4-2.15.0-logger-dml.sql
+++ upgrades/2.14.4-2.15.0-logger-dml.sql
@@ -6,3 +6,11 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (4, 1342, 'DomainsBlockUpdate'),
 (4, 1343, 'DomainsUnblock'),
 (4, 1344, 'DomainsBlacklistAndDelete');
+
+
+---
+--- Ticket #9339
+---
+INSERT INTO request_type (service_id, id, name) VALUES
+(6, 1513, 'PublicProfileChange');
+
--- upgrades/NEXT-mojeid-public-profile.sql
+++
@@ -1,4 +0,0 @@
----
---- Ticket #9339
----
-INSERT INTO request_type (service_id, id, name) VALUES (6, 1513, 'PublicProfileChange');
