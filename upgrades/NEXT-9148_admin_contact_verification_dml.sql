1e1a3b7f0f3adebd9227bfedf24e7c0f600d1e12
Jan Korous <jan.korous@nic.cz>
2014-04-07 16:04:46 +0200

    Ticket #10936 - New test and testsuite for "thank you" letter

--- upgrades/NEXT-9148_admin_contact_verification_dml.sql
+++ upgrades/NEXT-9148_admin_contact_verification_dml.sql
@@ -1,10 +1,10 @@
--- Parse::SQL::Dia      version 0.22                              
+-- Parse::SQL::Dia      version 0.27                              
 -- Documentation        http://search.cpan.org/dist/Parse-Dia-SQL/
--- Environment          Perl 5.018001, /usr/bin/perl              
+-- Environment          Perl 5.018002, /usr/bin/perl              
 -- Architecture         x86_64-linux-gnu-thread-multi             
 -- Target Database      postgres                                  
 -- Input file           db_rev5_source.dia                        
--- Generated at         Fri Jan 17 09:22:32 2014                  
+-- Generated at         Mon Apr  7 15:55:09 2014                  
 -- Typemap for postgres not found in input file                   
 
 -- get_inserts
@@ -25,18 +25,21 @@ insert into enum_contact_test_status (id, handle) values('6', 'ok') ;
 insert into enum_contact_test_status (id, handle) values('7', 'fail') ;
 insert into enum_contact_testsuite (id, handle) values('1', 'automatic') ;
 insert into enum_contact_testsuite (id, handle) values('2', 'manual') ;
+insert into enum_contact_testsuite (id, handle) values('3', 'thank_you') ;
 insert into enum_contact_test (id, handle) values('1', 'name_syntax') ;
 insert into enum_contact_test (id, handle) values('2', 'phone_syntax') ;
 insert into enum_contact_test (id, handle) values('3', 'email_syntax') ;
 insert into enum_contact_test (id, handle) values('4', 'cz_address_existence') ;
 insert into enum_contact_test (id, handle) values('5', 'contactability') ;
 insert into enum_contact_test (id, handle) values('6', 'email_host_existence') ;
+insert into enum_contact_test (id, handle) values('7', 'send_letter') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('1', '1') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('2', '1') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('3', '1') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('4', '1') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('5', '2') ;
 insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('6', '1') ;
+insert into contact_testsuite_map (enum_contact_test_id, enum_contact_testsuite_id) values('7', '3') ;
 insert into enum_contact_test_status_localization (id, lang, name, description) values('1', 'en', 'enqueued', 'Test is ready to be run.') ;
 insert into enum_contact_test_status_localization (id, lang, name, description) values('2', 'en', 'running', 'Test is running.') ;
 insert into enum_contact_test_status_localization (id, lang, name, description) values('3', 'en', 'skipped', 'Test run was intentionally skipped.') ;
@@ -54,11 +57,13 @@ insert into enum_contact_check_status_localization (id, lang, name, description)
 insert into enum_contact_check_status_localization (id, lang, name, description) values('8', 'en', 'invalidated', 'Check was manually set to be ignored.') ;
 insert into enum_contact_testsuite_localization (id, lang, name, description) values('1', 'en', 'automatic', 'Tests without any contact owner cooperation.') ;
 insert into enum_contact_testsuite_localization (id, lang, name, description) values('2', 'en', 'manual', 'Tests where contact owner is actively taking part or is being informed.') ;
+insert into enum_contact_testsuite_localization (id, lang, name, description) values('3', 'en', 'thank_you', '"Thank you" letter used for contactability testing') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('1', 'en', 'name_syntax', 'Testing syntactical validity of name') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('2', 'en', 'phone_syntax', 'Testing syntactical validity of phone') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('3', 'en', 'email_syntax', 'Testing syntactical validity of e-mail') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('4', 'en', 'cz_address_existence', 'Testing address against official dataset (CZ only)') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('5', 'en', 'contactability', 'Testing if contact is reachable by e-mail or letter') ;
 insert into enum_contact_test_localization (id, lang, name, description) values('6', 'en', 'email_host_existence', 'Testing if e-mail host exists') ;
+insert into enum_contact_test_localization (id, lang, name, description) values('7', 'en', 'send_letter', 'Testing if contact is reachable by letter') ;
 insert into enum_filetype (id, name) values('8', 'admin contact correction notice') ;
 insert into message_type (id, type) values('9', 'admin_contact_correction_notice') ;
