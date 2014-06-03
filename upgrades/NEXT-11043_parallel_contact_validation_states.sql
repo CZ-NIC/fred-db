d14b96a17dcf4f2ee161499f93d1d3f610243195
Michal Strnad <michal.strnad@nic.cz>
2014-06-03 12:50:32 +0200

    Ticket #11043 - New public request types.

---
+++ upgrades/NEXT-11043_parallel_contact_validation_states.sql
@@ -0,0 +1,3 @@
+INSERT INTO enum_public_request_type (id,name,description) VALUES
+(19,'mojeid_contact_identification_loss','MojeID full identification loss'),
+(20,'mojeid_contact_validation_loss','MojeID validation loss');
