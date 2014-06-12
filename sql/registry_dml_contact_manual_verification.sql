e7495e2885002d5975c7f84098bf62fb2d92763b
Jan Korous <jan.korous@nic.cz>
2014-06-12 15:41:36 +0200

    Ticket #9147 - contactPassedManualVerification

--- sql/registry_dml_contact_manual_verification.sql
+++ sql/registry_dml_contact_manual_verification.sql
@@ -1,11 +1,11 @@
 INSERT INTO enum_object_states VALUES (26,'contactInManualVerification','{1}','t','t', NULL);
-INSERT INTO enum_object_states VALUES (26,'contactPassedManualVerification','{1}','t','t', NULL);
+INSERT INTO enum_object_states VALUES (25,'contactPassedManualVerification','{1}','t','t', NULL);
 INSERT INTO enum_object_states VALUES (27,'contactFailedManualVerification','{1}','t','t', NULL);
 
 INSERT INTO enum_object_states_desc VALUES (26, 'CS', 'Kontakt je ověřován zákaznickou podporou CZ.NIC');
-INSERT INTO enum_object_states_desc VALUES (26, 'CS', 'Kontakt byl ověřen zákaznickou podporou CZ.NIC');
+INSERT INTO enum_object_states_desc VALUES (25, 'CS', 'Kontakt byl ověřen zákaznickou podporou CZ.NIC');
 INSERT INTO enum_object_states_desc VALUES (27, 'CS', 'Ověření kontaktu zákaznickou podporou bylo neúspěšné');
 
 INSERT INTO enum_object_states_desc VALUES (26, 'EN', 'Contact is being verified by CZ.NIC customer support');
-INSERT INTO enum_object_states_desc VALUES (26, 'EN', 'Contact has been verified by CZ.NIC customer support');
+INSERT INTO enum_object_states_desc VALUES (25, 'EN', 'Contact has been verified by CZ.NIC customer support');
 INSERT INTO enum_object_states_desc VALUES (27, 'EN', 'Contact has failed the verification by CZ.NIC customer support');
