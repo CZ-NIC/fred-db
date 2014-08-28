73f35042b6d16c19d763e443b784e0bc9c3b6ea0
Jiri Sadek <jiri.sadek@nic.cz>
2014-08-28 15:42:22 +0200

    Ticket #11043 - add public request type for mojeid re-identification

--- sql/public_request_dml_verification.sql
+++ sql/public_request_dml_verification.sql
@@ -7,9 +7,9 @@ INSERT INTO enum_public_request_type (id, name, description) VALUES (13, 'mojeid
 INSERT INTO enum_public_request_type (id, name, description) VALUES (14, 'mojeid_contact_validation', 'MojeID validation');
 INSERT INTO enum_public_request_type (id, name, description) VALUES (17, 'mojeid_conditionally_identified_contact_transfer', 'MojeID conditionally identified contact transfer');
 INSERT INTO enum_public_request_type (id, name, description) VALUES (18, 'mojeid_identified_contact_transfer', 'MojeID identified contact transfer');
+INSERT INTO enum_public_request_type (id, name, description) VALUES (19, 'mojeid_contact_reidentification', 'MojeID full identification repeated');
 
 
 INSERT INTO enum_public_request_type (id, name, description) VALUES (15, 'contact_conditional_identification', 'Conditional identification');
 INSERT INTO enum_public_request_type (id, name, description) VALUES (16, 'contact_identification', 'Full identification');
-
 
