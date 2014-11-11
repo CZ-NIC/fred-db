35392ab24f77f2519c82a2bcb9401a6dced26e32
Jiri Sadek <jiri.sadek@nic.cz>
2014-11-11 16:35:59 +0100

    Ticket #11668 - removed unnecessary NEXT script

--- upgrades/NEXT-send_mojeid_pin3_via_OPTYS.sql
+++
@@ -1,6 +0,0 @@
---send mojeid_pin3 letters via OPTYS
---Ticket #11241 comment:24
---Ticket #11668 comment:31
-
-UPDATE message_type_forwarding_service_map SET service_handle = 'OPTYS'::message_forwarding_service
-    WHERE message_type_id = (SELECT id FROM message_type WHERE type = 'mojeid_pin3'::text);
