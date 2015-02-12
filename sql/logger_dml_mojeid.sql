f19b87263897adb6b791fc2b58053464311daea6
Vlastimil Zíma <vlastimil.zima@nic.cz>
2015-02-12 17:01:51 +0100

    Ticket #12807 - Add request type for pin3 resend in mojeid

--- sql/logger_dml_mojeid.sql
+++ sql/logger_dml_mojeid.sql
@@ -13,7 +13,8 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (6, 1509, 'TrustChange'),
 (6, 1511, 'AccountStateChange'),
 (6, 1512, 'AuthChange'),
-(6, 1513, 'PublicProfileChange');
+(6, 1513, 'PublicProfileChange'),
+(6, 1515, 'ResendPIN3');
 
 INSERT INTO result_code (service_id, result_code, name) VALUES 
 (6, 1 , 'Success'),
