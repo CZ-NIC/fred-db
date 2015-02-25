4f3a2afb72da69152327ed0ea744ab9dfba7ceae
Jan Zima <jan.zima@nic.cz>
2015-02-25 16:40:27 +0100

    Ticket #12846 - identifiers and check query changed

--- sql/state.sql
+++ sql/state.sql
@@ -1063,43 +1063,33 @@ CREATE TABLE object_state_request_reason
 CREATE OR REPLACE FUNCTION contact_discloseaddr_constraint_impl(f_object_id BIGINT)
 RETURNS void AS $$
 DECLARE
-    is_contact_with_hidden_address_and_have_nonempty_organization BOOLEAN;
-    is_contact_with_hidden_address_and_not_identified BOOLEAN;
-    is_contact_with_hidden_address_and_not_validated BOOLEAN;
+    is_contact_hidden_address_and_nonempty_organization BOOLEAN;
+    is_contact_hidden_address_and_not_identified_and_not_validated BOOLEAN;
 BEGIN
     -- ids of other object types unconstrained
     RAISE NOTICE 'check contact_discloseaddr_constraint_impl f_object_id: %', f_object_id;
 
     SELECT (trim(both ' ' from COALESCE(c.organization,'')) <> '') AS have_organization
-    , osri.id IS NULL AS not_identified_contact
-    , osrv.id IS NULL AS not_validated_contact
-    INTO is_contact_with_hidden_address_and_have_nonempty_organization,
-    is_contact_with_hidden_address_and_not_identified,
-    is_contact_with_hidden_address_and_not_validated
+    , osr.id IS NULL AS not_identified_or_validated_contact
+    INTO is_contact_hidden_address_and_nonempty_organization,
+    is_contact_hidden_address_and_not_identified_and_not_validated
     FROM contact c
-    LEFT JOIN object_state_request osri ON (
-        osri.object_id=c.id
-        AND osri.state_id = (SELECT id FROM enum_object_states WHERE name = 'identifiedContact')
+    LEFT JOIN object_state_request osr ON (
+        osr.object_id=c.id
+        AND osr.state_id IN (SELECT id FROM enum_object_states WHERE name IN ('identifiedContact','validatedContact'))
         -- following condition is from view object_state_request_now used by function update_object_states to set object states
-        AND osri.valid_from<=CURRENT_TIMESTAMP AND (osri.valid_to ISNULL OR osri.valid_to>=CURRENT_TIMESTAMP)
-        AND osri.canceled IS NULL
-    )
-    LEFT JOIN object_state_request osrv ON (
-        osrv.object_id=c.id
-        AND osrv.state_id = (SELECT id FROM enum_object_states WHERE name = 'validatedContact')
-        -- following condition is from view object_state_request_now used by function update_object_states to set object states
-        AND osrv.valid_from<=CURRENT_TIMESTAMP AND (osrv.valid_to ISNULL OR osrv.valid_to>=CURRENT_TIMESTAMP)
-        AND osrv.canceled IS NULL
+        AND osr.valid_from<=CURRENT_TIMESTAMP AND (osr.valid_to ISNULL OR osr.valid_to>=CURRENT_TIMESTAMP)
+        AND osr.canceled IS NULL
     )
     WHERE c.discloseaddress = FALSE
     AND c.id = f_object_id;
 
-    IF is_contact_with_hidden_address_and_have_nonempty_organization THEN
+    IF is_contact_hidden_address_and_nonempty_organization THEN
         --TODO: RAISE EXCEPTION
         RAISE WARNING 'contact_discloseaddr_constraint_impl failed f_object_id: % can''t have organization', f_object_id;
     END IF;
 
-    IF is_contact_with_hidden_address_and_not_identified AND is_contact_with_hidden_address_and_not_validated THEN
+    IF is_contact_hidden_address_and_not_identified_and_not_validated THEN
         --TODO: RAISE EXCEPTION
         RAISE WARNING 'contact_discloseaddr_constraint_impl failed f_object_id: % have to be identified or validated contact', f_object_id;
     END IF;
@@ -1153,5 +1143,4 @@ CREATE CONSTRAINT TRIGGER "trigger_contact_discloseaddr_constraint"
   DEFERRABLE INITIALLY DEFERRED
   FOR EACH ROW
 EXECUTE PROCEDURE contact_discloseaddr_constraint();
-
 
