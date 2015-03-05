88587588722e7b23c9118a8496ec3520e0d001f5
Jiri Sadek <jiri.sadek@nic.cz>
2015-03-05 12:56:25 +0100

    Ticket #12663 - upgrade script (saml logger)

--- upgrades/2.19.1-2.20.0-logger-dml.sql
+++ upgrades/2.19.1-2.20.0-logger-dml.sql
@@ -1,2 +1,4 @@
 INSERT INTO request_type (service_id, id, name) VALUES
+    (6, 1514, 'SamlRequest');
+INSERT INTO request_type (service_id, id, name) VALUES
     (6, 1515, 'ResendPIN3');
--- upgrades/NEXT-logger-dml_saml.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO request_type (service_id, id, name) VALUES
-(6, 1514, 'SamlRequest');
-
