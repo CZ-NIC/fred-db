42d35368fc5defebac0e170f15ad5b050a37b33e
Vlastimil Zíma <vlastimil.zima@nic.cz>
2015-04-13 11:09:22 +0200

    Ticket #12991 - Add request type - send mojeid card

--- sql/logger_dml_mojeid.sql
+++ sql/logger_dml_mojeid.sql
@@ -15,7 +15,8 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (6, 1512, 'AuthChange'),
 (6, 1513, 'PublicProfileChange'),
 (6, 1514, 'SamlRequest'),
-(6, 1515, 'ResendPIN3');
+(6, 1515, 'ResendPIN3'),
+(6, 1516, 'SendMojeIDCard');
 
 
 INSERT INTO result_code (service_id, result_code, name) VALUES 
