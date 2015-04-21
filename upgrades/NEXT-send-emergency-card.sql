cf2ee1cf59ab2f51d765ddeb9432ccb444a86742
Michal Strnad <michal.strnad@nic.cz>
2015-04-21 14:30:20 +0200

    Ticket #13007 - Add mojeid_card into message_type_forwarding_service_map.

--- upgrades/NEXT-send-emergency-card.sql
+++ upgrades/NEXT-send-emergency-card.sql
@@ -1,2 +1,6 @@
 INSERT INTO message_type  (id,type) VALUES (11,'mojeid_card');
 INSERT INTO enum_filetype (id,name) VALUES (10,'mojeid card');
+INSERT INTO message_type_forwarding_service_map (message_type_id,service_handle)
+    SELECT id,'OPTYS'::message_forwarding_service
+    FROM message_type
+    WHERE type='mojeid_card';
