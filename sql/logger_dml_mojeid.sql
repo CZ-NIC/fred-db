5809fa56d27530857dbd60506cd6fa7f089bded6
Tomas Pazderka <tomas.pazderka@nic.cz>
2015-10-01 11:03:07 +0200

    Ticket #13846 - Logging of refresh request

--- sql/logger_dml_mojeid.sql
+++ sql/logger_dml_mojeid.sql
@@ -17,7 +17,8 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (6, 1514, 'SamlRequest'),
 (6, 1515, 'ResendPIN3'),
 (6, 1516, 'SendMojeIDCard'),
-(6, 1517, 'OpenIDConnectRequest');
+(6, 1517, 'OpenIDConnectRequest'),
+(6, 1518, 'OpenIDConnectRefreshRequest');
 
 
 INSERT INTO result_code (service_id, result_code, name) VALUES 
