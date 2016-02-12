75d304c7ea9ff46767235639af7d5ced26626d7c
Jan Korous <jan.korous@nic.cz>
2016-02-12 10:37:05 +0100

    Ticket #14324 - Correct naming

--- upgrades/NEXT-changes_notifications.sql
+++ upgrades/NEXT-changes_notifications.sql
@@ -1,9 +1,9 @@
-CREATE TYPE notificated_event AS ENUM (
+CREATE TYPE notified_event AS ENUM (
     'created', 'updated', 'transferred', 'deleted', 'renewed'
 );
 
 CREATE TABLE notification_queue (
-    change                  notificated_event NOT NULL,
+    change                  notified_event NOT NULL,
     done_by_registrar       integer NOT NULL references registrar(id),
     historyid_post_change   integer NOT NULL references history(id),
     svtrid                  text NOT NULL
