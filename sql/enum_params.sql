5150c8ca8621999613e311045279bc13c65f9e1f
Michal Strnad <michal.strnad@nic.cz>
2016-04-05 11:59:39 +0200

    Ticket #13743 - Add prefix mojeid_.

--- sql/enum_params.sql
+++ sql/enum_params.sql
@@ -75,17 +75,17 @@ VALUES (14, 'regular_day_outzone_procedure_period', '14');
 -- possible values for disabling are 'disabled', 'disable', 'false', 'f' or '0'
 -- other value or missing parameter leads to enable this option
 INSERT INTO enum_parameters (id, name, val) 
-VALUES (15, 'async_sms_generation', 'enabled');
+VALUES (15, 'mojeid_async_sms_generation', 'enabled');
 -- parameter 16 can disable asynchronous generation of letters. 
 -- possible values for disabling are 'disabled', 'disable', 'false', 'f' or '0'
 -- other value or missing parameter leads to enable this option
 INSERT INTO enum_parameters (id, name, val) 
-VALUES (16, 'async_letter_generation', 'enabled');
+VALUES (16, 'mojeid_async_letter_generation', 'enabled');
 -- parameter 17 can disable asynchronous generation of e-mails. 
 -- possible values for disabling are 'disabled', 'disable', 'false', 'f' or '0'
 -- other value or missing parameter leads to enable this option
 INSERT INTO enum_parameters (id, name, val) 
-VALUES (17, 'async_email_generation', 'enabled');
+VALUES (17, 'mojeid_async_email_generation', 'enabled');
 
 
 
