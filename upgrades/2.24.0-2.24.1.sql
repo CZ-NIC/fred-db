f91e3d238ae3dd3f04c1356ecaf2a1c554ccb18b
Jiri Sadek <jiri.sadek@nic.cz>
2016-11-02 00:41:47 +0100

    Ticket #17028 - upgrade script

--- upgrades/2.24.0-2.24.1.sql
+++ upgrades/2.24.0-2.24.1.sql
@@ -113,3 +113,33 @@ Support of <?cs var:defaults.company_en ?>
 '
 WHERE id = 5;
 
+---
+--- Fax removal from e-mail templates
+---
+DELETE FROM mail_defaults WHERE name = 'fax';
+
+UPDATE mail_footer SET footer =
+'--
+<?cs var:defaults.company ?>
+<?cs var:defaults.street ?>
+<?cs var:defaults.postalcode ?> <?cs var:defaults.city ?>
+---------------------------------
+tel.: <?cs var:defaults.tel ?>
+e-mail : <?cs var:defaults.emailsupport ?>
+---------------------------------
+' WHERE id = 1;
+
+UPDATE mail_vcard SET vcard =
+'BEGIN:VCARD
+VERSION:2.1
+N:podpora CZ.NIC, z. s. p. o.
+FN:podpora CZ.NIC, z. s. p. o.
+ORG:CZ.NIC, z. s. p. o.
+TITLE:zákaznická podpora
+TEL;WORK;VOICE:+420 222 745 111
+ADR;WORK:;;Milešovská 1136/5;Praha 3;;130 00;Česká republika
+URL;WORK:http://www.nic.cz
+EMAIL;PREF;INTERNET:podpora@nic.cz
+REV:20150818T150541Z
+END:VCARD
+';
