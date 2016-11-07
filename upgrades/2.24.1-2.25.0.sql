ab1ec9119f6602895cf0b4c2ce0ed279d2402cfe
Jiri Sadek <jiri.sadek@nic.cz>
2016-11-07 14:26:51 +0100

    Ticket #16972 - upgrade script

---
+++ upgrades/2.24.1-2.25.0.sql
@@ -0,0 +1,25 @@
+---
+--- don't forget to update database schema version
+---
+UPDATE enum_parameters SET val = '2.24.1' WHERE id = 1;
+
+---
+--- Ticket #16022 - nsset dnshost prohibited IP address config
+---
+CREATE TABLE nsset_dnshost_prohibited_ipaddr (
+    network INET NOT NULL,
+    netmask INET NOT NULL,
+    UNIQUE (network, netmask),
+    CHECK (family(network) = family(netmask)),
+    CHECK (host(network)::inet = network),
+    CHECK (host(netmask)::inet = netmask)
+);
+
+COMMENT ON TABLE nsset_dnshost_prohibited_ipaddr
+    IS 'nsset dnshost prohibited IP address config, IP address $1 is prohibited if SELECT bool_or(($1::inet & netmask) = network) FROM nsset_dnshost_prohibited_ipaddr WHERE family($1::inet) = family(network); returns true.';
+
+---
+--- Ticket #15032 - duplicated nameserver hostname reason
+---
+INSERT INTO enum_reason VALUES (63, 'Duplicated nameserver hostname', 'Duplicitní jméno jmenného serveru DNS');
+SELECT setval('enum_reason_id_seq', 63);
