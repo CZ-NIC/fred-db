4fbb68c47294ff2b3570ac15fa5375bb67f479d4
Jiri Sadek <jiri.sadek@nic.cz>
2017-01-18 23:47:44 +0100

    Ticket #17824 - fix domain name validator checker ids

--- sql/enum_domain_name_validation_checker_dml.sql
+++ sql/enum_domain_name_validation_checker_dml.sql
@@ -11,6 +11,6 @@ INSERT INTO enum_domain_name_validation_checker (id, name, description)
 INSERT INTO enum_domain_name_validation_checker (id, name, description)
     VALUES (6, 'dncheck_rfc1035_preferred_syntax', 'enforces rfc1035 preferred syntax');
 INSERT INTO enum_domain_name_validation_checker (id, name, description)
-    VAlUES (7, 'dncheck_single_digit_labels_only', 'enforces single digit labels (for enum domains)');
+    VALUES (7, 'dncheck_single_digit_labels_only', 'enforces single digit labels (for enum domains)');
 INSERT INTO enum_domain_name_validation_checker (id, name, description)
-    VAlUES (7, 'dncheck_no_idn_punycode', 'forbid idn punycode encoding');
+    VALUES (8, 'dncheck_no_idn_punycode', 'forbid idn punycode encoding');
--- upgrades/NEXT-17824-allow-idn.sql
+++ upgrades/NEXT-17824-allow-idn.sql
@@ -1,2 +1,2 @@
 INSERT INTO enum_domain_name_validation_checker (id, name, description)
-    VAlUES (7, 'dncheck_no_idn_punycode', 'forbid idn punycode encoding');
+    VALUES (8, 'dncheck_no_idn_punycode', 'forbid idn punycode encoding');
