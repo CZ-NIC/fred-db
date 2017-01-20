1e5c136e5eb5cb7ea103b34711508d2c54398d41
Ales Friedl <ales.friedl@nic.cz>
2017-01-20 17:29:31 +0100

    Ticket #17931 - re-set sequences

--- upgrades/NEXT-rfc1123-hardcoded.sql
+++ upgrades/NEXT-rfc1123-hardcoded.sql
@@ -1,2 +1,4 @@
-DELETE FROM zone_domain_name_validation_checker_map WHERE checker_id IN enum_domain_name_validation_checker (SELECT id FROM enum_domain_name_validation_checker WHERE name IN ('enum_domain_name_validation_checker', 'dncheck_no_label_beginning_hyphen', 'dncheck_no_label_ending_hyphen');
-DELETE FROM enum_domain_name_validation_checker WHERE name IN ('enum_domain_name_validation_checker', 'dncheck_no_label_beginning_hyphen', 'dncheck_no_label_ending_hyphen');
+DELETE FROM zone_domain_name_validation_checker_map WHERE checker_id IN (SELECT id FROM enum_domain_name_validation_checker WHERE name IN ('dncheck_letters_digits_hyphen_chars_only', 'dncheck_no_label_beginning_hyphen', 'dncheck_no_label_ending_hyphen'));
+DELETE FROM enum_domain_name_validation_checker WHERE name IN ('dncheck_letters_digits_hyphen_chars_only', 'dncheck_no_label_beginning_hyphen', 'dncheck_no_label_ending_hyphen');
+SELECT setval('enum_domain_name_validation_checker_id_seq', (SELECT MAX(id) FROM enum_domain_name_validation_checker));
+SELECT setval('zone_domain_name_validation_checker_map_id_seq', (SELECT MAX(id) FROM zone_domain_name_validation_checker_map));
