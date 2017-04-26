977164f1f2ae5ab3c32da8555c198ee7f0ba759d
Jan Zima <jan.zima@nic.cz>
2017-04-26 09:51:05 +0200

    Ticket #17972 - comment fix

--- sql/ccreg.sql
+++ sql/ccreg.sql
@@ -21,7 +21,7 @@ RETURNS integer AS
 LANGUAGE SQL IMMUTABLE RETURNS NULL ON NULL INPUT;
 
 CREATE OR REPLACE FUNCTION domain_name_syntax_check(fqdn text) RETURNS bool AS $$
-SELECT CASE WHEN length($1::text) = 0 THEN FALSE ELSE -- bool_and() returns NULL if given 0 rows from unnest, so for empty fqdn it have to be FALSE
+SELECT CASE WHEN length($1::text) = 0 THEN FALSE ELSE -- bool_and() returns NULL if given 0 rows from unnest, so for empty fqdn it has to be FALSE
 (SELECT bool_and(label_length > 0 AND label_length <= 63 AND domain_name_length > 0 AND domain_name_length <= 255 -- RFC1035#section-2.3.4 size limits
     AND label !~* '[^a-z0-9-]' AND left(label,1) != '-' AND right(label,1) != '-') -- LDH and RFC1123#section-2.1 syntax
   FROM (SELECT label, length(label) AS label_length, length(domain_name) + 1 AS domain_name_length -- +1 for optional trailing dot
