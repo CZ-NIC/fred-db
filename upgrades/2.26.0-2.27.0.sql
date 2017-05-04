e3dd94ac8cfaa833c4687abb3bc3bdd86deac31c
Jiri Sadek <jiri.sadek@nic.cz>
2017-05-04 07:31:25 +0200

    Ticket #18491 - upgrade script

--- upgrades/2.26.0-2.27.0.sql
+++ upgrades/2.26.0-2.27.0.sql
@@ -101,3 +101,87 @@ ALTER TABLE host ADD CONSTRAINT domain_name_check CHECK (domain_name_syntax_chec
 --- Ticket #17973 - domain name db constraint
 ---
 ALTER TABLE object_registry ADD CONSTRAINT domain_name_check CHECK (type <> get_object_type_id('domain') OR domain_name_syntax_check(name));
+
+
+---
+--- Ticket #18334
+---
+INSERT INTO mail_type (id, name, subject) VALUES (31, 'expiration_dns_warning_owner', 'Upozornění na blížící se vyřazení domény <?cs var:domain ?> z DNS / Notification on the oncoming exclusion of the <?cs var:domain ?> domain from DNS');
+INSERT INTO mail_type_mail_header_defaults_map (mail_type_id,mail_header_defaults_id) SELECT id,1 FROM mail_type WHERE name='expiration_dns_warning_owner';
+INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
+(31, 'plain', 1,
+'The English version of the email follows the Czech version
+
+Vážený zákazníku,
+
+dovolujeme si Vás tímto upozornit, že jste dosud neprodloužil registraci
+doménového jména <?cs var:domain ?>. Vzhledem k této skutečnosti tak na
+základě Pravidel registrace doménových jmen rušíme ke dni <?cs var:dnsdate ?>
+delegaci doménového jména a vyřazujeme Vaši doménu ze zóny <?cs var:zone ?>.
+
+Pokud nejpozději do <?cs var:day_before_exregdate ?> neprodloužíte prostřednictvím
+určeného registrátora registraci doménového jména, dojde následujícího dne
+definitivně k zániku jeho registrace a toto doménové jméno bude k dispozici
+pro registraci i ostatním zájemcům.
+
+Jestliže se domníváte, že jste o prodloužení registrace domény u určeného
+registrátora již žádal, neváhejte ho neprodleně kontaktovat a zjistit
+příčinu, proč dosud k prodloužení registrace nedošlo. Připomínáme, že
+určeného registrátora můžete kdykoliv změnit.
+
+Co se bude dít, pokud k prodloužení nedojde:
+
+<?cs var:dnsdate ?> - Znefunkčnění doménového jména (vyřazení z DNS).
+<?cs var:exregdate ?> - Definitivní zrušení registrace doménového jména.
+
+V této chvíli evidujeme o Vaší doméně následující údaje:
+
+Doménové jméno: <?cs var:domain ?>
+Držitel: <?cs var:owner ?>
+Určený registrátor: <?cs var:registrar ?>
+<?cs each:item = administrators ?>Administrativní kontakt: <?cs var:item ?>
+<?cs /each ?>
+
+S pozdravem
+podpora <?cs var:defaults.company_cs ?>
+
+
+
+Dear customer,
+
+We would like to notify you that you still have not extended the registration
+of the domain name <?cs var:domain ?>. With regard to that fact
+and in accordance with the Domain Name Registration Rules, we are
+suspending the domain name registration and excluding it from the
+<?cs var:zone ?> zone on <?cs var:dnsdate ?>.
+
+Unless you extend the registration of your domain name through your
+designated registrar by <?cs var:day_before_exregdate ?>, 
+the registration will be cancelled definitely
+and your domain name will be released for use by another applicant.
+
+If you believe that you have already asked your designated registrar
+to extend the registration, do not hesitate to contact them again
+and find out why the extension has not occurred. Let us remind you
+that you can switch to another registrar any time.
+
+What is going to happen unless the domain is extended:
+
+<?cs var:dnsdate ?> - The domain name will become inaccessible (exclusion from DNS).
+<?cs var:exregdate ?> - The final cancellation of the domain name registration.
+
+At present, we keep the following details concerning your domain:
+
+Domain name: <?cs var:domain ?>
+Owner: <?cs var:owner ?>
+Designated registrar: <?cs var:registrar ?>
+<?cs each:item = administrators ?>Administrative contact: <?cs var:item ?>
+<?cs /each ?>
+
+Yours sincerely
+Support of <?cs var:defaults.company_en ?>
+');
+INSERT INTO mail_type_template_map (typeid, templateid) VALUES (31, 31);
+
+UPDATE notify_statechange_map SET state_id=28,mail_type_id=31 WHERE id=12;
+UPDATE notify_statechange_map SET mail_type_id=31 WHERE id=13;
