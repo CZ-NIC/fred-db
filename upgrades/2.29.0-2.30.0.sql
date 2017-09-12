e6007242e041ada197357a9b05db22aaee1b9422
Jiri Sadek <jiri.sadek@nic.cz>
2017-09-12 18:32:14 +0200

    Ticket #19903 - Update upgrade script (record statement)

--- upgrades/2.29.0-2.30.0.sql
+++ upgrades/2.29.0-2.30.0.sql
@@ -216,3 +216,36 @@ DROP INDEX message_seen_idx;
 CREATE INDEX message_clid_id_unseen_idx ON message(clid,id) WHERE NOT seen;
 COMMENT ON TABLE message IS 'Message queue for registrars which can be fetched from by epp poll functions';
 CREATE INDEX check_result_checkid_idx ON check_result(checkid);
+
+---
+--- Ticket #18680 - registry record statement printout
+---
+INSERT INTO enum_filetype (id, name) VALUES (11, 'record statement');
+
+INSERT INTO mail_type (id, name, subject) VALUES (35, 'record_statement', 'Výpis z registru / Registry record statement');
+INSERT INTO mail_type_mail_header_defaults_map (mail_type_id,mail_header_defaults_id) VALUES ((SELECT id FROM mail_type WHERE name = 'record_statement'), 1);
+INSERT INTO mail_templates (id, contenttype, footer, template) VALUES
+(35, 'plain', 1,
+'Vážený zákazníku,
+
+na základě Vaší žádosti podané prostřednictvím webového formuláře na našich stránkách dne <?cs var:request_day ?>.<?cs var:request_month ?>.<?cs var:request_year ?>, Vám v příloze zasíláme požadovaný výpis z registru doménových jmen.
+
+V případě, že jste tuto žádost nepodali, oznamte nám prosím tuto skutečnost na adresu podpora@nic.cz.
+
+S pozdravem
+podpora <?cs var:defaults.company_cs ?>
+
+
+
+Dear customer,
+
+based on the request, you submitted through a web form on our website on <?cs var:request_day ?>.<?cs var:request_month ?>.<?cs var:request_year ?>,
+we are sending the requested domain registry record statement attached to this email.
+
+If you did not make the request, please report this matter to us at podpora@nic.cz.
+
+Yours sincerely
+Support of <?cs var:defaults.company_en ?>
+
+');
+INSERT INTO mail_type_template_map (typeid, templateid) VALUES (35, 35);
