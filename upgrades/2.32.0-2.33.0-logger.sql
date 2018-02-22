fb51f852d7ae6cf7371d7deb8b6eef38b620b896
Jiri Sadek <jiri.sadek@nic.cz>
2018-02-23 00:15:48 +0100

    Ticket #21158 - Update logdb upgrade script

--- upgrades/2.32.0-2.33.0-logger.sql
+++ upgrades/2.32.0-2.33.0-logger.sql
@@ -3,3 +3,11 @@
 ---
 INSERT INTO request_type (id, name, service_id)
     VALUES (1353, 'RecordStatement', 4);  -- Webadmin
+
+
+---
+--- Ticket #17765 - Create expired domain
+---
+INSERT INTO request_type (id, name, service_id)
+    VALUES
+        (13, 'CreateExpiredDomain', 8);
--- upgrades/NEXT-logger-dml.sql
+++
@@ -1,8 +0,0 @@
----
---- Ticket #17765 - Create expired domain
----
-
-INSERT INTO request_type (service_id, id, name)
-    VALUES
-        (8, 13, 'CreateExpiredDomain');
-
