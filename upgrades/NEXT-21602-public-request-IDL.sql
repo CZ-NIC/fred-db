a40b91d1c09d3770491af2770c55e75591dbba84
Jiri Sadek <jiri.sadek@nic.cz>
2018-04-20 11:45:36 +0200

    Ticket #21659 - Remove partial upgrades

--- upgrades/NEXT-21602-public-request-IDL.sql
+++
@@ -1,6 +0,0 @@
----
---- Public request types for sending personalinfo
----
-INSERT INTO enum_public_request_type (id, name, description) VALUES (22, 'personalinfo_auto_pif', 'PersonalInfo (Web/Auto)');
-INSERT INTO enum_public_request_type (id, name, description) VALUES (23, 'personalinfo_email_pif', 'PersonalInfo (Web/Email)');
-INSERT INTO enum_public_request_type (id, name, description) VALUES (24, 'personalinfo_post_pif', 'PersonalInfo (Web/Post)');
