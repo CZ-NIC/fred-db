9c771e4d942f3fefca7f224bdc06a2af119b3208
Jiri Sadek <jiri.sadek@nic.cz>
2019-03-08 16:27:00 +0100

    Ticket #24308 - Add upgrade script (and remove partial ones)

--- upgrades/NEXT-22922-uuid.sql
+++
@@ -1,6 +0,0 @@
----
---- Ticket #22922 - Add uuid to object_registry and history
----
-CREATE EXTENSION pgcrypto;
-ALTER TABLE object_registry ADD COLUMN uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid();
-ALTER TABLE history ADD COLUMN uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid();
