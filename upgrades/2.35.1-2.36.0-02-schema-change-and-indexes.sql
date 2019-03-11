40ff7e6c7750b724b409b1273efa2f6bdda0e6c6
Jiri Sadek <jiri.sadek@nic.cz>
2019-03-11 09:58:33 +0100

    Ticket #24308 - Create index concurrently

--- upgrades/2.35.1-2.36.0-02-schema-change-and-indexes.sql
+++ upgrades/2.35.1-2.36.0-02-schema-change-and-indexes.sql
@@ -30,58 +30,58 @@ LANGUAGE sql
 IMMUTABLE;
 
 
-CREATE INDEX object_registry_name_trgm_idx ON object_registry
+CREATE INDEX CONCURRENTLY object_registry_name_trgm_idx ON object_registry
        USING GIST (name gist_trgm_ops) WHERE erdate IS NULL;
 
 
-CREATE INDEX contact_name_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_name_trgm_idx ON contact
        USING GIST (f_unaccent(name) gist_trgm_ops);
 
-CREATE INDEX contact_organization_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_organization_trgm_idx ON contact
        USING GIST (f_unaccent(organization) gist_trgm_ops);
 
-CREATE INDEX contact_streets_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_streets_trgm_idx ON contact
        USING GIST (unaccent_streets(street1, street2, street3) gist_trgm_ops);
 
-CREATE INDEX contact_city_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_city_trgm_idx ON contact
        USING GIST (f_unaccent(city) gist_trgm_ops);
 
-CREATE INDEX contact_stateorprovince_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_stateorprovince_trgm_idx ON contact
        USING GIST (f_unaccent(stateorprovince) gist_trgm_ops);
 
-CREATE INDEX contact_postalcode_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_postalcode_trgm_idx ON contact
        USING GIST (postalcode gist_trgm_ops);
 
-CREATE INDEX contact_telephone_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_telephone_trgm_idx ON contact
        USING GIST (telephone gist_trgm_ops);
 
-CREATE INDEX contact_fax_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_fax_trgm_idx ON contact
        USING GIST (fax gist_trgm_ops);
 
-CREATE INDEX contact_email_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_email_trgm_idx ON contact
        USING GIST (f_unaccent(email) gist_trgm_ops);
 
-CREATE INDEX contact_notifyemail_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_notifyemail_trgm_idx ON contact
        USING GIST (f_unaccent(notifyemail) gist_trgm_ops);
 
-CREATE INDEX contact_vat_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_vat_trgm_idx ON contact
        USING GIST (vat gist_trgm_ops);
 
-CREATE INDEX contact_ssn_trgm_idx ON contact
+CREATE INDEX CONCURRENTLY contact_ssn_trgm_idx ON contact
        USING GIST (ssn gist_trgm_ops);
 
 
-CREATE INDEX contact_address_company_name_trgm_idx ON contact_address
+CREATE INDEX CONCURRENTLY contact_address_company_name_trgm_idx ON contact_address
        USING GIST (f_unaccent(company_name) gist_trgm_ops);
 
-CREATE INDEX contact_address_streets_trgm_idx ON contact_address
+CREATE INDEX CONCURRENTLY contact_address_streets_trgm_idx ON contact_address
        USING GIST (unaccent_streets(street1, street2, street3) gist_trgm_ops);
 
-CREATE INDEX contact_address_city_trgm_idx ON contact_address
+CREATE INDEX CONCURRENTLY contact_address_city_trgm_idx ON contact_address
        USING GIST (f_unaccent(city) gist_trgm_ops);
 
-CREATE INDEX contact_address_stateorprovince_trgm_idx ON contact_address
+CREATE INDEX CONCURRENTLY contact_address_stateorprovince_trgm_idx ON contact_address
        USING GIST (f_unaccent(stateorprovince) gist_trgm_ops);
 
-CREATE INDEX contact_address_postalcode_trgm_idx ON contact_address
+CREATE INDEX CONCURRENTLY contact_address_postalcode_trgm_idx ON contact_address
        USING GIST (postalcode gist_trgm_ops);
