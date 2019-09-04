48ff894c6a6dcf83bcc7af58695d7662f68efcf6
Tomas Pazderka <tomas.pazderka@nic.cz>
2019-09-04 17:24:04 +0200

    Ticket #25845 - Add request for NiaPairing

--- sql/logger_dml_mojeid.sql
+++ sql/logger_dml_mojeid.sql
@@ -19,7 +19,8 @@ INSERT INTO request_type (service_id, id, name) VALUES
 (6, 1516, 'SendMojeIDCard'),
 (6, 1517, 'OpenIDConnectRequest'),
 (6, 1518, 'OpenIDConnectRefreshRequest'),
-(6, 1519, 'OIDCConsumerRegistration');
+(6, 1519, 'OIDCConsumerRegistration'),
+(6, 1520, 'NiaPairingRequest');
 
 
 INSERT INTO result_code (service_id, result_code, name) VALUES 
