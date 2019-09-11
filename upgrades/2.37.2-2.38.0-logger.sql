27d9371c7f5520bc90f92ed6fdccea5cccaa2fb3
Jiri Sadek <jiri.sadek@nic.cz>
2019-09-11 12:34:49 +0200

    Ticket #25848 - Add logger upgrade script

---
+++ upgrades/2.37.2-2.38.0-logger.sql
@@ -0,0 +1,5 @@
+INSERT INTO request_type (service_id, id, name)
+    VALUES (8, 15, 'MojeidDeactivateNiaAuthenticators');
+
+INSERT INTO request_type (service_id, id, name)
+    VALUES (6, 1520, 'NiaPairingRequest');
