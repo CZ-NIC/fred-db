af2ff3af74de432fb387a808095324bf6950473b
Jiri Sadek <jiri.sadek@nic.cz>
2019-11-20 12:05:14 +0100

    Ticket #25848 - Fix messed up upgrade script after tagging :/

--- upgrades/2.37.2-2.38.0.sql
+++ upgrades/2.37.2-2.38.0.sql
@@ -5,10 +5,3 @@ UPDATE enum_parameters SET val = '2.38.0' WHERE id = 1;
 
 
 INSERT INTO enum_operation (id, operation) VALUES (6, 'MonthlyFee'); -- registry contractual fee per month
-
----
---- new type for mojeid two factor reset SMS
----
-INSERT INTO message_type (id, type) VALUES (13, 'mojeid_sms_two_factor_reset');
-INSERT INTO message_type_forwarding_service_map (message_type_id, service_handle)
-  SELECT id, 'MOBILEM'::message_forwarding_service FROM message_type WHERE type = 'mojeid_sms_two_factor_reset'::text;
