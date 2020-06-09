b8541255389f9c86329d96833d4157276e216ad5
Ales Friedl <ales.friedl@nic.cz>
2020-06-09 21:02:05 +0200

    Ticket #27813 - add functional indexes to force case-insensitive unique values

--- upgrades/NEXT-27813-logger-unique.sql
+++ upgrades/NEXT-27813-logger-unique.sql
@@ -1,1 +1,10 @@
-ALTER TABLE request_object_type ADD CONSTRAINT request_object_type_name_key UNIQUE (name);
+ALTER TABLE service DROP CONSTRAINT service_partition_postfix_key;
+ALTER TABLE service DROP CONSTRAINT service_name_key;
+ALTER TABLE result_code DROP CONSTRAINT result_code_unique_name;
+ALTER TABLE request_type DROP CONSTRAINT request_type_name_service_id_key;
+
+CREATE UNIQUE INDEX service_name_unique_idx ON service (LOWER(name));
+CREATE UNIQUE INDEX service_partition_postfix_unique_idx ON service (LOWER(partition_postfix));
+CREATE UNIQUE INDEX result_code_service_id_name_unique_idx ON result_code (service_id, LOWER(name));
+CREATE UNIQUE INDEX request_type_service_id_name_unique_idx ON request_type (service_id, LOWER(name));
+CREATE UNIQUE INDEX request_object_type_name_unique_idx ON request_object_type (LOWER(name));
