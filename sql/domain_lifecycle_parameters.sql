fd92f045753cfd20378ebb8a769f825742bcf865
Michal Strnad <michal.strnad@nic.cz>
2020-06-10 08:41:03 +0200

    Ticket #27440 - Add domain_lifecycle_parameters table

---
+++ sql/domain_lifecycle_parameters.sql
@@ -0,0 +1,9 @@
+CREATE TABLE domain_lifecycle_parameters (
+    id SERIAL PRIMARY KEY,
+    valid_from TIMESTAMP NOT NULL UNIQUE DEFAULT CURRENT_TIMESTAMP,
+    expiration_notify_period INTERVAL NOT NULL,
+    outzone_unguarded_email_warning_period INTERVAL NOT NULL,
+    expiration_dns_protection_period INTERVAL NOT NULL,
+    expiration_letter_warning_period INTERVAL NOT NULL,
+    expiration_registration_protection_period INTERVAL NOT NULL
+);
