ddc00c4152169e10215c6cd0d06ae8cde39d6e9a
Michal Strnad <michal.strnad@nic.cz>
2021-05-12 18:45:08 +0200

    Ticket #30595 - Rename column constraint

--- sql/contact_identity.sql
+++ sql/contact_identity.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS contact_identity (
     identity_provider VARCHAR NOT NULL,
     subject VARCHAR NOT NULL CONSTRAINT contact_identity_subject_nonempty CHECK (0 < LENGTH(subject)),
     valid_from TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    valid_to TIMESTAMP NULL DEFAULT NULL CONSTRAINT contact_identity_valid_to_not_ahead_of_valid_from CHECK (NOT (valid_to < valid_from)),
+    valid_to TIMESTAMP NULL DEFAULT NULL CONSTRAINT contact_identity_valid_to_not_before_valid_from CHECK (valid_from <= valid_to),
     CONSTRAINT contact_identity_contact_id_fkey FOREIGN KEY (contact_id) REFERENCES object_registry (id));
 
 CREATE UNIQUE INDEX IF NOT EXISTS contact_identity_identity_provider_subject_contact_id_valid_idx ON contact_identity (identity_provider, subject, contact_id) WHERE valid_to IS NULL;
