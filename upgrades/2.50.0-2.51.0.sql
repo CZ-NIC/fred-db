db78bf2e50162a8d574f583d52c589ac15998bf6
Jiri Sadek <jiri.sadek@nic.cz>
2022-08-16 17:53:03 +0200

    Add 2.51.0 upgrade script

--- upgrades/2.50.0-2.51.0.sql
+++ upgrades/2.50.0-2.51.0.sql
@@ -1,3 +1,8 @@
+---
+--- don't forget to update database schema version
+---
+UPDATE enum_parameters SET val = '2.51.0' WHERE id = 1;
+
 ALTER TABLE registrar ADD COLUMN uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid();
 
 COMMENT ON COLUMN registrar.uuid IS 'uuid for external reference';
--- upgrades/NEXT-registrar-uuid.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE registrar ADD COLUMN uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid();
-
-COMMENT ON COLUMN registrar.uuid IS 'uuid for external reference';
