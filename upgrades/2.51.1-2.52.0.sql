9925311fcecbcf812dbffcfa1ce92efd13d93770
Jiri Sadek <jiri.sadek@gmail.com>
2022-09-21 13:19:46 +0200

    Update 2.52.0 upgrade script

--- upgrades/2.51.1-2.52.0.sql
+++ upgrades/2.51.1-2.52.0.sql
@@ -28,3 +28,18 @@ CREATE INDEX IF NOT EXISTS object_authinfo_expires_at_idx
     ON object_authinfo (expires_at)
  WHERE password <> '' AND
        canceled_at IS NULL;
+
+
+ALTER TABLE registraracl
+    ADD COLUMN uuid UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
+    ADD create_time TIMESTAMP,
+    ADD cert_data_pem VARCHAR(16384),
+    ALTER create_time SET DEFAULT CURRENT_TIMESTAMP;
+
+CREATE UNIQUE INDEX registraracl_registrarid_hexcert_fkey
+       ON registraracl (registrarid, DECODE(REGEXP_REPLACE(cert, ':', '', 'g'), 'hex'));
+
+COMMENT ON COLUMN registraracl.cert IS 'MD5 fingerprint of registrar''s certificate in HEX format ''FA:09:...:73''';
+COMMENT ON COLUMN registraracl.uuid IS 'uuid for external reference';
+COMMENT ON COLUMN registraracl.create_time IS 'when the record was created';
+COMMENT ON COLUMN registraracl.cert_data_pem IS 'certificate data in PEM format';
